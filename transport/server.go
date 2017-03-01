4cd17b0c65748d1c60e776531b7c054a5559ba2e
Greg Poirier <greg.istehbest@gmail.com>
2017-03-01 13:19:40 -0800

    Add simple request handler that yields transports

---
+++ transport/server.go
@@ -0,0 +1,30 @@
+package transport
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+// Server ...
+type Server struct {
+	upgrader *websocket.Upgrader
+}
+
+// NewServer is used to initialize a new Server and return a pointer to it.
+func NewServer() *Server {
+	return &Server{
+		upgrader: &websocket.Upgrader{},
+	}
+}
+
+// Serve is used to initialize a websocket connection and returns an pointer to
+// a Transport used to communicate with that client.
+func (s *Server) Serve(w http.ResponseWriter, r *http.Request) (*Transport, error) {
+	conn, err := s.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewTransport(conn), err
+}
