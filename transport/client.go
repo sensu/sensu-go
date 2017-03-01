4895e2876e2eb62c7556739948929c271073b667
Greg Poirier <greg.istehbest@gmail.com>
2017-03-01 13:18:53 -0800

    refactor send/receive out of client into transport

--- transport/client.go
+++ transport/client.go
@@ -1,73 +1,27 @@
 package transport
 
 import (
-	"context"
 	"net/url"
-	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
-// A Client provides a reusable, buffering transport to a Sensu backend.
-type Client struct {
-	wsconn    *websocket.Conn
-	readLock  *sync.Mutex
-	writeLock *sync.Mutex
-}
-
 // Connect causes the transport Client to connect to a given websocket backend.
 // This is a thin wrapper around a websocket connection that makes the
 // connection safe for concurrent use by multiple goroutines.
-func (c *Client) Connect(wsServerURL string) error {
+func Connect(wsServerURL string) (*Transport, error) {
 	// TODO(grep): configurable max sendq depth
 	u, err := url.Parse(wsServerURL)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// Investigate what behavior conn.SetReadDeadline() masks/causes. What causes
 	// a read timeout?
-
-	c.wsconn = conn
-	c.readLock = &sync.Mutex{}
-	c.writeLock = &sync.Mutex{}
-	return nil
-}
-
-// Send is used to send a message over the transport. It takes a message type
-// hint and a serialized payload. Send will block until the message has been
-// sent.
-func (c *Client) Send(ctx context.Context, msgType string, payload []byte) error {
-	c.writeLock.Lock()
-	defer c.writeLock.Unlock()
-
-	msg := Encode(msgType, payload)
-	err := c.wsconn.WriteMessage(websocket.BinaryMessage, msg)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (c *Client) Receive(ctx context.Context) (string, []byte, error) {
-	c.readLock.Lock()
-	defer c.readLock.Unlock()
-
-	_, p, err := c.wsconn.ReadMessage()
-	if err != nil {
-		return "", nil, err
-	}
-
-	msgType, payload, err := Decode(p)
-	if err != nil {
-		return "", nil, err
-	}
-
-	return msgType, payload, nil
+	return NewTransport(conn), nil
 }
--- transport/transport.go
+++ transport/transport.go
@@ -2,7 +2,11 @@ package transport
 
 import (
 	"bytes"
+	"context"
 	"errors"
+	"sync"
+
+	"github.com/gorilla/websocket"
 )
 
 var (
@@ -26,4 +30,59 @@ func Decode(payload []byte) (string, []byte, error) {
 	msgType := payload[0:nl]
 	msg := payload[nl+1:]
 	return string(msgType), msg, nil
+}
+
+type Transport struct {
+	Connection *websocket.Conn
+
+	readLock  *sync.Mutex
+	writeLock *sync.Mutex
+}
+
+// Create an initialized Transport and return its pointer.
+func NewTransport(conn *websocket.Conn) *Transport {
+	return &Transport{
+		Connection: conn,
+		readLock:   &sync.Mutex{},
+		writeLock:  &sync.Mutex{},
+	}
+}
+
+// TODO(grep): handle context cancelling / read timeout so that we we don't
+// deadlock on the readLock mutex. Is this possible to do with contexts
+// and gorilla/websocket? Is there some way that this is not totally screwed?
+
+// Send is used to send a message over the transport. It takes a message type
+// hint and a serialized payload. Send will block until the message has been
+// sent.
+func (t *Transport) Send(ctx context.Context, msgType string, payload []byte) error {
+	t.writeLock.Lock()
+	defer t.writeLock.Unlock()
+
+	msg := Encode(msgType, payload)
+	err := t.Connection.WriteMessage(websocket.BinaryMessage, msg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Receive is used to receive a message from the transport. It takes a context
+// and blocks until the next message is received from the transport.
+func (t *Transport) Receive(ctx context.Context) (string, []byte, error) {
+	t.readLock.Lock()
+	defer t.readLock.Unlock()
+
+	_, p, err := t.Connection.ReadMessage()
+	if err != nil {
+		return "", nil, err
+	}
+
+	msgType, payload, err := Decode(p)
+	if err != nil {
+		return "", nil, err
+	}
+
+	return msgType, payload, nil
 }
