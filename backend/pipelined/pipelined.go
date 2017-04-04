b1420b12ad10f9aee88dbb480c1a73d207ecc8ab
Sean Porter <portertech@gmail.com>
2017-04-04 15:02:38 -0700

    [pipelined] beginnings of the sensu event pipeline

---
+++ backend/pipelined/pipelined.go
@@ -0,0 +1,136 @@
+// Package pipelined provides the traditional Sensu event pipeline.
+package pipelined
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"sync"
+	"sync/atomic"
+
+	"github.com/sensu/sensu-go/backend/messaging"
+	"github.com/sensu/sensu-go/command"
+	"github.com/sensu/sensu-go/types"
+)
+
+const (
+	PipelineCount int = 10
+)
+
+// Pipelined handles incoming Sensu events and puts them through the
+// Sensu event pipeline, i.e. filter -> mutator -> handler.
+type Pipelined struct {
+	stopping chan struct{}
+	running  *atomic.Value
+	wg       *sync.WaitGroup
+
+	MessageBus messaging.MessageBus
+	errChan    chan error
+}
+
+// Start pipelined.
+func (p *Pipelined) Start() error {
+	if p.MessageBus == nil {
+		return errors.New("no message bus found")
+	}
+
+	p.stopping = make(chan struct{}, 1)
+	p.running = &atomic.Value{}
+	p.wg = &sync.WaitGroup{}
+
+	p.errChan = make(chan error, 1)
+
+	ch := make(chan []byte, 100)
+
+	if err := p.MessageBus.Subscribe("sensu:event", "pipelined", ch); err != nil {
+		return err
+	}
+
+	if err := p.createPipelines(PipelineCount, ch); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Stop pipelined.
+func (p *Pipelined) Stop() error {
+	p.running.Store(false)
+	close(p.stopping)
+	close(p.errChan)
+	p.wg.Wait()
+
+	return nil
+}
+
+// Status returns an error if pipelined is unhealthy.
+func (p *Pipelined) Status() error {
+	return nil
+}
+
+// Err returns a channel to listen for terminal errors on.
+func (p *Pipelined) Err() <-chan error {
+	return p.errChan
+}
+
+func (p *Pipelined) createPipelines(count int, channel chan []byte) error {
+	for i := 1; i <= count; i++ {
+		p.wg.Add(1)
+		go func() {
+			defer p.wg.Done()
+			for {
+				select {
+				case <-p.stopping:
+					return
+				case msg := <-channel:
+					event := &types.Event{}
+					err := json.Unmarshal(msg, event)
+
+					if err != nil {
+						continue
+					}
+
+					p.handleEvent(event)
+				}
+			}
+		}()
+	}
+
+	return nil
+}
+
+func (p *Pipelined) handleEvent(event *types.Event) error {
+	eventData, err := p.executeEventMutator(event)
+
+	if err != nil {
+		return nil
+	}
+
+	handlerExec, err := p.executeEventHandler(eventData)
+
+	if err != nil {
+		return nil
+	}
+
+	log.Printf("executed event handler: status: %x output: %s", handlerExec.Status, handlerExec.Output)
+
+	return err
+}
+
+func (p *Pipelined) executeEventHandler(eventData []byte) (*command.Execution, error) {
+	handlerExec := &command.Execution{}
+
+	handlerExec.Command = "cat"
+	handlerExec.Input = string(eventData[:])
+
+	result, err := command.ExecuteCommand(context.Background(), handlerExec)
+
+	return result, err
+}
+
+func (p *Pipelined) executeEventMutator(event *types.Event) ([]byte, error) {
+	eventData, err := json.Marshal(event)
+
+	return eventData, err
+}
---
+++ backend/pipelined/pipelined_test.go
@@ -0,0 +1,31 @@
+// Package pipelined provides the traditional Sensu event pipeline.
+package pipelined
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/sensu/sensu-go/backend/messaging"
+	"github.com/sensu/sensu-go/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPipelined(t *testing.T) {
+	p := &Pipelined{}
+
+	bus := &messaging.WizardBus{}
+	bus.Start()
+
+	p.MessageBus = bus
+
+	assert.NoError(t, p.Start())
+
+	event := &types.Event{}
+
+	eventJSON, _ := json.Marshal(event)
+
+	err := bus.Publish("sensu:event", eventJSON)
+	assert.NoError(t, err)
+
+	assert.NoError(t, p.Stop())
+}
