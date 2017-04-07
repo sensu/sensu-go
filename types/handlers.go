1e2d74a4fba4e60b897da8e36d703dbf6e74332f
Sean Porter <portertech@gmail.com>
2017-04-07 09:36:27 -0700

    [socket-handlers] added handler type socket

--- types/handlers.go
+++ types/handlers.go
@@ -14,13 +14,24 @@ type Handler struct {
 	Mutator string `json:"mutator,omitempty"`
 
 	// Command is the command to be executed for a pipe handler.
-	Command string `json:"command"`
+	Command string `json:"command,omitempty"`
 
 	// Timeout is the handler timeout in seconds.
 	Timeout int `json:"timeout"`
 
+	Socket HandlerSocket `json:"socket,omitempty"`
+
 	// Handlers is a list of handlers for a handler set.
 	Handlers []string `json:"handlers,omitempty"`
+}
+
+// HandlerSocket contains configuration for a TCP or UDP handler.
+type HandlerSocket struct {
+	// Host is the socket peer address.
+	Host string `json:"host"`
+
+	// Port is the socket peer port.
+	Port int `json:"port"`
 }
 
 // Validate returns an error if the handler does not pass validation tests.
