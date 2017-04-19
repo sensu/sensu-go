8d5e89193aa5fe886230c067e5ef6c8c34b9bdd4
Greg Poirier <greg.istehbest@gmail.com>
2017-04-19 08:45:18 -0700

    Add events controller

---
+++ backend/apid/controllers/events.go
@@ -0,0 +1,58 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sensu/sensu-go/backend/store"
+)
+
+// EventsController handles requests for /events
+type EventsController struct {
+	Store store.Store
+}
+
+// Register the EventsController with a mux.Router.
+func (c *EventsController) Register(r *mux.Router) {
+	r.HandleFunc("/events/{entity}", c.entityEvents).Methods(http.MethodGet)
+	r.HandleFunc("/events/{entity}/{check}", c.entityCheckEvents).Methods(http.MethodGet)
+	// TODO(greg): Handle a PUT to /events
+}
+
+func (c *EventsController) entityEvents(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	entityID := vars["entity"]
+	// Do we need to test that this isn't empty? We should figure that out.
+
+	events, err := c.Store.GetEventsByEntity(entityID)
+	if err != nil {
+		http.Error(w, "error getting events for entity", http.StatusInternalServerError)
+	}
+
+	jsonStr, err := json.Marshal(events)
+	if err != nil {
+		http.Error(w, "error marshalling response", http.StatusInternalServerError)
+	}
+
+	fmt.Fprint(w, string(jsonStr))
+}
+
+func (c *EventsController) entityCheckEvents(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	entityID := vars["entity"]
+	checkID := vars["check"]
+
+	event, err := c.Store.GetEventByEntityCheck(entityID, checkID)
+	if err != nil {
+		http.Error(w, "error getting event for check", http.StatusInternalServerError)
+	}
+
+	jsonStr, err := json.Marshal(event)
+	if err != nil {
+		http.Error(w, "error marshalling response", http.StatusInternalServerError)
+	}
+
+	fmt.Fprint(w, string(jsonStr))
+}
