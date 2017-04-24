ae23a2f39f8620094fb540bde2640461f11eed9a
James Phillips <jamesdphillips@gmail.com>
2017-04-23 23:00:17 -0700

    Pattern for rendering tabular representation of results
    Introduces dependency `tablewriter`

--- cli/commands/event/list.go
+++ cli/commands/event/list.go
@@ -4,27 +4,89 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/sensu/sensu-go/cli"
+	"github.com/sensu/sensu-go/cli/elements/table"
+	"github.com/sensu/sensu-go/types"
 	"github.com/spf13/cobra"
 )
 
 // ListCommand defines new list events command
 func ListCommand(cli *cli.SensuCli) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "list",
-		Short: "list events",
-		Run: func(cmd *cobra.Command, args []string) {
+		Use:          "list",
+		Short:        "list events",
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			config := cli.Config
+			config.BindPFlag("format", cmd.Flags().Lookup("format"))
+			format := config.GetString("format")
+
+			// Fetch events from API
 			r, err := cli.Client.ListEvents()
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s\n", err)
-				os.Exit(1)
+				return err
 			}
 
-			result, _ := json.MarshalIndent(r, "", "  ")
-			fmt.Fprintf(os.Stdout, "%s\n", result)
+			if format == "json" {
+				writeEventsInJSON(&r)
+			} else {
+				writeEventsToTable(r)
+			}
+
+			return nil
 		},
 	}
 
+	cmd.Flags().StringP("format", "f", "", "format of data returned; defaults to human readable tabular style")
+
 	return cmd
+}
+
+func writeEventsInJSON(queryResults *[]types.Event) {
+	result, _ := json.MarshalIndent(queryResults, "", "  ")
+	fmt.Fprintf(os.Stdout, "%s\n", result)
+}
+
+func writeEventsToTable(queryResults []types.Event) {
+	rows := make([]*table.Row, len(queryResults))
+	for i, result := range queryResults {
+		rows[i] = &table.Row{Value: result}
+	}
+
+	table := table.New([]*table.Column{
+		{
+			Title:       "Source",
+			ColumnStyle: table.PrimaryTextStyle,
+			CellTransformer: func(data interface{}) string {
+				event, _ := data.(types.Event)
+				return event.Entity.ID
+			},
+		},
+		{
+			Title: "Check",
+			CellTransformer: func(data interface{}) string {
+				event, _ := data.(types.Event)
+				return event.Check.Name
+			},
+		},
+		{
+			Title: "Result",
+			CellTransformer: func(data interface{}) string {
+				event, _ := data.(types.Event)
+				return event.Check.Output
+			},
+		},
+		{
+			Title: "Timestamp",
+			CellTransformer: func(data interface{}) string {
+				event, _ := data.(types.Event)
+				time := time.Unix(event.Timestamp, 0)
+				return time.String()
+			},
+		},
+	})
+
+	table.Render(rows)
 }
