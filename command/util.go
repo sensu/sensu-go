a09465e2a7d4864433927867f36a551732848618
Justin Kolberg <amd.prophet@gmail.com>
2017-05-16 11:55:34 -0700

    export FakeCommand, document command mock funcs

---
+++ command/util.go
@@ -0,0 +1,23 @@
+package command
+
+import (
+	"os"
+	"strings"
+)
+
+// FakeCommand takes a command and (optionally) command args and will execute
+// the TestHelperProcess test within the package FakeCommand is called from.
+func FakeCommand(command string, args ...string) *Execution {
+	cs := []string{os.Args[0], "-test.run=TestHelperProcess", "--", command}
+	cs = append(cs, args...)
+	cmdStr := strings.Join(cs, " ")
+	trimmedCmd := strings.Trim(cmdStr, " ")
+	env := []string{"GO_WANT_HELPER_PROCESS=1"}
+
+	execution := &Execution{
+		Command: trimmedCmd,
+		Env:     env,
+	}
+
+	return execution
+}
--- types/util.go
+++ types/util.go
@@ -6,6 +6,9 @@ import (
 	"strings"
 )
 
+// FakeHandlerCommand takes a command and (optionally) command args and will
+// execute the TestHelperHandlerProcess test within the package
+// FakeHandlerCommand is called from.
 func FakeHandlerCommand(command string, args ...string) *Handler {
 	cs := []string{os.Args[0], "-test.run=TestHelperHandlerProcess", "--", command}
 	cs = append(cs, args...)
@@ -20,6 +23,9 @@ func FakeHandlerCommand(command string, args ...string) *Handler {
 	return handler
 }
 
+// FakeMutatorCommand takes a command and (optionally) command args and will
+// execute the TestHelperMutatorProcess test within the package
+// FakeMutatorCommand is called from.
 func FakeMutatorCommand(command string, args ...string) *Mutator {
 	cs := []string{os.Args[0], "-test.run=TestHelperMutatorProcess", "--", command}
 	cs = append(cs, args...)
