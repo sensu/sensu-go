af5d0fefa6d852fed09f8db5d3a4d8a3dc2d6cff
Simon Plourde <simon@sensu.io>
2017-06-02 09:37:23 -0400

    Cleanup

--- backend/authentication/middleware.go
+++ backend/authentication/middleware.go
@@ -23,40 +23,6 @@ func Middleware(provider Provider, next http.Handler) http.Handler {
 			return
 		}
 
-		// // Does credentials were provided in the Authorization header?
-		// username, password, ok := r.BasicAuth()
-		// if ok {
-		// 	user, err := provider.Authenticate(username, password)
-		// 	if err != nil {
-		// 		logger.WithField(
-		// 			"user", username,
-		// 		).Info("Authentication failed. Invalid username or password")
-		// 		http.Error(w, "Request unauthorized", http.StatusUnauthorized)
-		// 		return
-		// 	}
-		//
-		// // Create the token and a signed version
-		// token, tokenString, err := jwt.AccessToken(user.Username)
-		// if err != nil {
-		// 	logger.WithField(
-		// 		"user", username,
-		// 	).Infof("Authentication failed: %s", err.Error())
-		// 	http.Error(w, "Request unauthorized", http.StatusUnauthorized)
-		// 	return
-		// }
-		//
-		// 	// Set the claims into the request context
-		// 	jwt.SetClaimsIntoContext(r, token)
-		//
-		// 	// Add the signed token to the response if login
-		// 	if r.URL.Path == "/auth" {
-		// 		w.Write([]byte(tokenString))
-		// 	}
-		//
-		// 	next.ServeHTTP(w, r)
-		// 	return
-		// }
-
 		// Does a bearer token was provided in the Authorization header?
 		var tokenString string
 		tokens, ok := r.Header["Authorization"]
