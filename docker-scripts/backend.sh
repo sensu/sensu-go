c5d9c1e0ee51798166e3b43492498c3d28134cda
Greg Poirier <greg.istehbest@gmail.com>
2017-06-30 12:02:04 -0700

    Fix backend.sh to make docker-compose work again

--- docker-scripts/backend.sh
+++ docker-scripts/backend.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-/opt/bin/sensu-backend start --store-client-url http://0.0.0.0:2379 &
+/opt/bin/sensu-backend start --store-client-url http://127.0.0.1:2379 &
 /opt/bin/tools/sleep 5
-/opt/bin/sensu-cli handler create slack -t pipe --baseURL http://backend1:8080 -c "/opt/bin/handlers/slack -w https://hooks.slack.com/services/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 -c '#sensu-spam'"
-/opt/bin/sensu-cli check create false -c /opt/bin/tools/false -s test --handlers slack --baseURL http://backend1:8080 -i 10
+/opt/bin/sensu-cli handler create slack -t pipe --api-url http://backend1:8080 -c "/opt/bin/handlers/slack -w https://hooks.slack.com/services/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 -c '#sensu-spam'"
+/opt/bin/sensu-cli check create false -c /opt/bin/tools/false -s test --handlers slack --api-url http://backend1:8080 -i 10
 
 # wait for sensu-backend pid to finish so we don't kill the container
 # prematurely
