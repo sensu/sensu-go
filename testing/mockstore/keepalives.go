3e121c5514b0500ad9d9f26629560e16a1903799
Greg Poirier <greg.istehbest@gmail.com>
2017-07-17 15:48:42 -0700

    Add GetFailingKeepalives to MockStore

--- testing/mockstore/keepalives.go
+++ testing/mockstore/keepalives.go
@@ -1,11 +1,21 @@
 package mockstore
 
-import "context"
+import (
+	"context"
+
+	"github.com/sensu/sensu-go/types"
+)
 
 // GetKeepalive ...
 func (s *MockStore) GetKeepalive(ctx context.Context, entityID string) (int64, error) {
 	args := s.Called(ctx, entityID)
 	return args.Get(0).(int64), args.Error(1)
+}
+
+// GetFailingKeepalives ...
+func (s *MockStore) GetFailingKeepalives(ctx context.Context) ([]*types.KeepaliveRecord, error) {
+	args := s.Called(ctx)
+	return args.Get(0).([]*types.KeepaliveRecord), args.Error(1)
 }
 
 // UpdateKeepalive ...
