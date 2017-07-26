89963349e04006bee052f5757a873929999d960a
Justin Kolberg <amd.prophet@gmail.com>
2017-07-25 17:37:20 -0700

    add rpm support

--- Vagrantfile
+++ Vagrantfile
@@ -1,11 +1,12 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 Vagrant.configure("2") do |config|
-  config.vm.box = "ubuntu/trusty64"
-  config.vm.synced_folder ".", "/vagrant_data"
+  config.vm.box = "ubuntu/xenial64"
+  config.vm.synced_folder ".", "/sensu-go"
 
   config.vm.provision "shell", inline: <<-SHELL
-    apt-get install -y ruby ruby-dev build-essential
+    apt-get update
+    apt-get install -y ruby ruby-dev build-essential rpm rpmlint
     gem install --no-ri --no-rdoc fpm
     gem install --no-ri --no-rdoc pleaserun
   SHELL
--- package.sh
+++ package.sh
@@ -21,9 +21,13 @@ SERVICE_USER=sensu
 SERVICE_GROUP=sensu
 
 BINARY_NAME=sensu-agent
-BINARY_SOURCE_PATH=target/$TARGET_OS-$TARGET_ARCH/$BINARY_NAME
-BINARY_TARGET_PATH=/usr/local/bin/$BINARY_NAME
 BINARY_START_ARGS="start"
+
+# /usr/bin : linux
+# /usr/local/bin : macOS, freebsd, openbsd, solaris, maybe aix?
+# C:\Program Files\Sensu\sensu-agent\bin : windows (drive should be chooseable by user)
+BINARY_TARGET_PATH=/usr/bin/$BINARY_NAME
+BINARY_SOURCE_PATH=target/$TARGET_OS-$TARGET_ARCH/$BINARY_NAME
 
 # safe_rpm_version will return a version string that is rpm-compatible
 # e.g.
@@ -68,6 +72,9 @@ generate_service_files () {
     done
 }
 
+mkdir -p packages/sysvinit
+mkdir -p packages/systemd
+
 # deb - sysvinit
 fpm --input-type dir \
     --output-type deb \
@@ -75,7 +82,7 @@ fpm --input-type dir \
     --version $PACKAGE_VERSION \
     --iteration $PACKAGE_ITERATION \
     --architecture $PACKAGE_ARCH \
-    --package "${PACKAGE_NAME}_${PACKAGE_VERSION}-${PACKAGE_ITERATION}_${PACKAGE_ARCH}_sysvinit.deb" \
+    --package "packages/sysvinit/${PACKAGE_NAME}_${PACKAGE_VERSION}-${PACKAGE_ITERATION}_${PACKAGE_ARCH}.deb" \
     --description "$PACKAGE_DESCRIPTION" \
     --url "$PACKAGE_URL" \
     --license "$PACKAGE_LICENSE" \
@@ -87,14 +94,14 @@ fpm --input-type dir \
     --deb-default packaging/services/sysv/etc/default/$SERVICE_NAME \
     $BINARY_SOURCE_PATH=$BINARY_TARGET_PATH
 
-# deb - sysvinit
+# deb - systemd
 fpm --input-type dir \
     --output-type deb \
     --name $PACKAGE_NAME \
     --version $PACKAGE_VERSION \
     --iteration $PACKAGE_ITERATION \
     --architecture $PACKAGE_ARCH \
-    --package "${PACKAGE_NAME}_${PACKAGE_VERSION}-${PACKAGE_ITERATION}_${PACKAGE_ARCH}_systemd.deb" \
+    --package "packages/systemd/${PACKAGE_NAME}_${PACKAGE_VERSION}-${PACKAGE_ITERATION}_${PACKAGE_ARCH}.deb" \
     --description "$PACKAGE_DESCRIPTION" \
     --url "$PACKAGE_URL" \
     --license "$PACKAGE_LICENSE" \
@@ -105,3 +112,39 @@ fpm --input-type dir \
     --deb-systemd packaging/services/systemd/etc/systemd/system/$SERVICE_NAME.service \
     --deb-default packaging/services/systemd/etc/default/$SERVICE_NAME \
     $BINARY_SOURCE_PATH=$BINARY_TARGET_PATH
+
+#
+
+# rpm - sysvinit
+fpm --input-type dir \
+    --output-type rpm \
+    --name $PACKAGE_NAME \
+    --version $PACKAGE_VERSION \
+    --iteration $PACKAGE_ITERATION \
+    --architecture $PACKAGE_ARCH \
+    --package "packages/sysvinit/${PACKAGE_NAME}-${PACKAGE_VERSION}-${PACKAGE_ITERATION}.${PACKAGE_ARCH}.rpm" \
+    --description "$PACKAGE_DESCRIPTION" \
+    --url "$PACKAGE_URL" \
+    --license "$PACKAGE_LICENSE" \
+    --vendor "$PACKAGE_VENDOR" \
+    --maintainer "$PACKAGE_MAINTAINER" \
+    --rpm-init packaging/services/sysv/etc/init.d/$SERVICE_NAME \
+    $BINARY_SOURCE_PATH=$BINARY_TARGET_PATH \
+    packaging/services/sysv/etc/default/$SERVICE_NAME=/etc/default/
+
+# rpm - systemd
+fpm --input-type dir \
+    --output-type rpm \
+    --name $PACKAGE_NAME \
+    --version $PACKAGE_VERSION \
+    --iteration $PACKAGE_ITERATION \
+    --architecture $PACKAGE_ARCH \
+    --package "packages/systemd/${PACKAGE_NAME}-${PACKAGE_VERSION}-${PACKAGE_ITERATION}.${PACKAGE_ARCH}.rpm" \
+    --description "$PACKAGE_DESCRIPTION" \
+    --url "$PACKAGE_URL" \
+    --license "$PACKAGE_LICENSE" \
+    --vendor "$PACKAGE_VENDOR" \
+    --maintainer "$PACKAGE_MAINTAINER" \
+    $BINARY_SOURCE_PATH=$BINARY_TARGET_PATH \
+    packaging/services/systemd/etc/systemd/system/$SERVICE_NAME.service=/lib/systemd/system/ \
+    packaging/services/systemd/etc/default/$SERVICE_NAME=/etc/default/
