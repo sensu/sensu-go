504d6502e7405a85ba936dcbb71c126449bcaef5
James Phillips <jamesdphillips@gmail.com>
2017-08-02 22:07:44 -0700

    Update authorization (handlers controller)

---
+++ backend/authorization/handlers.go
@@ -0,0 +1,56 @@
+package authorization
+
+import (
+	"context"
+
+	"github.com/sensu/sensu-go/types"
+)
+
+// Handlers is global instance of HandlerPolicy
+var Handlers = HandlerPolicy{}
+
+// HandlerPolicy ...
+type HandlerPolicy struct {
+	context Context
+}
+
+// Resource this policy is associated with
+func (u *HandlerPolicy) Resource() string {
+	return types.RuleTypeHandler
+}
+
+// Context(ual) info this instance of the policy is associated with
+func (u *HandlerPolicy) Context() Context {
+	return u.context
+}
+
+// WithContext returns new policy populated with rules & organization.
+func (p HandlerPolicy) WithContext(ctx context.Context) HandlerPolicy {
+	p.context = ExtractValueFromContext(ctx)
+	return p
+}
+
+// CanList returns true if actor has read access to resource.
+func (p *HandlerPolicy) CanList() bool {
+	return canPerform(p, types.RulePermRead)
+}
+
+// CanRead returns true if actor has read access to resource.
+func (p *HandlerPolicy) CanRead(handler *types.Handler) bool {
+	return canPerformOn(p, handler.Organization, types.RulePermRead)
+}
+
+// CanCreate returns true if actor has access to create.
+func (p *HandlerPolicy) CanCreate() bool {
+	return canPerform(p, types.RulePermCreate)
+}
+
+// CanUpdate returns true if actor has access to update.
+func (p *HandlerPolicy) CanUpdate() bool {
+	return canPerform(p, types.RulePermUpdate)
+}
+
+// CanDelete returns true if actor has access to delete.
+func (p *HandlerPolicy) CanDelete() bool {
+	return canPerform(p, types.RulePermDelete)
+}
