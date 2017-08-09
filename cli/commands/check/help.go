8e11729a713b8e0c5b6efba5a50e70ccc9c0fed0
James Phillips <jamesdphillips@gmail.com>
2017-08-09 11:49:03 -0700

    Add command to display details of a specified check

--- cli/commands/check/help.go
+++ cli/commands/check/help.go
@@ -15,6 +15,7 @@ func HelpCommand(cli *cli.SensuCli) *cobra.Command {
 	// Add sub-commands
 	cmd.AddCommand(
 		ListCommand(cli),
+		ShowCommand(cli),
 		CreateCommand(cli),
 		ImportCommand(cli),
 		DeleteCommand(cli),
