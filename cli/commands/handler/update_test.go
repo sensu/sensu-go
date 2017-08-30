6012ec0887163ccbabe8870a68c761e7d4f3034a
James Phillips <jamesdphillips@gmail.com>
2017-08-30 10:37:37 -0700

    Add tests for check, handler, env & org update commands

---
+++ cli/commands/handler/update_test.go
@@ -0,0 +1,64 @@
+package handler
+
+import (
+	"fmt"
+	"testing"
+
+	client "github.com/sensu/sensu-go/cli/client/testing"
+	test "github.com/sensu/sensu-go/cli/commands/testing"
+	"github.com/sensu/sensu-go/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestUpdateCommand(t *testing.T) {
+	testCases := []struct {
+		args           []string
+		fetchResponse  error
+		updateResponse error
+		expectedOutput string
+		expectError    bool
+	}{
+		{[]string{}, nil, nil, "Usage", false},
+		{[]string{"foo"}, fmt.Errorf("error"), nil, "", true},
+		{[]string{"foo"}, nil, fmt.Errorf("error"), "", true},
+		{[]string{"foo"}, nil, nil, "OK", false},
+	}
+
+	for _, tc := range testCases {
+		name := ""
+		if len(tc.args) > 0 {
+			name = tc.args[0]
+		}
+
+		testName := fmt.Sprintf(
+			"update the handler %s",
+			name,
+		)
+		t.Run(testName, func(t *testing.T) {
+			handler := types.FixtureHandler(name)
+			cli := test.NewMockCLI()
+
+			client := cli.Client.(*client.MockClient)
+			client.On(
+				"FetchHandler",
+				name,
+			).Return(handler, tc.fetchResponse)
+
+			client.On(
+				"CreateHandler",
+				mock.Anything,
+			).Return(tc.updateResponse)
+
+			cmd := UpdateCommand(cli)
+			out, err := test.RunCmd(cmd, tc.args)
+
+			assert.Regexp(t, tc.expectedOutput, out)
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
