7f952c1eabe5661e5b0d42b649048a98de9ed290
James Phillips <jamesdphillips@gmail.com>
2017-10-02 18:38:36 -0700

    ✨  add prettier for automatic formatting of JS
    - For those Gophers out there this is relatively equivalent to `gofmt`.
    - Chose to use double-quotes so that styles are more congruent with our Go code
    - Chose to use trailing commas to again match our Go code
    
    Make JS more like Go 🎉

--- backend/dashboardd/src/App.js
+++ backend/dashboardd/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.png';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.png";
+import "./App.css";
 
 class App extends Component {
   render() {
