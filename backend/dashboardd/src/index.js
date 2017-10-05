62fa81ba09cede571676fea9392fd8e6a376aa14
James Phillips <jamesdphillips@gmail.com>
2017-10-05 11:47:16 -0700

    Add routing library 'found'

--- backend/dashboardd/src/index.js
+++ backend/dashboardd/src/index.js
@@ -1,8 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Resolver } from 'found-relay';
+import BrowserProtocol from 'farce/lib/BrowserProtocol';
+import queryMiddleware from 'farce/lib/queryMiddleware';
+import createFarceRouter from 'found/lib/createFarceRouter';
+import createRender from 'found/lib/createRender';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import routes from './routes';
+import registerServiceWorker from './registerServiceWorker';
+import environment from './environment';
+
+import './index.css';
+
+const Router = createFarceRouter({
+  historyProtocol: new BrowserProtocol(),
+  historyMiddlewares: [queryMiddleware],
+  routeConfig: routes,
+  render: createRender({}),
+});
+
+// Register React Tap event plugin
+injectTapEventPlugin();
+
+// Renderer
+ReactDOM.render(
+  <Router resolver={new Resolver(environment)} />,
+  document.getElementById('root'),
+);
+
+// Register service workers
 registerServiceWorker();
