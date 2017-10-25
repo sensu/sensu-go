1d234756f662929edafff55784b31fe383ed29c0
Justin Kolberg <amd.prophet@gmail.com>
2017-10-25 16:37:43 -0700

    add event filters to pipelined

--- backend/pipelined/filter_test.go
+++ backend/pipelined/filter_test.go
@@ -11,78 +11,181 @@ import (
 func TestPipelinedFilter(t *testing.T) {
 	p := &Pipelined{}
 
-	handler := &types.Handler{
-		Type:    "pipe",
-		Command: "cat",
+	testCases := []struct {
+		name     string
+		status   int
+		metrics  *types.Metrics
+		filters  []types.EventFilter
+		expected bool
+	}{
+		{
+			name:     "Not Incident",
+			status:   0,
+			metrics:  nil,
+			filters:  nil,
+			expected: true,
+		},
+		{
+			name:     "Incident",
+			status:   1,
+			metrics:  nil,
+			filters:  nil,
+			expected: false,
+		},
+		{
+			name:     "Metrics OK Status",
+			status:   0,
+			metrics:  &types.Metrics{},
+			filters:  nil,
+			expected: false,
+		},
+		{
+			name:     "Metrics Warning Status",
+			status:   1,
+			metrics:  &types.Metrics{},
+			filters:  nil,
+			expected: false,
+		},
+		{
+			name:    "Allow Filter With No Match",
+			status:  1,
+			metrics: nil,
+			filters: []types.EventFilter{
+				{
+					Name:       "filter",
+					Action:     types.EventFilterActionAllow,
+					Statements: []string{`event.Check.Output == "bar"`},
+				},
+			},
+			expected: true,
+		},
+		{
+			name:    "Allow Filter With Match",
+			status:  1,
+			metrics: nil,
+			filters: []types.EventFilter{
+				{
+					Name:       "filter",
+					Action:     types.EventFilterActionAllow,
+					Statements: []string{`event.Check.Output == "foo"`},
+				},
+			},
+			expected: false,
+		},
+		{
+			name:    "Deny Filter With No Match",
+			status:  1,
+			metrics: nil,
+			filters: []types.EventFilter{
+				{
+					Name:       "filter",
+					Action:     types.EventFilterActionDeny,
+					Statements: []string{`event.Check.Output == "bar"`},
+				},
+			},
+			expected: false,
+		},
+		{
+			name:    "Deny Filter With Match",
+			status:  1,
+			metrics: nil,
+			filters: []types.EventFilter{
+				{
+					Name:       "filter",
+					Action:     types.EventFilterActionDeny,
+					Statements: []string{`event.Check.Output == "foo"`},
+				},
+			},
+			expected: true,
+		},
 	}
 
-	event := &types.Event{
-		Check: &types.Check{},
+	for _, tc := range testCases {
+		handler := &types.Handler{
+			Type:    "pipe",
+			Command: "cat",
+			Filters: tc.filters,
+		}
+
+		t.Run(tc.name, func(t *testing.T) {
+			event := &types.Event{
+				Check: &types.Check{
+					Status: tc.status,
+					Output: "foo",
+				},
+				Metrics: tc.metrics,
+			}
+			filtered := p.filterEvent(handler, event)
+			assert.Equal(t, tc.expected, filtered)
+		})
 	}
-
-	event.Check.Status = 0
-
-	notIncident := p.filterEvent(handler, event)
-
-	assert.Equal(t, true, notIncident)
-
-	event.Check.Status = 1
-
-	incident := p.filterEvent(handler, event)
-
-	assert.Equal(t, false, incident)
-
-	event.Check.Status = 0
-
-	noMetrics := p.filterEvent(handler, event)
-
-	assert.Equal(t, true, noMetrics)
-
-	event.Metrics = &types.Metrics{}
-
-	metrics := p.filterEvent(handler, event)
-
-	assert.Equal(t, false, metrics)
-
-	event.Check.Status = 1
-
-	both := p.filterEvent(handler, event)
-
-	assert.Equal(t, false, both)
 }
 
 func TestPipelinedIsIncident(t *testing.T) {
 	p := &Pipelined{}
 
-	event := &types.Event{
-		Check: &types.Check{},
+	testCases := []struct {
+		name     string
+		status   int
+		expected bool
+	}{
+		{
+			name:     "OK Status",
+			status:   0,
+			expected: false,
+		},
+		{
+			name:     "Non-zero Status",
+			status:   1,
+			expected: true,
+		},
+		{
+			name:     "Negative Status",
+			status:   -1,
+			expected: true,
+		},
 	}
 
-	notIncident := p.isIncident(event)
-
-	assert.Equal(t, false, notIncident)
-
-	event.Check.Status = 1
-
-	incident := p.isIncident(event)
-
-	assert.Equal(t, true, incident)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			event := &types.Event{
+				Check: &types.Check{
+					Status: tc.status,
+				},
+			}
+			incident := p.isIncident(event)
+			assert.Equal(t, tc.expected, incident)
+		})
+	}
 }
 
 func TestPipelinedHasMetrics(t *testing.T) {
 	p := &Pipelined{}
 
-	event := &types.Event{
-		Check: &types.Check{},
+	testCases := []struct {
+		name     string
+		metrics  *types.Metrics
+		expected bool
+	}{
+		{
+			name:     "Not Metrics",
+			metrics:  nil,
+			expected: false,
+		},
+		{
+			name:     "Metrics",
+			metrics:  &types.Metrics{},
+			expected: true,
+		},
 	}
 
-	notMetrics := p.hasMetrics(event)
-
-	assert.Equal(t, false, notMetrics)
-
-	event.Metrics = &types.Metrics{}
-
-	metrics := p.hasMetrics(event)
-
-	assert.Equal(t, true, metrics)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			event := &types.Event{
+				Metrics: tc.metrics,
+			}
+			metrics := p.hasMetrics(event)
+			assert.Equal(t, tc.expected, metrics)
+		})
+	}
 }
