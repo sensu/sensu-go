526c8f6d8e0882c9530357ecc09e7a8669cf1b72
James Phillips <jamesdphillips@gmail.com>
2017-10-30 17:18:19 -0700

    Add script for generating protobuf types

---
+++ scripts/genproto.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+# Generate all protobuf bindings.
+# Run from repository root.
+#
+# Following is largely borrowed from coreos/etcd/scripts/genproto.sh rev. 9abe9da
+#
+set -e
+
+if ! [[ "$0" =~ scripts/genproto.sh ]]; then
+	echo "must be run from repository root"
+	exit 255
+fi
+
+if [[ $(protoc --version | cut -f2 -d' ') != "3.4.0" ]]; then
+	echo "could not find protoc 3.4.0, is it installed + in PATH?"
+	exit 255
+fi
+
+# directories containing protos to be built
+DIRS="./types"
+
+# exact version of packages to build
+# TODO: possibly grab from Gopkg.toml
+GOGO_PROTO_SHA="117892bf1866fbaa2318c03e50e40564c8845457"
+
+# export paths
+SENSU_ROOT="${GOPATH}/src/github.com/sensu/sensu-go"
+GOGOPROTO_ROOT="${GOPATH}/src/github.com/gogo/protobuf"
+GOGOPROTO_PATH="${GOGOPROTO_ROOT}:${GOGOPROTO_ROOT}/protobuf"
+
+# install gogoproto dep
+go get -u github.com/gogo/protobuf/{proto,protoc-gen-gogo,gogoproto}
+go get -u golang.org/x/tools/cmd/goimports
+pushd "${GOGOPROTO_ROOT}"
+  git reset --hard "${GOGO_PROTO_SHA}"
+  make install
+popd
+
+for dir in ${DIRS}; do
+  pushd "$dir"
+    protoc --gofast_out=plugins:. -I="$SENSU_ROOT/vendor/:./" ./*.proto
+    goimports -w ./*.pb.go
+  popd
+done
