abd6556e03c3e5a90277113e5745b42e3a92207d
Eric Chlebek <eric@sensu.io>
2017-11-09 15:08:49 -0800

    Add MutatorsRouter.
    Add a MutatorsRouter, which is similar in nature to the other
    routers in the routers package.
    
    Refs #526

---
+++ backend/apid/routers/mutators.go
@@ -0,0 +1,66 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sensu/sensu-go/backend/apid/actions"
+	"github.com/sensu/sensu-go/backend/store"
+	"github.com/sensu/sensu-go/types"
+)
+
+type MutatorsRouter struct {
+	controller actions.MutatorController
+}
+
+func NewMutatorsRouter(store store.MutatorStore) MutatorsRouter {
+	return MutatorsRouter{
+		controller: actions.NewMutatorController(store),
+	}
+}
+
+// Mount the MutatorsRouter to a parent Router
+func (r *MutatorsRouter) Mount(parent *mux.Router) {
+	routes := resourceRoute{router: parent, pathPrefix: "/mutators"}
+	routes.index(r.list)
+	routes.show(r.find)
+	routes.create(r.create)
+	routes.update(r.update)
+	routes.destroy(r.destroy)
+}
+
+func (r *MutatorsRouter) list(req *http.Request) (interface{}, error) {
+	params := actions.QueryParams(mux.Vars(req))
+	return r.controller.Query(req.Context(), params)
+}
+
+func (r *MutatorsRouter) find(req *http.Request) (interface{}, error) {
+	params := actions.QueryParams(mux.Vars(req))
+	return r.controller.Find(req.Context(), params)
+}
+
+func (r *MutatorsRouter) create(req *http.Request) (interface{}, error) {
+	mut := types.Mutator{}
+	if err := unmarshalBody(req, &mut); err != nil {
+		return nil, err
+	}
+
+	err := r.controller.Create(req.Context(), mut)
+	return mut, err
+}
+
+func (r *MutatorsRouter) update(req *http.Request) (interface{}, error) {
+	mut := types.Mutator{}
+	if err := unmarshalBody(req, &mut); err != nil {
+		return nil, err
+	}
+
+	err := r.controller.Update(req.Context(), mut)
+	return mut, err
+}
+
+func (r *MutatorsRouter) destroy(req *http.Request) (interface{}, error) {
+	params := actions.QueryParams(mux.Vars(req))
+	err := r.controller.Destroy(req.Context(), params)
+	return nil, err
+}
