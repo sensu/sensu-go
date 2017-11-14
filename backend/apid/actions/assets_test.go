b1a1ef4e8bc92fdc352502436715e43ae6a7c850
Nikki Attea <nikki@sensu.io>
2017-11-14 09:16:08 -0600

    Add assets tests

---
+++ backend/apid/actions/assets_test.go
@@ -0,0 +1,404 @@
+package actions
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/sensu/sensu-go/testing/mockstore"
+	"github.com/sensu/sensu-go/testing/testutil"
+	"github.com/sensu/sensu-go/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestNewAssetController(t *testing.T) {
+	assert := assert.New(t)
+
+	store := &mockstore.MockStore{}
+	actions := NewAssetController(store)
+
+	assert.NotNil(actions)
+	assert.Equal(store, actions.Store)
+	assert.NotNil(actions.Policy)
+}
+
+func TestAssetQuery(t *testing.T) {
+	defaultCtx := testutil.NewContext(
+		testutil.ContextWithOrgEnv("default", "default"),
+		testutil.ContextWithRules(
+			types.FixtureRuleWithPerms(types.RuleTypeAsset, types.RulePermRead),
+		),
+	)
+
+	testCases := []struct {
+		name        string
+		ctx         context.Context
+		records     []*types.Asset
+		params      QueryParams
+		expectedLen int
+		storeErr    error
+		expectedErr error
+	}{
+		{
+			name:        "No Assets",
+			ctx:         defaultCtx,
+			records:     []*types.Asset{},
+			params:      QueryParams{},
+			expectedLen: 0,
+			storeErr:    nil,
+			expectedErr: nil,
+		},
+		{
+			name: "With Assets",
+			ctx:  defaultCtx,
+			records: []*types.Asset{
+				types.FixtureAsset("asset1"),
+				types.FixtureAsset("asset2"),
+			},
+			params:      QueryParams{},
+			expectedLen: 2,
+			storeErr:    nil,
+			expectedErr: nil,
+		},
+		{
+			name: "With Only Create Access",
+			ctx: testutil.NewContext(testutil.ContextWithRules(
+				types.FixtureRuleWithPerms(types.RuleTypeAsset, types.RulePermCreate),
+			)),
+			records: []*types.Asset{
+				types.FixtureAsset("asset1"),
+				types.FixtureAsset("asset2"),
+			},
+			params:      QueryParams{},
+			expectedLen: 0,
+			storeErr:    nil,
+		},
+		{
+			name:        "Store Failure",
+			ctx:         defaultCtx,
+			records:     nil,
+			params:      QueryParams{},
+			expectedLen: 0,
+			storeErr:    errors.New(""),
+			expectedErr: NewError(InternalErr, errors.New("")),
+		},
+	}
+
+	for _, tc := range testCases {
+		store := &mockstore.MockStore{}
+		actions := NewAssetController(store)
+
+		t.Run(tc.name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			// Mock store methods
+			store.On("GetAssets", tc.ctx).Return(tc.records, tc.storeErr)
+
+			// Exec Query
+			results, err := actions.Query(tc.ctx, tc.params)
+
+			// Assert
+			assert.EqualValues(tc.expectedErr, err)
+			assert.Len(results, tc.expectedLen)
+		})
+	}
+}
+
+func TestAssetFind(t *testing.T) {
+	defaultCtx := testutil.NewContext(
+		testutil.ContextWithOrgEnv("default", "default"),
+		testutil.ContextWithRules(
+			types.FixtureRuleWithPerms(types.RuleTypeAsset, types.RulePermRead),
+		),
+	)
+
+	testCases := []struct {
+		name            string
+		ctx             context.Context
+		record          *types.Asset
+		argument        string
+		expected        bool
+		expectedErrCode ErrCode
+		params          QueryParams
+	}{
+		{
+			name:            "No name given",
+			ctx:             defaultCtx,
+			argument:        "",
+			expected:        false,
+			expectedErrCode: NotFound,
+			params:          QueryParams{},
+		},
+		{
+			name:            "Found",
+			ctx:             defaultCtx,
+			record:          types.FixtureAsset("asset1"),
+			argument:        "asset1",
+			expected:        true,
+			expectedErrCode: 0,
+			params:          QueryParams{},
+		},
+		{
+			name:            "Not Found",
+			ctx:             defaultCtx,
+			record:          nil,
+			argument:        "asset1",
+			expected:        false,
+			expectedErrCode: NotFound,
+			params:          QueryParams{},
+		},
+		{
+			name: "No Read Permission",
+			ctx: testutil.NewContext(testutil.ContextWithRules(
+				types.FixtureRuleWithPerms(types.RuleTypeAsset, types.RulePermCreate),
+			)),
+			record:          types.FixtureAsset("asset1"),
+			argument:        "asset1",
+			expected:        false,
+			expectedErrCode: NotFound,
+			params:          QueryParams{},
+		},
+	}
+
+	for _, tc := range testCases {
+		store := &mockstore.MockStore{}
+		actions := NewAssetController(store)
+
+		t.Run(tc.name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			// Mock store methods
+			store.
+				On("GetAssetByName", tc.ctx, mock.Anything, mock.Anything).
+				Return(tc.record, nil)
+
+			// Exec Query
+			result, err := actions.Find(tc.ctx, tc.params)
+
+			inferErr, ok := err.(Error)
+			if ok {
+				assert.Equal(tc.expectedErrCode, inferErr.Code)
+			} else {
+				assert.NoError(err)
+			}
+			assert.Equal(tc.expected, result != nil, "expects Find() to return a record")
+		})
+	}
+}
+
+func TestAssetCreate(t *testing.T) {
+	defaultCtx := testutil.NewContext(
+		testutil.ContextWithOrgEnv("default", "default"),
+		testutil.ContextWithRules(
+			types.FixtureRuleWithPerms(types.RuleTypeAsset, types.RulePermCreate),
+		),
+	)
+	wrongPermsCtx := testutil.NewContext(
+		testutil.ContextWithOrgEnv("default", "default"),
+		testutil.ContextWithRules(
+			types.FixtureRuleWithPerms(types.RuleTypeAsset, types.RulePermRead),
+		),
+	)
+
+	badAsset := types.FixtureAsset("asset1")
+	badAsset.Name = "!@#!#$@#^$%&$%&$&$%&%^*%&(%@###"
+
+	testCases := []struct {
+		name            string
+		ctx             context.Context
+		argument        *types.Asset
+		fetchResult     *types.Asset
+		fetchErr        error
+		createErr       error
+		expectedErr     bool
+		expectedErrCode ErrCode
+	}{
+		{
+			name:        "Created",
+			ctx:         defaultCtx,
+			argument:    types.FixtureAsset("asset1"),
+			expectedErr: false,
+		},
+		{
+			name:            "Already Exists",
+			ctx:             defaultCtx,
+			argument:        types.FixtureAsset("asset1"),
+			fetchResult:     types.FixtureAsset("asset1"),
+			expectedErr:     true,
+			expectedErrCode: AlreadyExistsErr,
+		},
+		{
+			name:            "Store Err on Create",
+			ctx:             defaultCtx,
+			argument:        types.FixtureAsset("asset1"),
+			createErr:       errors.New("dunno"),
+			expectedErr:     true,
+			expectedErrCode: InternalErr,
+		},
+		{
+			name:            "Store Err on Fetch",
+			ctx:             defaultCtx,
+			argument:        types.FixtureAsset("asset1"),
+			fetchErr:        errors.New("dunno"),
+			expectedErr:     true,
+			expectedErrCode: InternalErr,
+		},
+		{
+			name:            "No Permission",
+			ctx:             wrongPermsCtx,
+			argument:        types.FixtureAsset("asset1"),
+			expectedErr:     true,
+			expectedErrCode: PermissionDenied,
+		},
+		{
+			name:            "Validation Error",
+			ctx:             defaultCtx,
+			argument:        badAsset,
+			expectedErr:     true,
+			expectedErrCode: InvalidArgument,
+		},
+	}
+
+	for _, tc := range testCases {
+		store := &mockstore.MockStore{}
+		actions := NewAssetController(store)
+
+		t.Run(tc.name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			// Mock store methods
+			store.
+				On("GetAssetByName", mock.Anything, mock.Anything).
+				Return(tc.fetchResult, tc.fetchErr)
+			store.
+				On("UpdateAsset", mock.Anything, mock.Anything).
+				Return(tc.createErr)
+
+			// Exec Query
+			err := actions.Create(tc.ctx, *tc.argument)
+
+			if tc.expectedErr {
+				inferErr, ok := err.(Error)
+				if ok {
+					assert.Equal(tc.expectedErrCode, inferErr.Code)
+				} else {
+					assert.Error(err)
+					assert.FailNow("Given was not of type 'Error'")
+				}
+			} else {
+				assert.NoError(err)
+			}
+		})
+	}
+}
+
+func TestAssetUpdate(t *testing.T) {
+	defaultCtx := testutil.NewContext(
+		testutil.ContextWithOrgEnv("default", "default"),
+		testutil.ContextWithRules(
+			types.FixtureRuleWithPerms(types.RuleTypeAsset, types.RulePermUpdate),
+		),
+	)
+	wrongPermsCtx := testutil.NewContext(
+		testutil.ContextWithOrgEnv("default", "default"),
+		testutil.ContextWithRules(
+			types.FixtureRuleWithPerms(types.RuleTypeAsset, types.RulePermRead),
+		),
+	)
+
+	badAsset := types.FixtureAsset("asset1")
+
+	testCases := []struct {
+		name            string
+		ctx             context.Context
+		argument        *types.Asset
+		fetchResult     *types.Asset
+		fetchErr        error
+		updateErr       error
+		expectedErr     bool
+		expectedErrCode ErrCode
+	}{
+		{
+			name:        "Updated",
+			ctx:         defaultCtx,
+			argument:    types.FixtureAsset("asset1"),
+			fetchResult: types.FixtureAsset("asset1"),
+			expectedErr: false,
+		},
+		{
+			name:            "Does Not Exist",
+			ctx:             defaultCtx,
+			argument:        types.FixtureAsset("asset1"),
+			fetchResult:     nil,
+			expectedErr:     true,
+			expectedErrCode: NotFound,
+		},
+		{
+			name:            "Store Err on Update",
+			ctx:             defaultCtx,
+			argument:        types.FixtureAsset("asset1"),
+			fetchResult:     types.FixtureAsset("asset1"),
+			updateErr:       errors.New("dunno"),
+			expectedErr:     true,
+			expectedErrCode: InternalErr,
+		},
+		{
+			name:            "Store Err on Fetch",
+			ctx:             defaultCtx,
+			argument:        types.FixtureAsset("asset1"),
+			fetchResult:     types.FixtureAsset("asset1"),
+			fetchErr:        errors.New("dunno"),
+			expectedErr:     true,
+			expectedErrCode: InternalErr,
+		},
+		{
+			name:            "No Permission",
+			ctx:             wrongPermsCtx,
+			argument:        types.FixtureAsset("asset1"),
+			fetchResult:     types.FixtureAsset("asset1"),
+			expectedErr:     true,
+			expectedErrCode: PermissionDenied,
+		},
+		{
+			name:            "Validation Error",
+			ctx:             defaultCtx,
+			argument:        badAsset,
+			fetchResult:     types.FixtureAsset("asset1"),
+			expectedErr:     true,
+			expectedErrCode: InvalidArgument,
+		},
+	}
+
+	for _, tc := range testCases {
+		store := &mockstore.MockStore{}
+		actions := NewAssetController(store)
+
+		t.Run(tc.name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			// Mock store methods
+			store.
+				On("GetAssetByName", mock.Anything, mock.Anything).
+				Return(tc.fetchResult, tc.fetchErr)
+			store.
+				On("UpdateAsset", mock.Anything, mock.Anything).
+				Return(tc.updateErr)
+
+			// Exec Query
+			err := actions.Update(tc.ctx, *tc.argument)
+
+			if tc.expectedErr {
+				inferErr, ok := err.(Error)
+				if ok {
+					assert.Equal(tc.expectedErrCode, inferErr.Code)
+				} else {
+					assert.Error(err)
+					assert.FailNow("Given was not of type 'Error'")
+				}
+			} else {
+				assert.NoError(err)
+			}
+		})
+	}
+}
