c0efbb43e7c4a6f52dba81e4397387673a7db901
Simon Plourde <simon@sensu.io>
2017-11-14 14:31:08 -0500

    Add HandlerRouter in routers pkg

---
+++ backend/apid/routers/handlers.go
@@ -0,0 +1,65 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sensu/sensu-go/backend/apid/actions"
+	"github.com/sensu/sensu-go/backend/store"
+	"github.com/sensu/sensu-go/types"
+)
+
+// HandlersRouter handles requests for /handlers
+type HandlersRouter struct {
+	controller actions.HandlerController
+}
+
+// NewHandlersRouter instantiates new router for controlling handler resources
+func NewHandlersRouter(store store.HandlerStore) *HandlersRouter {
+	return &HandlersRouter{
+		controller: actions.NewHandlerController(store),
+	}
+}
+
+// Mount the HandlersRouter to a parent Router
+func (r *HandlersRouter) Mount(parent *mux.Router) {
+	routes := resourceRoute{router: parent, pathPrefix: "/handlers"}
+	routes.create(r.create)
+	routes.destroy(r.destroy)
+	routes.index(r.list)
+	routes.show(r.find)
+	routes.update(r.update)
+
+}
+
+func (r *HandlersRouter) create(req *http.Request) (interface{}, error) {
+	handler := types.Handler{}
+	if err := unmarshalBody(req, &handler); err != nil {
+		return nil, err
+	}
+
+	return handler, r.controller.Create(req.Context(), handler)
+}
+
+func (r *HandlersRouter) destroy(req *http.Request) (interface{}, error) {
+	params := mux.Vars(req)
+	return nil, r.controller.Destroy(req.Context(), params["name"])
+}
+
+func (r *HandlersRouter) find(req *http.Request) (interface{}, error) {
+	params := mux.Vars(req)
+	return r.controller.Find(req.Context(), params["name"])
+}
+
+func (r *HandlersRouter) list(req *http.Request) (interface{}, error) {
+	return r.controller.Query(req.Context())
+}
+
+func (r *HandlersRouter) update(req *http.Request) (interface{}, error) {
+	handler := types.Handler{}
+	if err := unmarshalBody(req, &handler); err != nil {
+		return nil, err
+	}
+
+	return handler, r.controller.Update(req.Context(), handler)
+}
---
+++ backend/apid/routers/handlers_test.go
@@ -0,0 +1,7 @@
+package routers
+
+import "testing"
+
+func TestHttpApiHandlersGet(t *testing.T) {
+
+}
