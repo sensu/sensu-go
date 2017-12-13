6473f9e25138b8c08d35eae6bc6f86ee8d7c9673
Simon Plourde <simon@sensu.io>
2017-12-13 14:43:37 -0500

    Optimize linting and unit tests in build.ps1
    Signed-off-by: Simon Plourde <simon@sensu.io>

--- build.ps1
+++ build.ps1
@@ -5,7 +5,7 @@ param (
 $REPO_PATH = "github.com/sensu/sensu-go"
 
 # source in the environment variables from `go env`
-  $env_commands = go env
+$env_commands = go env
 ForEach ($env_cmd in $env_commands) {
     $env_str = $env_cmd -replace "set " -replace ""
     $env = $env_str.Split("=")
@@ -18,7 +18,7 @@ $RACE = ""
 function set_race_flag
 {
     If ($env:GOARCH -eq "amd64") {
-	$RACE = "-race"
+        $RACE = "-race"
     }
 }
 
@@ -46,8 +46,8 @@ function build_tool_binary([string]$goos, [string]$goarch, [string]$bin, [string
     $env:GOARCH = $goarch
     go build -i -o $outfile "$REPO_PATH/$subdir/$bin/..."
     If ($LASTEXITCODE -ne 0) {
-	echo "Failed to build $outfile..."
-	exit 1
+        echo "Failed to build $outfile..."
+        exit 1
     }
 
     return $outfile
@@ -57,15 +57,15 @@ function cmd_name_map([string]$cmd)
 {
     switch ($cmd)
     {
-	"backend" {
-	    return "sensu-backend"
-	}
-	"agent" {
-	    return "sensu-agent"
-	}
-	"cli" {
-	    return "sensuctl"
-	}
+        "backend" {
+            return "sensu-backend"
+        }
+        "agent" {
+            return "sensu-agent"
+        }
+        "cli" {
+            return "sensuctl"
+        }
     }
 }
 
@@ -88,8 +88,8 @@ function build_binary([string]$goos, [string]$goarch, [string]$bin, [string]$cmd
 
     go build -ldflags "$ldflags" -i -o $outfile "$REPO_PATH/$bin/cmd/..."
     If ($LASTEXITCODE -ne 0) {
-	echo "Failed to build $outfile..."
-	exit 1
+        echo "Failed to build $outfile..."
+        exit 1
     }
 
     return $outfile
@@ -100,18 +100,18 @@ function build_tools
     echo "Running tool & plugin builds..."
 
     ForEach ($bin in "cat","false","sleep","true") {
-	build_tool $bin "tools"
+        build_tool $bin "tools"
     }
 
     ForEach ($bin in "slack") {
-	build_tool $bin "handlers"
+        build_tool $bin "handlers"
     }
 }
 
 function build_tool([string]$bin, [string]$subdir)
 {
     If (!(Test-Path -Path "bin/$subdir")) {
-	New-Item -ItemType directory -Path "bin/$subdir" | out-null
+        New-Item -ItemType directory -Path "bin/$subdir" | out-null
     }
 
     echo "Building $subdir/$bin for $env:GOOS-$env:GOARCH"
@@ -125,7 +125,7 @@ function build_commands
     echo "Running build..."
 
     ForEach ($bin in "agent","backend","cli") {
-	build_command $bin
+        build_command $bin
     }
 }
 
@@ -134,7 +134,7 @@ function build_command([string]$bin)
     $cmd_name = cmd_name_map $bin
 
     If (!(Test-Path -Path "bin")) {
-	New-Item -ItemType directory -Path "bin" | out-null
+        New-Item -ItemType directory -Path "bin" | out-null
     }
 
     echo "Building $bin for $env:GOOS-$env:GOARCH"
@@ -149,8 +149,14 @@ function linter_commands
 
     gometalinter.v1 --vendor --disable-all --enable=vet --linter='vet:go tool vet -composites=false {paths}:PATH:LINE:MESSAGE' --enable=golint --enable=ineffassign --enable=goconst --tests ./...
     If ($LASTEXITCODE -ne 0) {
-	echo "Linting failed..."
-	exit 1
+        echo "Linting failed..."
+        exit 1
+    }
+
+    errcheck $(go list ./... | Select-String -pattern "dashboardd", "cli/commands/importer", "agent/assetmanager" -notMatch)
+    If ($LASTEXITCODE -ne 0) {
+        echo "Linting failed..."
+        exit 1
     }
 }
 
@@ -158,19 +164,11 @@ function test_commands
 {
     echo "Running tests..."
 
-    $failed = 0
-    echo "" > "coverage.txt"
-    $packages = go list ./... | Select-String -pattern "testing", "vendor" -notMatch
-    ForEach ($pkg in $packages) {
-	go test -timeout=60s -v -coverprofile="profile.out" -covermode=atomic $pkg
-	If ($LASTEXITCODE -ne 0) {
-	    echo "Testing failed..."
-	    exit 1
-	}
-	If (Test-Path "profile.out") {
-	    cat "profile.out" >> "coverage.txt"
-	    rm "profile.out"
-	}
+
+    go test -timeout=60s -v $(go list ./... | Select-String -pattern "testing", "vendor" -notMatch)
+    If ($LASTEXITCODE -ne 0) {
+        echo "Testing failed..."
+        exit 1
     }
 }
 
@@ -180,8 +178,8 @@ function e2e_commands
 
     go test -v $REPO_PATH/testing/e2e
     If ($LASTEXITCODE -ne 0) {
-	echo "e2e testing failed..."
-	exit 1
+        echo "e2e testing failed..."
+        exit 1
     }
 }
 
