d58bb95645b901696f70fdac0be6bc9ec1229b14
Nikki Attea <nikki@sensu.io>
2017-12-14 16:05:45 -0600

    Implement backwards compatible TCP/UDP socket, now accepts 1.x CheckResult
    Signed-off-by: Nikki Attea <nikki@sensu.io>

--- agent/event.go
+++ agent/event.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/sensu/sensu-go/types"
+	"github.com/sensu/sensu-go/types/v1"
 )
 
 // prepareEvent accepts a partial or complete event and tries to add any missing
@@ -49,4 +50,46 @@ func prepareEvent(a *Agent, event *types.Event) error {
 
 	// The entity should pass validation at this point
 	return event.Entity.Validate()
+}
+
+// translateToEvent accepts a 1.x compatible check result
+// and attempts to translate it to a 2.x event
+func translateToEvent(a *Agent, result v1.CheckResult, event *types.Event) error {
+	var checkConfig types.CheckConfig
+	var check types.Check
+
+	if result.Name == "" {
+		return fmt.Errorf("a check name must be provided")
+	}
+
+	if result.Output == "" {
+		return fmt.Errorf("a check output must be provided")
+	}
+
+	agentEntity := a.getAgentEntity()
+	if result.Client == "" || result.Client == agentEntity.ID {
+		event.Entity = agentEntity
+	} else {
+		event.Entity = &types.Entity{
+			ID:    result.Client,
+			Class: types.EntityProxyClass,
+		}
+	}
+
+	check.Status = result.Status
+	checkConfig.Command = result.Command
+	checkConfig.Subscriptions = result.Subscribers
+	checkConfig.Interval = result.Interval
+	checkConfig.Name = result.Name
+	check.Issued = result.Issued
+	check.Executed = result.Executed
+	check.Duration = result.Duration
+	check.Output = result.Output
+	checkConfig.SetExtendedAttributes(result.GetExtendedAttributes())
+
+	// add config and check values to the 2.x event
+	check.Config = &checkConfig
+	event.Check = &check
+
+	return nil
 }
