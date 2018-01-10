e0f35919abe414f8f4a9522e38a391c8ea96128f
James Phillips <jamesdphillips@gmail.com>
2018-01-09 22:34:37 -0800

    [GraphQL] Schema description generator
    TODO
    
    - [-] Remove generated interfaces from all types (except object.)
    - [-] Add interfaces for resolving types and scalars. (Replaces generated interfaces.)
    - [-] Generate Register method for each generated type.
    - [-] Implement Register[Type] methods on Service.
    - [x] Combine "service" and "generator/util" package. Move to project root; possibly name it `graphql`.
    - [ ] Generate interface for each field resolver.
    - [ ] Generate one interface that includes all fields. Possibly called `[ObjName]FieldsResolver`(?)
    - [ ] Implement Do method on Service.
    
    Re-implemented Types
    
    - [x] Enum
    - [x] Input
    - [x] Interface
    - [ ] Object
    - [x] Scalar
    - [x] Schema
    - [x] Union
    
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- graphql/generator/schema.go
+++ graphql/generator/schema.go
@@ -7,43 +7,136 @@ import (
 	"github.com/jamesdphillips/graphql/language/ast"
 )
 
+//
+// Generates description for schema
+//
+// == Example input SDL
+//
+//   schema {
+//     query:    QueryType
+//     mutation: MutationType
+//     subscription: SubscriptionType
+//   }
+//
+// == Example output
+//
+//   // Schema supplies the root types of each type of operation, query,
+//   // mutation (optional), and subscription (optional).
+//   var Schema = graphql.NewType("Schema", graphql.SchemaKind)
+//
+//   // RegisterSchema registers schema description with given service.
+//   func RegisterSchema(svc graphql.Service) {
+//     svc.RegisterSchema(_SchemaDesc)
+//   }
+//
+//   // define configuration thunk
+//   func _SchemaConfigFn() graphql.SchemaConfig {
+//     return graphql.SchemaConfig{
+//       Query:        &graphql.Object{PrivateName: "QueryType"},
+//       Mutation:     &graphql.Object{PrivateName: "MutationType"},
+//     }
+//   }
+//
+//   // describe schema's configuration; keep private to avoid
+//   // unintentional tampering at runtime.
+//   func _SchemaDesc = graphql.SchemaDesc{
+//     Config: _SchemaConfigFn,
+//   }
+//
 func genSchema(node *ast.SchemaDefinition) jen.Code {
 	code := newGroup()
 
+	// Ids
+	registerFnName := "RegisterSchema"
+	publicRefName := "Schema"
+	privateConfigName := "_SchemaDesc"
+	privateConfigThunkName := "_SchemaConfigFn"
+
 	//
-	// Generates thunk that returns new instance of schema config
+	// Generate record that will be used to refer to the schema
 	//
-	//  == Example input SDL
+	// == Example output
 	//
-	//    schema {
-	//      query:    QueryType
-	//      mutation: MutationType
+	//   // Schema supplies the root types of each type of operation, query,
+	//   // mutation (optional), and subscription (optional).
+	//   var Schema = graphql.NewType("Schema", graphql.SchemaKind)
 	//
-	//      // TODO: To be implemented.
-	//      // subscription: SubscriptionType
-	//    }
+	code.Comment(`// Schema supplies the root types of each type of operation, query,
+// mutation (optional), and subscription (optional).`)
+	code.
+		Var().Id(publicRefName).Op("=").
+		Qual(servicePkg, "NewType").
+		Call(jen.Lit("Schema"), jen.Qual(servicePkg, "SchemaKind"))
+
 	//
-	//  == Example output
+	// Generate public func to register type with service
 	//
-	//    // Schema exposes the root types for each operation.
-	//    func Schema() graphql.SchemaConfig {
-	//      return graphql.SchemaConfig{
-	//        Query:        &graphql.Object{PrivateName: "QueryType"},
-	//        Mutation:     &graphql.Object{PrivateName: "MutationType"},
-	//      }
-	//    }
+	// == Example output
 	//
-	code.Comment("Schema exposes the root types for each operation.")
-	code.Func().Id("Schema").Params().Qual(defsPkg, "SchemaConfig").Block(
-		jen.Return(jen.Qual(defsPkg, "SchemaConfig").Values(
-			jen.DictFunc(func(d jen.Dict) {
-				for _, op := range node.OperationTypes {
-					opName := strings.Title(op.Operation)
-					d[jen.Id(opName)] = genMockObjectReference(op.Type)
-				}
-			}),
-		)),
+	//   // RegisterSchema registers schema description with given service.
+	//   func RegisterSchema(svc graphql.Service) {
+	//     svc.RegisterSchema(_SchemaDesc)
+	//   }
+	//
+	code.Comment(
+		"RegisterSchema registers schema description with given service.",
 	)
+	code.
+		Func().Id(registerFnName).
+		Params(jen.Id("svc").Qual(servicePkg, "Service")).
+		Block(
+			jen.Id("svc.RegisterSchema").Call(
+				jen.Id(privateConfigName),
+			),
+		)
+
+	//
+	// Generate type config thunk
+	//
+	// == Example output
+	//
+	//   // define configuration thunk
+	//   func _SchemaConfigFn() graphql.SchemaConfig {
+	//     return graphql.SchemaConfig{
+	//       Query:        &graphql.Object{PrivateName: "QueryType"},
+	//       Mutation:     &graphql.Object{PrivateName: "MutationType"},
+	//     }
+	//   }
+	//
+	code.
+		Func().Id(privateConfigThunkName).
+		Params().Qual(defsPkg, "SchemaConfig").
+		Block(
+			jen.Return(jen.Qual(defsPkg, "SchemaConfig").Values(
+				jen.DictFunc(func(d jen.Dict) {
+					for _, op := range node.OperationTypes {
+						opName := strings.Title(op.Operation)
+						d[jen.Id(opName)] = genMockObjectReference(op.Type)
+					}
+				}),
+			)),
+		)
+
+	//
+	// Generate type description
+	//
+	// == Example output
+	//
+	//   // describe schema's configuration; keep private to avoid
+	//   // unintentional tampering at runtime.
+	//   func _SchemaDesc = graphql.SchemaDesc{
+	//     Config: _SchemaConfigFn,
+	//   }
+	//
+	code.Comment(
+		`describe schema's configuration; kept private to avoid unintentional tampering of configuration at runtime.`,
+	)
+	code.
+		Var().Id(privateConfigName).Op("=").
+		Qual(servicePkg, "SchemaDesc").
+		Values(jen.Dict{
+			jen.Id("Config"): jen.Id(privateConfigThunkName),
+		})
 
 	return code
 }
