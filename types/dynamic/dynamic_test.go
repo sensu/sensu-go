86be1c05ac7e41c8ff7add60de9ec5d3a068a4d2
Simon Plourde <simon@sensu.io>
2018-01-18 16:19:35 -0500

    Fix TestGetJSONStructField test
    Signed-off-by: Simon Plourde <simon@sensu.io>

--- types/dynamic/dynamic_test.go
+++ types/dynamic/dynamic_test.go
@@ -67,41 +67,6 @@ func TestGetStructField(t *testing.T) {
 	assert.Equal(field.Value.Interface(), 10)
 }
 
-func TestGetJSONStructField(t *testing.T) {
-	assert := assert.New(t)
-	require := require.New(t)
-
-	test := struct {
-		Valid             int `json:"valid"`
-		invalidUnexported int
-		ValidEmpty        int `json:"validEmpty"`
-		InvalidEmpty      int `json:"invalidEmpty,omitempty"`
-		Invalid           int `json:"-"`
-		Attributes        []byte
-	}{
-		Valid:             5,
-		invalidUnexported: 1,
-		ValidEmpty:        0,
-		InvalidEmpty:      0,
-		Invalid:           10,
-		Attributes:        []byte(`"hello!"`),
-	}
-
-	fields := getJSONFields(reflect.ValueOf(test), &test.Attributes[0])
-	require.Equal(2, len(fields))
-
-	field, ok := fields["valid"]
-	require.Equal(true, ok)
-	assert.Equal(field.Value.Interface(), 5)
-	assert.Equal("valid", field.JSONName)
-	assert.Equal(false, field.OmitEmpty)
-
-	field = fields["validEmpty"]
-	assert.Equal(field.Value.Interface(), 0)
-	assert.Equal("validEmpty", field.JSONName)
-	assert.Equal(false, field.OmitEmpty)
-}
-
 type MyType struct {
 	Foo string   `json:"foo"`
 	Bar []MyType `json:"bar"`
