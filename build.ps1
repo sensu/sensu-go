779b7a108de86c288f409760b8d717bf905514a5
Eric Chlebek <eric@sensu.io>
2018-01-29 10:41:39 -0800

    Don't use -v for tests.
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- build.ps1
+++ build.ps1
@@ -197,7 +197,7 @@ function e2e_commands
 {
     echo "Running e2e tests..."
 
-    go test -v $REPO_PATH/testing/e2e
+    go test $REPO_PATH/testing/e2e
     If ($LASTEXITCODE -ne 0) {
         echo "e2e testing failed..."
         exit 1
