fe8e203b6022a12f15f9b620853c67bab1613a51
Justin Kolberg <amd.prophet@gmail.com>
2018-02-22 15:51:31 -0800

    Add iteration subcommand to version.sh
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- version/version.sh
+++ version/version.sh
@@ -1,38 +1,63 @@
 #!/bin/sh
 
-##
-# This script will output the type of build that this git commit should be
-# considered.
-#
-# * If no git tag exists, it is a nightly build
-# * If a git tag exists and the name contains "alpha", it is an alpha build
-# * If a git tag exists and the name contains "beta", it is a beta build
-# * If a git tag exists and the name contains "rc", it is an rc build
-# * If a git tag exists and does not contain the above, it is a stable build
-##
-
 script="version.sh"
 
+# build_type will output the type of build that this git commit should be
+# considered.
 function build_type() {
     TAG=$(git describe --exact-match --tags HEAD 2> /dev/null || exit 1)
 
     case "x$TAG" in
         x)
+            # no tag exists, this is a nightly build
             echo "nightly"
             ;;
         *alpha*)
+            # tag exists and contains alpha, this is an alpha build
             echo "alpha"
             ;;
         *beta*)
+            # tag exists and contains beta, this is a beta build
             echo "beta"
             ;;
         *rc*)
+            # tag exists and contains rc, this is a release candidate build
             echo "rc"
             ;;
         *)
+            # tag exists but does not contain any of the above, this is a stable build
             echo "stable"
             ;;
     esac
+}
+
+# iteration will output an iteration number based on what type of build the git
+# sha represents and the ci platform it is running on.
+function iteration() {
+    if [ $(build_type) == "nightly" ]; then
+        # get the iteration from environment variables
+        if [[ $TRAVIS == "true" ]]; then
+            echo $TRAVIS_BUILD_NUMBER
+        elif [[ $APPVEYOR == "true" ]]; then
+            echo $APPVEYOR_BUILD_NUMBER
+        elif [[ "x$SENSU_BUILD_ITERATION" != "x" ]]; then
+            echo $SENSU_BUILD_ITERATION
+        else
+            echo "Build iteration could not be found. If running locally you must set SENSU_BUILD_ITERATION."
+            exit 1
+        fi
+    else
+        # parse the iteration from the git tag
+        TAG=$(git describe --exact-match --tags HEAD)
+        RE="[0-9]+$"
+
+        if [[ $TAG =~ $RE ]]; then
+            echo ${BASH_REMATCH[0]}
+        else
+            echo "A build iteration could not be parsed from the tag: $TAG."
+            exit 1
+        fi
+    fi
 }
 
 function usage() {
@@ -43,6 +68,7 @@ function usage() {
     echo "options:"
     echo "-h, --help                show help"
     echo "-t, --build-type          output the type of build this is"
+    echo "-i, --iteration           output the iteration of the build"
     exit 0
 }
 
@@ -52,6 +78,9 @@ case "$1" in
         ;;
     -t|--build-type)
         build_type
+        ;;
+    -i|--iteration)
+        iteration
         ;;
     *)
         usage
