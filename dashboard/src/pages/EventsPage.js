e6831cf6ff0f7c3f4c12544b7b1978a6b3464b1b
Melissa Page <73hl10n@gmail.com>
2018-02-22 13:41:14 -0800

    [Web] Search Box (#1059)
    * Set up search box
    * Grab input from search box
    * Added changelog entry
    * Small updates for review
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>

--- dashboard/src/pages/EventsPage.js
+++ dashboard/src/pages/EventsPage.js
@@ -1,13 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+
 import { graphql } from "react-relay";
 import Typography from "material-ui/Typography";
+import { withStyles } from "material-ui/styles";
 
 import AppContent from "../components/AppContent";
 import EventsContainer from "../components/EventsContainer";
+import SearchBox from "../components/SearchBox";
+
+const styles = {
+  headline: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignContent: "center",
+  },
+  title: {
+    display: "flex",
+    alignSelf: "flex-end",
+  },
+  container: {
+    marginTop: 10,
+  },
+};
 
 class EventsPage extends React.Component {
   static propTypes = {
+    // eslint-disable-next-line react/forbid-prop-types
+    classes: PropTypes.object.isRequired,
     viewer: PropTypes.objectOf(PropTypes.any).isRequired,
   };
 
@@ -19,17 +39,36 @@ class EventsPage extends React.Component {
     }
   `;
 
+  state = { inputValue: "" };
+
+  requerySearchBox = query => {
+    this.setState({ inputValue: query });
+    // TODO return to this and make it actually query
+    // eslint-disable-next-line no-console
+    console.info("query", query);
+  };
+
   render() {
-    const { viewer } = this.props;
+    const { viewer, classes } = this.props;
     return (
       <AppContent>
         <div>
-          <Typography type="headline">Recent Events</Typography>
-          <EventsContainer viewer={viewer} />
+          <div className={classes.headline}>
+            <Typography className={classes.title} type="headline">
+              Recent Events
+            </Typography>
+            <SearchBox
+              onUpdateInput={this.requerySearchBox}
+              state={this.state.inputValue}
+            />
+          </div>
+          <div className={classes.container}>
+            <EventsContainer viewer={viewer} />
+          </div>
         </div>
       </AppContent>
     );
   }
 }
 
-export default EventsPage;
+export default withStyles(styles)(EventsPage);
