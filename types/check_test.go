98f34d6c490f7da6ff1b24d1adfc465d0d7d7cdc
Justin Kolberg <amd.prophet@gmail.com>
2018-02-26 15:02:42 -0800

    Implement check occurrences & occurrences_watermark
    Fixes #1033
    
    Fixes an issue with `MergeWith()` in `types/check.go` where the
    current check's status and execution time were not added to the
    check's history.
    
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- types/check_test.go
+++ types/check_test.go
@@ -96,8 +96,7 @@ func TestMergeWith(t *testing.T) {
 	newCheck.MergeWith(originalCheck)
 
 	assert.NotEmpty(t, newCheck.History)
-	// History has a length of 21, so just index it directly. jfc.
-	assert.Equal(t, int32(1), newCheck.History[20].Status)
+	assert.Equal(t, newCheck.Status, newCheck.History[20].Status)
 }
 
 func TestExtendedAttributes(t *testing.T) {
