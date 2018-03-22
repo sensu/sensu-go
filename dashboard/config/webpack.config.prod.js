154cf4304cb0d38c7fb9b0819abc910515a6b22a
James Phillips <jamesdphillips@gmail.com>
2018-03-21 20:23:55 -0700

    Omit sourcemaps from release builds; too large
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- dashboard/config/webpack.config.prod.js
+++ dashboard/config/webpack.config.prod.js
@@ -20,7 +20,7 @@ const publicPath = paths.servedPath;
 // For these, "homepage" can be set to "." to enable relative asset paths.
 const shouldUseRelativeAssetPaths = publicPath === "./";
 // Source maps are resource heavy and can cause out of memory issue for large source files.
-const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== "false";
+const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP === "true";
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
@@ -302,7 +302,7 @@ module.exports = {
           return;
         }
         // eslint-disable-next-line no-console
-        console.log(message);
+        console.debug(message);
       },
       minify: true,
       // For unknown URLs, fallback to the index page
