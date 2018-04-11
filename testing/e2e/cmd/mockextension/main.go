4a33a2ac730a88ec7e64a292b590c88cd76d5858
Eric Chlebek <eric@sensu.io>
2018-04-10 22:36:38 -0700

    Refactor and document the mockextension program.
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- testing/e2e/cmd/mockextension/main.go
+++ testing/e2e/cmd/mockextension/main.go
@@ -1,3 +1,5 @@
+// mockextension is a sensu extension service whose behaviour can be controlled
+// through environment variables.
 package main
 
 import (
@@ -49,84 +51,63 @@ func init() {
 
 type extension struct{}
 
-func (e *extension) HandleEvent(ctx context.Context, req *rpc.HandleEventRequest) (resp *rpc.HandleEventResponse, err error) {
+// processEvent processes an incoming event. It is called by the exported
+// extension methods.
+// processEvent uses printing to standard out as a method of one-way
+// communication to the outside world. Users are expected to synchronize on
+// its output.
+func (e *extension) processEvent(ctx context.Context, req interface{}, errVar, respVar string, respVal interface{}) (err error) {
 	defer func() {
 		if err != nil {
 			fmt.Print(err)
 		}
 	}()
 	defer responded()
-	if rawErr := os.Getenv(SENSU_E2E_HANDLE_ERROR); rawErr != "" {
+
+	if rawErr := os.Getenv(errVar); rawErr != "" {
 		lastError.Store(true)
-		return nil, errors.New(rawErr)
+		return errors.New(rawErr)
 	}
-	var response rpc.HandleEventResponse
-	rawResp := os.Getenv(SENSU_E2E_HANDLE_RESPONSE)
-	if err := json.NewDecoder(strings.NewReader(rawResp)).Decode(&response); err != nil {
+
+	rawResp := os.Getenv(respVar)
+	if err := json.NewDecoder(strings.NewReader(rawResp)).Decode(respVal); err != nil {
 		lastError.Store(true)
-		return nil, fmt.Errorf("error decoding SENSU_E2E_HANDLE_RESPONSE: %s", err)
+		return fmt.Errorf("error decoding response: %s", err)
 	}
 	fmt.Print(rawResp)
-	return &response, nil
+	return nil
+}
+
+func (e *extension) HandleEvent(ctx context.Context, req *rpc.HandleEventRequest) (resp *rpc.HandleEventResponse, err error) {
+	var response rpc.HandleEventResponse
+	return &response, e.processEvent(ctx, req, SENSU_E2E_HANDLE_ERROR, SENSU_E2E_HANDLE_RESPONSE, &response)
 }
 
 func (e *extension) MutateEvent(ctx context.Context, req *rpc.MutateEventRequest) (resp *rpc.MutateEventResponse, err error) {
-	defer func() {
-		if err != nil {
-			fmt.Print(err)
-		}
-	}()
-	defer responded()
-	if rawErr := os.Getenv(SENSU_E2E_MUTATE_ERROR); rawErr != "" {
-		lastError.Store(true)
-		return nil, errors.New(rawErr)
-	}
 	var response rpc.MutateEventResponse
-	rawResp := os.Getenv(SENSU_E2E_MUTATE_RESPONSE)
-	if err := json.NewDecoder(strings.NewReader(rawResp)).Decode(&response); err != nil {
-		lastError.Store(true)
-		return nil, fmt.Errorf("error decoding SENSU_E2E_MUTATE_RESPONSE: %s", err)
-	}
-	fmt.Print(rawResp)
-	return &response, nil
+	return &response, e.processEvent(ctx, req, SENSU_E2E_MUTATE_ERROR, SENSU_E2E_MUTATE_RESPONSE, &response)
 }
 
 func (e *extension) FilterEvent(ctx context.Context, req *rpc.FilterEventRequest) (resp *rpc.FilterEventResponse, err error) {
-	defer func() {
-		if err != nil {
-			fmt.Print(err)
-		}
-	}()
-	defer responded()
-	if rawErr := os.Getenv(SENSU_E2E_FILTER_ERROR); rawErr != "" {
-		lastError.Store(true)
-		fmt.Fprintln(os.Stderr, rawErr)
-		return nil, errors.New(rawErr)
-	}
 	var response rpc.FilterEventResponse
-	rawResp := os.Getenv(SENSU_E2E_FILTER_RESPONSE)
-	if err := json.NewDecoder(strings.NewReader(rawResp)).Decode(&response); err != nil {
-		lastError.Store(true)
-		return nil, fmt.Errorf("error decoding SENSU_E2E_FILTER_RESPONSE: %s", err)
-	}
-	fmt.Print(rawResp)
-	return &response, nil
+	return &response, e.processEvent(ctx, req, SENSU_E2E_FILTER_ERROR, SENSU_E2E_FILTER_RESPONSE, &response)
 }
 
 func printEnv() {
 	p := fmt.Fprintln
 	o := os.Stderr
-	p(o, "SENSU_E2E_HANDLE_RESPONSE=", os.Getenv("SENSU_E2E_HANDLE_RESPONSE"))
-	p(o, "SENSU_E2E_HANDLE_ERROR=", os.Getenv("SENSU_E2E_HANDLE_ERROR"))
-	p(o, "SENSU_E2E_MUTATE_RESPONSE=", os.Getenv("SENSU_E2E_MUTATE_RESPONSE"))
-	p(o, "SENSU_E2E_MUTATE_ERROR=", os.Getenv("SENSU_E2E_MUTATE_ERROR"))
-	p(o, "SENSU_E2E_FILTER_RESPONSE=", os.Getenv("SENSU_E2E_FILTER_RESPONSE"))
-	p(o, "SENSU_E2E_FILTER_EVENT_ERROR=", os.Getenv("SENSU_E2E_FILTER_EVENT_ERROR"))
+	p(o, "SENSU_E2E_HANDLE_RESPONSE=", os.Getenv(SENSU_E2E_HANDLE_RESPONSE))
+	p(o, "SENSU_E2E_HANDLE_ERROR=", os.Getenv(SENSU_E2E_HANDLE_ERROR))
+	p(o, "SENSU_E2E_MUTATE_RESPONSE=", os.Getenv(SENSU_E2E_MUTATE_RESPONSE))
+	p(o, "SENSU_E2E_MUTATE_ERROR=", os.Getenv(SENSU_E2E_MUTATE_ERROR))
+	p(o, "SENSU_E2E_FILTER_RESPONSE=", os.Getenv(SENSU_E2E_FILTER_RESPONSE))
+	p(o, "SENSU_E2E_FILTER_ERROR=", os.Getenv(SENSU_E2E_FILTER_ERROR))
 }
 
 func main() {
 	flag.Parse()
 	printEnv()
+	// nb: using localhost:%d here does not work reliably.
 	ln, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", *port))
 	if err != nil {
 		log.Fatal(err)
@@ -140,7 +121,7 @@ func main() {
 	}()
 	go func() {
 		<-time.After(15 * time.Second)
-		log.Fatal("service did not respond within 10 seconds")
+		log.Fatal("service did not respond within 15 seconds")
 	}()
 	fmt.Println("ready")
 	respondedWg.Wait()
