0863336fe9e25c42daa975cca80a7923ff5aa1bb
James Phillips <jamesdphillips@gmail.com>
2018-04-12 13:10:20 -0700

    [GraphQL] Snip extra value from environment id (#1290)
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- backend/apid/graphql/globalid/environments.go
+++ backend/apid/graphql/globalid/environments.go
@@ -11,11 +11,30 @@ var environmentName = "environments"
 // EnvironmentTranslator global ID resource
 var EnvironmentTranslator = commonTranslator{
 	name:       environmentName,
-	encodeFunc: standardEncoder(environmentName, "Name"), // TODO: Include org.
 	decodeFunc: standardDecoder,
 	isResponsibleFunc: func(record interface{}) bool {
 		_, ok := record.(*types.Environment)
 		return ok
+	},
+
+	//
+	// Example output:
+	//
+	//   srn:environments:myorg:myenv
+	//   srn:environments:myotherorg:myproductionenv
+	//
+	encodeFunc: func(record interface{}) Components {
+		env, ok := record.(*types.Environment)
+		if !ok {
+			return nil
+		}
+
+		components := StandardComponents{
+			resource:        environmentName,
+			organization:    env.Organization,
+			uniqueComponent: env.Name,
+		}
+		return &components
 	},
 }
 
