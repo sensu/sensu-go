4b159e33476b594b74943894aa613a5c15e502d4
James Phillips <jamesdphillips@gmail.com>
2018-04-15 23:24:18 -0700

    [GraphQL] Retrieve environment from store
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- backend/apid/graphql/query_test.go
+++ backend/apid/graphql/query_test.go
@@ -19,14 +19,36 @@ func (m mockQueryEventFetcher) Find(ctx context.Context, entity, check string) (
 	return m.record, m.err
 }
 
+type mockQueryEnvironmentFetcher struct {
+	record *types.Environment
+	err    error
+}
+
+func (m mockQueryEnvironmentFetcher) Find(ctx context.Context, org, env string) (*types.Environment, error) {
+	return m.record, m.err
+}
+
 func TestQueryTypeEventField(t *testing.T) {
 	mock := mockQueryEventFetcher{&types.Event{}, nil}
-	impl := queryImpl{eventController: mock}
+	impl := queryImpl{eventCtrl: mock}
 
 	args := schema.QueryEventFieldResolverArgs{Ns: schema.NewNamespaceInput("a", "b")}
 	params := schema.QueryEventFieldResolverParams{Args: args}
 
 	res, err := impl.Event(params)
+	require.NoError(t, err)
+	assert.NotEmpty(t, res)
+}
+
+func TestQueryTypeEnvironmentField(t *testing.T) {
+	mock := mockQueryEnvironmentFetcher{&types.Environment{}, nil}
+	impl := queryImpl{environmentCtrl: mock}
+
+	params := schema.QueryEnvironmentFieldResolverParams{}
+	params.Args.Environment = "default"
+	params.Args.Organization = "default"
+
+	res, err := impl.Environment(params)
 	require.NoError(t, err)
 	assert.NotEmpty(t, res)
 }
