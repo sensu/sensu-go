d7dd443423e103180b44039caff9a85e51d69584
Neal Granger <neal@nealg.com>
2018-04-26 18:15:15 -0700

    Extract reusable components out of EventsContainer
    Signed-off-by: Neal Granger <neal@nealg.com>

--- dashboard/src/components/TableList/TableListHeader.js
+++ dashboard/src/components/TableList/TableListHeader.js
@@ -4,20 +4,44 @@ import classnames from "classnames";
 import { withStyles } from "material-ui/styles";
 import Typography from "material-ui/Typography";
 
-const styles = theme => ({
-  root: {
-    padding: `0 ${theme.spacing.unit * 2}px`,
-    backgroundColor: theme.palette.primary.light,
-    color: theme.palette.primary.contrastText,
-    display: "flex",
-    alignItems: "center",
-    height: 56,
-    zIndex: theme.zIndex.appBar - 1,
-  },
-  active: {
-    backgroundColor: theme.palette.primary.main,
-  },
-});
+const styles = theme => {
+  const toolbar = theme.mixins.toolbar;
+  const xsBrk = `${theme.breakpoints.up("xs")} and (orientation: landscape)`;
+  const smBrk = theme.breakpoints.up("sm");
+  const calcTopWithFallback = size => ({
+    top: `calc(${size}px + env(safe-area-inset-top))`,
+    fallbacks: [{ top: size }],
+  });
+
+  return {
+    root: {
+      padding: `0 ${theme.spacing.unit * 2}px`,
+      backgroundColor: theme.palette.primary.light,
+      color: theme.palette.primary.contrastText,
+      display: "flex",
+      alignItems: "center",
+      height: 56,
+      zIndex: theme.zIndex.appBar - 1,
+      "& *": {
+        color: theme.palette.primary.contrastText,
+      },
+    },
+    active: {
+      backgroundColor: theme.palette.primary.main,
+    },
+    sticky: {
+      position: "sticky",
+      ...calcTopWithFallback(toolbar.minHeight),
+      [xsBrk]: {
+        ...calcTopWithFallback(toolbar[xsBrk].minHeight),
+      },
+      [smBrk]: {
+        ...calcTopWithFallback(toolbar[smBrk].minHeight),
+      },
+      color: theme.palette.primary.contrastText,
+    },
+  };
+};
 
 export class TableListHeader extends React.Component {
   static propTypes = {
@@ -25,17 +49,27 @@ export class TableListHeader extends React.Component {
     classes: PropTypes.object.isRequired,
     className: PropTypes.string,
     children: PropTypes.node.isRequired,
+    sticky: PropTypes.bool,
   };
 
   static defaultProps = {
     active: false,
+    sticky: false,
     className: "",
   };
 
   render() {
-    const { active, classes, className: classNameProp, children } = this.props;
+    const {
+      active,
+      sticky,
+      classes,
+      className: classNameProp,
+      children,
+    } = this.props;
+
     const className = classnames(classes.root, classNameProp, {
       [classes.active]: active,
+      [classes.sticky]: sticky,
     });
 
     return (
