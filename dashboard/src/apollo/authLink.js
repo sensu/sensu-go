5d8653dbdd74befdd98625f58ff0c705d31d5a4e
Neal Granger <neal@nealg.com>
2018-05-16 14:23:26 -0700

    Rewrite authLink to abort operation on auth fail
    In the event that an auth token refresh fails while processing a mutation or query, abort the operation with the failed token request error. There is no sense in continuing to attempt the operation if authentication has failed. This prevents a cascade of additional failed attempts with the API.
    
    Signed-off-by: Neal Granger <neal@nealg.com>

--- dashboard/src/apollo/authLink.js
+++ dashboard/src/apollo/authLink.js
@@ -1,30 +1,52 @@
-import { setContext } from "apollo-link-context";
+import { ApolloLink, Observable } from "apollo-link";
 
-import handle from "/exceptionHandler";
+import { when } from "/utils/promise";
+import { UnauthorizedError } from "/errors/FetchError";
 
 import refreshTokens from "/mutations/refreshTokens";
+import invalidateTokens from "/mutations/invalidateTokens";
 
 const EXPIRY_THRESHOLD_MS = 13 * 60 * 1000;
 
 const authLink = ({ getClient }) =>
-  setContext(async () => {
-    try {
-      const notBefore = new Date(Date.now() + EXPIRY_THRESHOLD_MS);
+  new ApolloLink(
+    (operation, forward) =>
+      new Observable(observer => {
+        let sub;
+        refreshTokens(getClient(), {
+          notBefore: new Date(Date.now() + EXPIRY_THRESHOLD_MS).toISOString(),
+        })
+          .then(
+            ({ data }) => {
+              operation.setContext({
+                headers: {
+                  Authorization: `Bearer ${
+                    data.refreshTokens.auth.accessToken
+                  }`,
+                },
+              });
+              sub = forward(operation).subscribe(observer);
+            },
+            when(UnauthorizedError, error => {
+              // Remove the current stored tokens if the token refresh attempt
+              // fails. This will redirect the user back to the login screen.
+              // We could potentially introduce a less disruptive behavior here,
+              // maybe show a modal allowing the user to re-enter credentials
+              // without loosing the current app state.
+              invalidateTokens(getClient());
 
-      // TODO: Prevent parallel auth token requests
-      const { data } = await refreshTokens(getClient(), {
-        notBefore: notBefore.toISOString(),
-      });
+              // re-throw the UnauthorizedError instance to ensure later error
+              // handling (likely a <Query> instance) can deal with it
+              // appropriately.
+              throw error;
+            }),
+          )
+          .catch(observer.error.bind(observer));
 
-      return {
-        headers: {
-          Authorization: `Bearer ${data.refreshTokens.auth.accessToken}`,
-        },
-      };
-    } catch (error) {
-      handle(error);
-      return {};
-    }
-  });
+        return () => {
+          if (sub) sub.unsubscribe();
+        };
+      }),
+  );
 
 export default authLink;
