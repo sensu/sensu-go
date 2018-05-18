050298f470d8a0b489495310433d6d924d26e1ef
Neal Granger <neal@nealg.com>
2018-05-17 21:54:45 -0700

    Remove Object.assign polyfill
    Signed-off-by: Neal Granger <neal@nealg.com>

--- dashboard/config/polyfills.js
+++ dashboard/config/polyfills.js
@@ -12,10 +12,6 @@ if (typeof Promise === "undefined") {
 // fetch() polyfill for making API calls.
 require("whatwg-fetch");
 
-// Object.assign() is commonly used with React.
-// It will use the native implementation if it's present and isn't buggy.
-Object.assign = require("object-assign");
-
 // URLSearch
 require("url-search-params-polyfill");
 
