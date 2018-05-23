ff5daea0ea20cb4119ab0a1f9dd08e9c2ff86d2d
Eric Chlebek <eric@sensu.io>
2018-05-22 23:00:17 -0700

    Prune unnecessarily vendored packages.
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- vendor/github.com/emicklei/proto/cmd/protofmt/format.sh
+++
@@ -1,4 +0,0 @@
-go install
-protofmt unittest_proto2.proto > unittest_proto2_formatted.proto
-protofmt unittest_proto3.proto > unittest_proto3_formatted.proto
-protofmt unittest_proto3_arena.proto > unittest_proto3_arena_formatted.proto
