463bb1c89aa0e5b6b14485f696b871bb3bb2bca8
Nikki Attea <nikki@sensu.io>
2018-05-30 15:21:10 -0500

    Add debug log in pipelined with full event data
    Signed-off-by: Nikki Attea <nikki@sensu.io>

--- util/logging/logging.go
+++ util/logging/logging.go
@@ -7,7 +7,7 @@ import (
 
 // EventFields populates a map with fields containing relevant information about
 // an event for logging
-func EventFields(event *types.Event) map[string]interface{} {
+func EventFields(event *types.Event, debug bool) map[string]interface{} {
 	// Ensure the entity is present
 	if event.Entity == nil {
 		return map[string]interface{}{}
@@ -19,18 +19,34 @@ func EventFields(event *types.Event) map[string]interface{} {
 		"organization": event.Entity.Organization,
 	}
 
-	// The event might not have a check if it's a metric so verify that before
-	// adding the check name
-	if event.HasCheck() {
-		fields["check"] = event.Check.Name
-	}
+	if debug {
+		fields["timestamp"] = event.Timestamp
+		if event.HasCheck() {
+			fields["check"] = event.Check
+		}
+		if event.HasMetrics() {
+			fields["metrics"] = event.Metrics
+		}
+		if event.Hooks != nil {
+			fields["hooks"] = event.Hooks
+		}
+		if event.Silenced != nil {
+			fields["silenced"] = event.Silenced
+		}
+	} else {
+		// The event might not have a check if it's a metric so verify that before
+		// adding the check name
+		if event.HasCheck() {
+			fields["check"] = event.Check.Name
+		}
 
-	if event.HasMetrics() {
-		count := len(event.Metrics.Points)
-		fields["metric_count"] = count
-		if count > 0 {
-			fields["first_metric_name"] = event.Metrics.Points[0].Name
-			fields["first_metric_value"] = event.Metrics.Points[0].Value
+		if event.HasMetrics() {
+			count := len(event.Metrics.Points)
+			fields["metric_count"] = count
+			if count > 0 {
+				fields["first_metric_name"] = event.Metrics.Points[0].Name
+				fields["first_metric_value"] = event.Metrics.Points[0].Value
+			}
 		}
 	}
 
