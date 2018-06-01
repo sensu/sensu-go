a4674100b2d84938f2f601b27a3e08212afd1319
James Phillips <jamesdphillips@gmail.com>
2018-06-01 12:41:55 -0700

    use deepmerge instead of lodash.merge
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- dashboard/src/apollo/stateLink.js
+++ dashboard/src/apollo/stateLink.js
@@ -1,22 +1,14 @@
+import merge from "deepmerge";
 import { withClientState } from "apollo-link-state";
 import auth from "./resolvers/auth";
 import addDeletedFieldTo from "./resolvers/deleted";
 
-function mergeResolvers(...resolvers) {
-  return resolvers.reduce(
-    (acc, res) => ({
-      defaults: Object.assign({}, acc.defaults, res.defaults),
-      resolvers: Object.assign({}, acc.resolvers, res.resolvers),
-    }),
-    {},
-  );
-}
-
-const resolvers = mergeResolvers(
+const resolvers = merge.all([
+  {},
   auth,
   addDeletedFieldTo("Event"),
   addDeletedFieldTo("Entity"),
-);
+]);
 
 const stateLink = ({ cache }) => withClientState({ ...resolvers, cache });
 export default stateLink;
