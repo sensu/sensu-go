ec209106b541ea52e49c96ede863da9cf60c91f7
Nikki Attea <nikki@sensu.io>
2018-06-06 11:31:50 -0700

    Use fmt for cross platform newline
    Signed-off-by: Nikki Attea <nikki@sensu.io>

--- tools/echo/main.go
+++ tools/echo/main.go
@@ -15,11 +15,7 @@ func main() {
 		line := strings.Join(os.Args[1:], " ")
 		lines := strings.Split(line, `\n`)
 		for _, l := range lines {
-			_, err := os.Stdout.WriteString(l + "\n")
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
+			fmt.Println(l)
 		}
 	} else {
 		usage()
