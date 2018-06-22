6bd26698e272d3734ac564503463a26df6b9f8b2
Sean Porter <portertech@gmail.com>
2018-06-22 08:50:54 -0700

    Agent Transport debug log event (#1726)
    * added agent transport sending message debug log event
    
    Signed-off-by: Sean Porter <portertech@gmail.com>
    
    * use "payload" as field name in agent transport debug log events
    
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- agent/agent.go
+++ agent/agent.go
@@ -287,7 +287,7 @@ func (a *Agent) receivePump() {
 
 			logger.WithFields(logrus.Fields{
 				"type":    msg.Type,
-				"message": string(msg.Payload),
+				"payload": string(msg.Payload),
 			}).Info("message received")
 			err := a.handler.Handle(msg.Type, msg.Payload)
 			if err != nil {
@@ -298,6 +298,10 @@ func (a *Agent) receivePump() {
 }
 
 func (a *Agent) sendMessage(msgType string, payload []byte) {
+	logger.WithFields(logrus.Fields{
+		"type":    msgType,
+		"payload": string(payload),
+	}).Debug("sending message")
 	// blocks until message can be enqueued.
 	// TODO(greg): ring buffer?
 	msg := &transport.Message{
