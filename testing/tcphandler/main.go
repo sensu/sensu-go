6912023e31ac0cde97f453f893af3f826991c3bd
Nikki Attea <nikki@sensu.io>
2018-06-29 08:41:12 -0700

    Address pr feedback
    Signed-off-by: Nikki Attea <nikki@sensu.io>

--- testing/tcphandler/main.go
+++ testing/tcphandler/main.go
@@ -5,11 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"os"
 )
 
 var (
-	port = flag.Int("port", 3333, "port to run extension server on")
+	port = flag.Int("port", 3333, "port to run tcp server on")
 	conn = flag.String("type", "tcp", "type of connection to establish (ex. tcp)")
 )
 
@@ -18,7 +17,6 @@ func main() {
 	l, err := net.Listen(*conn, fmt.Sprintf("127.0.0.1:%d", *port))
 	if err != nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
 	defer l.Close()
 	log.Print(fmt.Sprintf("Listening on %s 127.0.0.1:%d", *conn, *port))
@@ -26,18 +24,18 @@ func main() {
 		conn, err := l.Accept()
 		if err != nil {
 			log.Fatal(err)
-			os.Exit(1)
 		}
 		go handleRequest(conn)
 	}
 }
 
 func handleRequest(conn net.Conn) {
+	defer conn.Close()
 	buf := make([]byte, 4096)
 	_, err := conn.Read(buf)
 	if err != nil {
 		log.Print("Error reading: ", err.Error())
+		return
 	}
 	log.Print(string(buf))
-	conn.Close()
 }
