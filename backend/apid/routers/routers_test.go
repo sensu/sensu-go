f824aaf7e07f8d6912ce86338e43436d4b11c4e0
Eric Chlebek <eric@sensu.io>
2018-07-09 12:03:26 -0700

    Add unit tests for environments and organizations.
    * Make the environment and organization controllers abstract
    * Mock the controllers in unit tests
    
    Signed-off-by: Eric Chlebek <eric@sensu.io>

---
+++ backend/apid/routers/routers_test.go
@@ -0,0 +1,34 @@
+package routers
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/sensu/sensu-go/backend/authorization"
+	"github.com/sensu/sensu-go/types"
+)
+
+func newRequest(t *testing.T, method, endpoint string, body io.Reader) *http.Request {
+	t.Helper()
+	req, err := http.NewRequest(method, endpoint, body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx := context.Background()
+	rules := []types.Rule{
+		types.Rule{
+			Type:         types.RuleTypeAll,
+			Environment:  "*",
+			Organization: "*",
+			Permissions:  types.RuleAllPerms,
+		},
+	}
+	actor := authorization.Actor{
+		Name:  "admin",
+		Rules: rules,
+	}
+	ctx = context.WithValue(ctx, types.AuthorizationActorKey, actor)
+	return req.WithContext(ctx)
+}
