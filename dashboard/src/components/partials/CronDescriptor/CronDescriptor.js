bb5921fe01f6c1fb3674da46f04530dc6790d506
James Phillips <jamesdphillips@gmail.com>
2018-08-02 08:43:43 -0700

    [Web UI] Use cron value in check list (#1910)
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- dashboard/src/components/partials/CronDescriptor/CronDescriptor.js
+++ dashboard/src/components/partials/CronDescriptor/CronDescriptor.js
@@ -1,21 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
+import lowerFirst from "lodash/lowerFirst";
 import cronstrue from "cronstrue";
 import Tooltip from "@material-ui/core/Tooltip";
 
 class CronDescriptor extends React.PureComponent {
   static propTypes = {
+    capitalize: PropTypes.bool,
     expression: PropTypes.string.isRequired,
     component: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
   };
 
   static defaultProps = {
+    capitalize: false,
     component: "span",
   };
 
   render() {
-    const { expression, component: Component } = this.props;
-    const statement = cronstrue.toString(expression);
+    const { capitalize, expression, component: Component } = this.props;
+
+    let statement = cronstrue.toString(expression);
+    if (!capitalize) {
+      statement = lowerFirst(statement);
+    }
 
     return (
       <Tooltip title={expression}>
