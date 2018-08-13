585370d1567b28f119c8c79dcc9d0ae1f5fec62e
Eric Chlebek <echlebek@gmail.com>
2018-08-13 16:24:38 -0700

    Add gogo/protobuf install to rpc_gen.go. Run go generate. (#1951)
    Follow-up commit to 273d3bb732fb084c92172297888c155c429bf92c
    
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- rpc/rpc_gen.go
+++ rpc/rpc_gen.go
@@ -1,4 +1,5 @@
 package rpc
 
+//go:generate go install github.com/sensu/sensu-go/vendor/github.com/golang/protobuf/protoc-gen-go
 //go:generate -command protoc protoc -I ../../../../ -I . -I ../types/ -I ../vendor/ --go_out=plugins=grpc:.
 //go:generate protoc extension.proto
