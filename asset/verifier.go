c25065d8f480f58114ce0abf508972db9f418513
Greg Poirier <greg.istehbest@gmail.com>
2018-09-05 07:58:20 -0700

    Add SHA512 verifier and tests
    Signed-off-by: Greg Poirier <greg.istehbest@gmail.com>

--- asset/verifier.go
+++ asset/verifier.go
@@ -1,9 +1,35 @@
 package asset
 
-import "os"
+import (
+	"crypto/sha512"
+	"encoding/hex"
+	"fmt"
+	"io"
+)
 
 // A Verifier verifies that a file's SHA-512 matches the specified
 // SHA-512.
 type Verifier interface {
-	Verify(file *os.File, sha512 string) error
+	Verify(file io.ReadSeeker, sha512 string) error
+}
+
+type SHA512Verifier struct{}
+
+func (v *SHA512Verifier) Verify(rs io.ReadSeeker, desiredSHA string) error {
+	// Generate checksum for downloaded file
+	h := sha512.New()
+	if _, err := io.Copy(h, rs); err != nil {
+		return fmt.Errorf("generating checksum for asset failed: %s", err)
+	}
+
+	if _, err := rs.Seek(io.SeekStart, io.SeekStart); err != nil {
+		return err
+	}
+	fmt.Println(h.Sum(nil))
+
+	if foundSHA := hex.EncodeToString(h.Sum(nil)); foundSHA != desiredSHA {
+		return fmt.Errorf("sha512:%s does not match specified sha512%s", desiredSHA, foundSHA)
+	}
+
+	return nil
 }
--- asset/verifier_test.go
+++ asset/verifier_test.go
@@ -1,1 +1,50 @@
 package asset_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/sensu/sensu-go/asset"
+)
+
+func TestSuccessfulVerify(t *testing.T) {
+	t.Parallel()
+
+	assetPath := getFixturePath("rubby-on-rails.tar")
+	assetSHA, err := ioutil.ReadFile(fmt.Sprintf("%s.sha512", assetPath))
+	if err != nil {
+		t.Fatalf("could not read asset sha, error: %v", err)
+	}
+
+	f, err := os.Open(assetPath)
+	if err != nil {
+		t.Fatalf("could not open asset, error: %v", err)
+	}
+	defer f.Close()
+
+	verifier := &asset.SHA512Verifier{}
+	if err := verifier.Verify(f, string(assetSHA)); err != nil {
+		t.Logf("expected no error, got %v", err)
+		t.Fail()
+	}
+}
+
+func TestFailedVerify(t *testing.T) {
+	t.Parallel()
+
+	assetPath := getFixturePath("rubby-on-rails.tar")
+
+	f, err := os.Open(assetPath)
+	if err != nil {
+		t.Fatalf("could not open asset, error: %v", err)
+	}
+	defer f.Close()
+
+	verifier := &asset.SHA512Verifier{}
+	if err := verifier.Verify(f, "obviously not a SHA512"); err == nil {
+		t.Log("expected error, got nil")
+		t.Fail()
+	}
+}
