245dd96211fdf7017b13b90ccabf1c49a7d17792
Greg Poirier <greg.istehbest@gmail.com>
2018-09-05 07:29:12 -0700

    Add http fetcher and tests
    Signed-off-by: Greg Poirier <greg.istehbest@gmail.com>

--- asset/fetcher.go
+++ asset/fetcher.go
@@ -15,16 +15,50 @@ type Fetcher interface {
 	Fetch(source string) (*os.File, error)
 }
 
-// An HTTPFetcher fetches the contents of files at a given URL.
-type HTTPFetcher struct{}
+// URLGetter gets all content at the specified URL.
+type URLGetter interface {
+	Get(string) (io.ReadCloser, error)
+}
 
-// Fetch the file found at the specified url, and return the file or an
-// error indicating why the fetch failed.
-func (h HTTPFetcher) Fetch(url string) (*os.File, error) {
-	client := &http.Client{Timeout: 30 * time.Minute}
+// HTTPURLGetter uses an *http.Client to fetch content at the
+// given URL.
+type HTTPURLGetter struct {
+	Timeout time.Duration
+}
+
+// Get the target URL and return an io.ReadCloser
+func (h *HTTPURLGetter) Get(url string) (io.ReadCloser, error) {
+	client := &http.Client{Timeout: h.Timeout}
+
 	resp, err := client.Get(url)
 	if err != nil {
 		return nil, fmt.Errorf("error fetching asset: %s", err.Error())
+	}
+
+	return resp.Body, nil
+}
+
+// An HTTPFetcher fetches the contents of files at a given URL.
+type HTTPFetcher struct {
+	Timeout time.Duration
+	URLGetter
+}
+
+// InjectGetter injects the specified URLGetter.
+func (h *HTTPFetcher) InjectGetter(g URLGetter) {
+	h.URLGetter = g
+}
+
+// Fetch the file found at the specified url, and return the file or an
+// error indicating why the fetch failed.
+func (h *HTTPFetcher) Fetch(url string) (*os.File, error) {
+	if h.URLGetter == nil {
+		h.InjectGetter(&HTTPURLGetter{})
+	}
+
+	resp, err := h.Get(url)
+	if err != nil {
+		return nil, err
 	}
 
 	// Write response to tmp
@@ -33,9 +67,18 @@ func (h HTTPFetcher) Fetch(url string) (*os.File, error) {
 		return nil, fmt.Errorf("can't open tmp file for asset")
 	}
 
-	if _, err = io.Copy(tmpFile, resp.Body); err != nil {
+	if _, err = io.Copy(tmpFile, resp); err != nil {
 		return nil, fmt.Errorf("error downloading asset")
 	}
 
 	return tmpFile, resetFile(tmpFile)
+}
+
+// resetFile ensures file contents are synced and rewound
+func resetFile(f *os.File) error {
+	if err := f.Sync(); err != nil {
+		return err
+	}
+	_, err := f.Seek(0, 0)
+	return err
 }
--- asset/fetcher_test.go
+++ asset/fetcher_test.go
@@ -1,1 +1,59 @@
 package asset_test
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/sensu/sensu-go/asset"
+)
+
+var (
+	fixturePath string
+)
+
+func init() {
+	path, err := filepath.Abs(".")
+	if err != nil {
+		panic(err)
+	}
+	fixturePath = filepath.Join(path, "fixtures")
+}
+
+func getFixturePath(name string) string {
+	return filepath.Join(fixturePath, name)
+}
+
+type mockURLGetter struct {
+	filePath string
+}
+
+func (m *mockURLGetter) Get(string) (io.ReadCloser, error) {
+	return os.Open(m.filePath)
+}
+
+func TestFetchExistingAsset(t *testing.T) {
+	t.Parallel()
+
+	assetName := "rubby-on-rails.tar"
+
+	fetcher := &asset.HTTPFetcher{}
+	fetcher.InjectGetter(&mockURLGetter{getFixturePath(assetName)})
+	resp, err := fetcher.Fetch("")
+	if err != nil {
+		t.Logf("expected no error, got: %v", err)
+		t.FailNow()
+	}
+	defer resp.Close()
+
+	desiredSHA, _ := ioutil.ReadFile(getFixturePath(fmt.Sprintf("%s.sha512", assetName)))
+
+	verifier := &asset.SHA512Verifier{}
+	if err := verifier.Verify(resp, string(desiredSHA)); err != nil {
+		t.Logf("expected no error, got: %v", err)
+		t.FailNow()
+	}
+}
