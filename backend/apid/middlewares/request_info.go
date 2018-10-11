4d550a1ba4c94480e36a9bdf74fc6a1584f68144
Cyril Cressent <cyril.cressent@sensu.io>
2018-10-10 18:02:01 -0700

    Add support for API group, API version, and verb
    Signed-off-by: Cyril Cressent <cyril@sensu.io>

--- backend/apid/middlewares/request_info.go
+++ backend/apid/middlewares/request_info.go
@@ -3,6 +3,9 @@ package middlewares
 import (
 	"context"
 	"net/http"
+	"strings"
+
+	"github.com/sensu/sensu-go/types"
 )
 
 // RequestInfo is an HTTP middleware that populates a context for the request,
@@ -10,15 +13,51 @@ import (
 // executed early in the middleware stack.
 type RequestInfo struct{}
 
+// Then is the RequestInfo middleware's main logic, heavily inspired by
+// Kubernetes' WithRequestInfo.
+//
+// The general, expected format of a path is as
+// follow: /apis/{group}/{version}/namespaces/{namespace}/{type}/{name}
+//
+// This middleware tries to fill in as many fields as it can in the
+// types.RequestInfo struct that's added to the context, but there is no guarantee
+// of any fields being filled in.
 func (i RequestInfo) Then(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
+		info := types.RequestInfo{}
 
-		// APIGroup, Namespace, ... anything else we can parse out of the
-		// request and want to include.
-		ctx = context.WithValue(ctx, "APIGroup", "value")
-		ctx = context.WithValue(ctx, "Namespace", "value")
+		ctx = context.WithValue(ctx, types.RequestInfoKey, &info)
+		defer next.ServeHTTP(w, r.WithContext(ctx))
 
-		next.ServeHTTP(w, r.WithContext(ctx))
+		switch r.Method {
+		case "POST":
+			info.Verb = "create"
+		case "GET", "HEAD":
+			info.Verb = "get"
+		case "PUT":
+			info.Verb = "update"
+		case "DELETE":
+			info.Verb = "delete"
+		default:
+			info.Verb = ""
+		}
+
+		path := strings.Trim(r.URL.Path, "/")
+		parts := strings.Split(path, "/")
+
+		// We need at least the API prefix, the API group and the API version to
+		// build a meaningful RequestInfo.
+		if len(parts) < 3 {
+			return
+		}
+
+		// The first part of the path has to be "apis"
+		if parts[0] != "apis" {
+			return
+		}
+
+		info.APIGroup = parts[1]
+		info.APIVersion = parts[2]
 	})
 }
--- backend/apid/middlewares/request_info_test.go
+++ backend/apid/middlewares/request_info_test.go
@@ -1,9 +1,85 @@
 package middlewares
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/sensu/sensu-go/types"
+	"github.com/stretchr/testify/assert"
 )
 
+func TestRequestInfoVerb(t *testing.T) {
+	// TODO: Test focused on the translation of HTTP methods and "special verbs",
+	// such as "watch" and "list", to a Verb by the middleware.
+}
+
 func TestRequestInfo(t *testing.T) {
-	// Test cases for malformated URLs, eg: no API group, ...
+	cases := []struct {
+		description string
+		method      string
+		path        string
+		expected    types.RequestInfo
+	}{
+		{
+			description: "path not starting with the rigth prefix",
+			method:      "GET",
+			path:        "/some/unrelated/path",
+			expected: types.RequestInfo{
+				Verb: "get",
+			},
+		},
+		{
+			description: "path doesn't have an API group",
+			method:      "GET",
+			path:        "/apis/",
+			expected: types.RequestInfo{
+				Verb: "get",
+			},
+		},
+		{
+			description: "path doesn't have an API version",
+			method:      "GET",
+			path:        "/apis/rbac/",
+			expected: types.RequestInfo{
+				Verb: "get",
+			},
+		},
+		{
+			description: "path has both API group and version",
+			method:      "GET",
+			path:        "/apis/rbac/v1alpha1",
+			expected: types.RequestInfo{
+				APIGroup:   "rbac",
+				APIVersion: "v1alpha1",
+				Verb:       "get",
+			},
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.description, func(t *testing.T) {
+			checkResult := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				info := types.ContextRequestInfo(r.Context())
+				assert.NotNil(t, info)
+				assert.Equal(t, &tt.expected, info)
+			})
+
+			middleware := RequestInfo{}
+			server := httptest.NewServer(middleware.Then(checkResult))
+
+			req, err := http.NewRequest(tt.method, server.URL+tt.path, nil)
+			if err != nil {
+				t.Fatal("Couldn't create request: ", err)
+			}
+
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				t.Fatal("Failed to get a response: ", err)
+			}
+
+			assert.Equal(t, http.StatusOK, resp.StatusCode)
+			server.Close()
+		})
+	}
 }
