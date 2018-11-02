50daf4ea364c73ae60ea2b5708c45adb27e4a047
Cyril Cressent <cyril.cressent@sensu.io>
2018-11-02 12:31:00 -0700

    Add tests for LegacyAuthorizationAttributes
    Signed-off-by: Cyril Cressent <cyril.cressent@sensu.io>

--- backend/apid/middlewares/authorization_attributes_test.go
+++ backend/apid/middlewares/authorization_attributes_test.go
@@ -140,3 +140,405 @@ func TestAuthorizationAttributes(t *testing.T) {
 		})
 	}
 }
+
+func TestLegacyAuthorizationAttributes(t *testing.T) {
+	cases := []struct {
+		description string
+		method      string
+		path        string
+		urlVars     map[string]string
+		expected    authorization.Attributes
+	}{
+		{
+			description: "GET /assets",
+			method:      "GET",
+			path:        "/assets",
+			urlVars:     map[string]string{},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "assets",
+				ResourceName: "",
+				Verb:         "list",
+			},
+		},
+		{
+			description: "GET /assets?namespace=foo",
+			method:      "GET",
+			path:        "/assets?namespace=foo",
+			urlVars:     map[string]string{},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "foo",
+				Resource:     "assets",
+				ResourceName: "",
+				Verb:         "list",
+			},
+		},
+		{
+			description: "GET /assets/foo",
+			method:      "GET",
+			path:        "/assets/foo",
+			urlVars: map[string]string{
+				"id": "foo",
+			},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "assets",
+				ResourceName: "foo",
+				Verb:         "get",
+			},
+		},
+		{
+			description: "GET /assets/foo?namespace=bar",
+			method:      "GET",
+			path:        "/assets/foo?namespace=bar",
+			urlVars: map[string]string{
+				"id": "foo",
+			},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "bar",
+				Resource:     "assets",
+				ResourceName: "foo",
+				Verb:         "get",
+			},
+		},
+		{
+			description: "GET /checks",
+			method:      "GET",
+			path:        "/checks",
+			urlVars:     map[string]string{},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "checks",
+				ResourceName: "",
+				Verb:         "list",
+			},
+		},
+		{
+			description: "GET /checks/foo",
+			method:      "GET",
+			path:        "/checks/foo",
+			urlVars: map[string]string{
+				"id": "foo",
+			},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "checks",
+				ResourceName: "foo",
+				Verb:         "get",
+			},
+		},
+		{
+			description: "POST /checks/foo/execute",
+			method:      "POST",
+			path:        "/checks/foo/execute",
+			urlVars: map[string]string{
+				"id": "foo",
+			},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "checks",
+				ResourceName: "foo",
+				Verb:         "post",
+			},
+		},
+		{
+			description: "PUT /checks/foo/hooks/bar",
+			method:      "PUT",
+			path:        "/checks/foo/hooks/bar",
+			urlVars: map[string]string{
+				"id":   "foo",
+				"type": "bar",
+			},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "checks",
+				ResourceName: "foo",
+				Verb:         "put",
+			},
+		},
+		{
+			description: "DELETE /checks/foo/hooks/bar/hook/baz",
+			method:      "DELETE",
+			path:        "/checks/foo/hooks/bar/hook/baz",
+			urlVars: map[string]string{
+				"id":   "foo",
+				"type": "bar",
+				"hook": "baz",
+			},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "checks",
+				ResourceName: "foo",
+				Verb:         "delete",
+			},
+		},
+		{
+			description: "GET /cluster/members",
+			method:      "GET",
+			path:        "/cluster/members",
+			urlVars:     map[string]string{},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "cluster-members",
+				ResourceName: "",
+				Verb:         "list",
+			},
+		},
+		{
+			description: "GET /cluster/members/foo",
+			method:      "GET",
+			path:        "/cluster/members/foo",
+			urlVars: map[string]string{
+				"id": "foo",
+			},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "cluster-members",
+				ResourceName: "foo",
+				Verb:         "get",
+			},
+		},
+		{
+			description: "GET /events",
+			method:      "GET",
+			path:        "/events",
+			urlVars:     map[string]string{},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "events",
+				ResourceName: "",
+				Verb:         "list",
+			},
+		},
+		{
+			description: "GET /events/entity_name",
+			method:      "GET",
+			path:        "/events/entity_name",
+			urlVars: map[string]string{
+				"entity": "entity_name",
+			},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "events/entity_name",
+				ResourceName: "",
+				Verb:         "list",
+			},
+		},
+		{
+			description: "GET /events/entity_name/check_name",
+			method:      "GET",
+			path:        "/events/entity_name/check_name",
+			urlVars: map[string]string{
+				"check":  "check_name",
+				"entity": "entity_name",
+			},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "events/entity_name/check_name",
+				ResourceName: "",
+				Verb:         "list",
+			},
+		},
+		{
+			description: "GET /rbac/namespaces",
+			method:      "GET",
+			path:        "/rbac/namespaces",
+			urlVars:     map[string]string{},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "namespaces",
+				ResourceName: "",
+				Verb:         "list",
+			},
+		},
+		{
+			description: "GET /rbac/namespaces/foo",
+			method:      "GET",
+			path:        "/rbac/namespaces/foo",
+			urlVars: map[string]string{
+				"id": "foo",
+			},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "namespaces",
+				ResourceName: "foo",
+				Verb:         "get",
+			},
+		},
+		{
+			description: "GET /rbac/users",
+			method:      "GET",
+			path:        "/rbac/users",
+			urlVars:     map[string]string{},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "users",
+				ResourceName: "",
+				Verb:         "list",
+			},
+		},
+		{
+			description: "GET /rbac/users/foo",
+			method:      "GET",
+			path:        "/rbac/users/foo",
+			urlVars: map[string]string{
+				"id": "foo",
+			},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "users",
+				ResourceName: "foo",
+				Verb:         "get",
+			},
+		},
+		{
+			description: "GET /silenced",
+			method:      "GET",
+			path:        "/silenced",
+			urlVars:     map[string]string{},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "silenced",
+				ResourceName: "",
+				Verb:         "list",
+			},
+		},
+		{
+			description: "GET /silenced/foo",
+			method:      "GET",
+			path:        "/silenced/foo",
+			urlVars: map[string]string{
+				"id": "foo",
+			},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "silenced",
+				ResourceName: "foo",
+				Verb:         "get",
+			},
+		},
+		{
+			description: "GET /silenced/checks",
+			method:      "GET",
+			path:        "/silenced/checks",
+			urlVars:     map[string]string{},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "silenced/checks",
+				ResourceName: "",
+				Verb:         "list",
+			},
+		},
+		{
+			description: "GET /silenced/checks/foo",
+			method:      "GET",
+			path:        "/silenced/checks/foo",
+			urlVars: map[string]string{
+				"check": "foo",
+			},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "silenced/checks",
+				ResourceName: "foo",
+				Verb:         "get",
+			},
+		},
+		{
+			description: "GET /silenced/subscriptions",
+			method:      "GET",
+			path:        "/silenced/subscriptions",
+			urlVars:     map[string]string{},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "silenced/subscriptions",
+				ResourceName: "",
+				Verb:         "list",
+			},
+		},
+		{
+			description: "GET /silenced/subscriptions/foo",
+			method:      "GET",
+			path:        "/silenced/subscriptions/foo",
+			urlVars: map[string]string{
+				"subscription": "foo",
+			},
+			expected: authorization.Attributes{
+				APIGroup:     "core",
+				APIVersion:   "v2",
+				Namespace:    "default",
+				Resource:     "silenced/subscriptions",
+				ResourceName: "foo",
+				Verb:         "get",
+			},
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.description, func(t *testing.T) {
+			testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				attrs := authorization.GetAttributes(r.Context())
+				assert.NotNil(t, attrs)
+				assert.Equal(t, &tt.expected, attrs)
+			})
+			middleware := LegacyAuthorizationAttributes{}
+
+			w := httptest.NewRecorder()
+			r, err := http.NewRequest(tt.method, tt.path, nil)
+			if err != nil {
+				t.Fatal("Couldn't create request: ", err)
+			}
+
+			r = mux.SetURLVars(r, tt.urlVars)
+			handler := middleware.Then(testHandler)
+			handler.ServeHTTP(w, r)
+
+			assert.Equal(t, http.StatusOK, w.Code)
+		})
+	}
+}
