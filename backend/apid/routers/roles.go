9693ec8831bb8da4d86d9696068f9d199fc5e594
Cyril Cressent <cyril.cressent@sensu.io>
2018-11-06 12:08:16 -0800

    Cleanup roles router and controller
    Signed-off-by: Cyril Cressent <cyril.cressent@sensu.io>

--- backend/apid/routers/roles.go
+++ backend/apid/routers/roles.go
@@ -10,21 +10,21 @@ import (
 	"github.com/sensu/sensu-go/types"
 )
 
-// RolesRouter handles requests for /checks
+// RolesRouter handles requests for Roles.
 type RolesRouter struct {
 	controller actions.RoleController
 }
 
-// NewRolesRouter instantiates new router for controlling check resources
+// NewRolesRouter instantiates a new router for Roles.
 func NewRolesRouter(store store.RoleStore) *RolesRouter {
 	return &RolesRouter{
 		controller: actions.NewRoleController(store),
 	}
 }
 
-// Mount the RolesRouter to a parent Router
+// Mount the RolesRouter on the given parent Router
 func (r *RolesRouter) Mount(parent *mux.Router) {
-	routes := ResourceRoute{Router: parent, PathPrefix: "/rbac/roles"}
+	routes := ResourceRoute{Router: parent, PathPrefix: "/apis/rbac/v2/roles"}
 	routes.GetAll(r.list)
 	routes.Get(r.find)
 	routes.Post(r.create)
@@ -33,46 +33,52 @@ func (r *RolesRouter) Mount(parent *mux.Router) {
 }
 
 func (r *RolesRouter) create(req *http.Request) (interface{}, error) {
-	cfg := types.Role{}
-	if err := UnmarshalBody(req, &cfg); err != nil {
+	obj := types.Role{}
+
+	if err := UnmarshalBody(req, &obj); err != nil {
 		return nil, err
 	}
 
-	err := r.controller.Create(req.Context(), cfg)
-	return cfg, err
+	err := r.controller.Create(req.Context(), obj)
+	return obj, err
 }
 
 func (r *RolesRouter) createOrReplace(req *http.Request) (interface{}, error) {
-	cfg := types.Role{}
-	if err := UnmarshalBody(req, &cfg); err != nil {
+	obj := types.Role{}
+
+	if err := UnmarshalBody(req, &obj); err != nil {
 		return nil, err
 	}
 
-	err := r.controller.CreateOrReplace(req.Context(), cfg)
-	return cfg, err
+	err := r.controller.CreateOrReplace(req.Context(), obj)
+	return obj, err
 }
 
 func (r *RolesRouter) destroy(req *http.Request) (interface{}, error) {
 	params := mux.Vars(req)
+
 	id, err := url.PathUnescape(params["id"])
 	if err != nil {
 		return nil, err
 	}
+
 	err = r.controller.Destroy(req.Context(), id)
 	return nil, err
 }
 
 func (r *RolesRouter) find(req *http.Request) (interface{}, error) {
 	params := mux.Vars(req)
+
 	id, err := url.PathUnescape(params["id"])
 	if err != nil {
 		return nil, err
 	}
-	record, err := r.controller.Get(req.Context(), id)
-	return record, err
+
+	obj, err := r.controller.Get(req.Context(), id)
+	return obj, err
 }
 
 func (r *RolesRouter) list(req *http.Request) (interface{}, error) {
-	records, err := r.controller.List(req.Context())
-	return records, err
+	objs, err := r.controller.List(req.Context())
+	return objs, err
 }
