f274bd00a7d18b1fd91c0854de1a18dd068b3670
Eric Chlebek <eric@sensu.io>
2018-11-06 17:18:44 -0800

    Fix a yaml encoding bug.
    This commit fixes a bug where encoding a zero-length slice to yaml
    would generate an error.
    
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- cli/commands/helpers/yaml.go
+++ cli/commands/helpers/yaml.go
@@ -39,12 +39,17 @@ func toMap(v interface{}) map[string]interface{} {
 // PrintYAML serializes the value v to yaml and writes the result to w.
 func PrintYAML(v interface{}, w io.Writer) (err error) {
 	enc := yaml.NewEncoder(w)
+	var close = true
 	defer func() {
-		if err == nil {
+		if err == nil && close {
 			err = enc.Close()
 		}
 	}()
 	if resources, ok := v.([]types.Resource); ok {
+		if len(resources) == 0 {
+			close = false
+			return nil
+		}
 		for _, r := range resources {
 			if err := enc.Encode(wrapResource(r)); err != nil {
 				return err
