007ac41d3b31aefd6cd1539ec3442b1b90b3ee53
Simon Plourde <simon.plourde@gmail.com>
2018-11-08 09:42:14 -0500

    Add support for namespaces into path variable (#2306)
    Signed-off-by: Simon Plourde <simon@sensu.io>

---
+++ backend/apid/middlewares/namespace_test.go
@@ -0,0 +1,70 @@
+package middlewares
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/sensu/sensu-go/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNamespaceMiddlware(t *testing.T) {
+	cases := []struct {
+		description string
+		method      string
+		url         string
+		urlVars     map[string]string
+		expected    string
+	}{
+		{
+			description: "No query param or path variable",
+			method:      "GET",
+			url:         "/",
+			expected:    "default",
+		},
+		{
+			description: "Query param",
+			method:      "GET",
+			url:         "/?namespace=acme",
+			expected:    "acme",
+		},
+		{
+			description: "Path variable",
+			method:      "GET",
+			url:         "/",
+			urlVars: map[string]string{
+				"namespace": "foobar",
+			},
+			expected: "foobar",
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.description, func(t *testing.T) {
+			testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				var namespace string
+				if value := r.Context().Value(types.NamespaceKey); value != nil {
+					namespace = value.(string)
+				} else {
+					namespace = ""
+				}
+				assert.Equal(t, tt.expected, namespace)
+			})
+			middleware := Namespace{}
+
+			w := httptest.NewRecorder()
+			r, err := http.NewRequest(tt.method, tt.url, nil)
+			if err != nil {
+				t.Fatal("Couldn't create request: ", err)
+			}
+
+			r = mux.SetURLVars(r, tt.urlVars)
+			handler := middleware.Then(testHandler)
+			handler.ServeHTTP(w, r)
+
+			assert.Equal(t, http.StatusOK, w.Code)
+		})
+	}
+}
