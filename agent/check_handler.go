ee69cd99396cd5b705ad7221eb136c8df5cc8a3b
Cyril Cressent <cyril.cressent@sensu.io>
2018-11-20 14:44:59 -0800

    Make {from,to}Map() package level functions
    Signed-off-by: Cyril Cressent <cyril.cressent@sensu.io>

--- agent/check_handler.go
+++ agent/check_handler.go
@@ -236,47 +236,6 @@ func extractMetrics(event *types.Event) []*types.MetricPoint {
 // The expected format for an environment variable definition is VAR=VALUE. Any
 // malformed environment variable definition will be discarded by the merge.
 func mergeEnvironments(env1, env2 []string) []string {
-	toMap := func(s []string) map[string]string {
-		m := map[string]string{}
-
-		for _, v := range s {
-			// Try to split the variable definition into exactly 2 substrings:
-			// what's left of the first '=' (the variable name) and what's right
-			// of it (the variable value)
-			split := strings.SplitN(v, "=", 2)
-
-			switch len(split) {
-			case 1:
-				if split[0] == v {
-					// There is no '=' in the input, consider it malformed
-					break
-				} else {
-					// We came across VAR=, which is equivalent to VAR=""
-					m[split[0]] = ""
-				}
-			case 2:
-				// A proper VAR=VALUE definiton
-				m[split[0]] = split[1]
-			default:
-				// Anything else is considered malformed and ignored
-				break
-			}
-		}
-
-		return m
-	}
-
-	fromMap := func(m map[string]string) []string {
-		s := []string{}
-
-		for k, v := range m {
-			s = append(s, k+"="+v)
-		}
-		sort.StringSlice(s).Sort()
-
-		return s
-	}
-
 	e1 := toMap(env1)
 	e2 := toMap(env2)
 
@@ -290,4 +249,45 @@ func mergeEnvironments(env1, env2 []string) []string {
 	}
 
 	return fromMap(e1)
+}
+
+func toMap(s []string) map[string]string {
+	m := map[string]string{}
+
+	for _, v := range s {
+		// Try to split the variable definition into exactly 2 substrings:
+		// what's left of the first '=' (the variable name) and what's right
+		// of it (the variable value)
+		split := strings.SplitN(v, "=", 2)
+
+		switch len(split) {
+		case 1:
+			if split[0] == v {
+				// There is no '=' in the input, consider it malformed
+				break
+			} else {
+				// We came across VAR=, which is equivalent to VAR=""
+				m[split[0]] = ""
+			}
+		case 2:
+			// A proper VAR=VALUE definiton
+			m[split[0]] = split[1]
+		default:
+			// Anything else is considered malformed and ignored
+			break
+		}
+	}
+
+	return m
+}
+
+func fromMap(m map[string]string) []string {
+	s := []string{}
+
+	for k, v := range m {
+		s = append(s, k+"="+v)
+	}
+	sort.StringSlice(s).Sort()
+
+	return s
 }
