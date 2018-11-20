d0d36d6ca8cb8b38c3b0f53a961b3c8ff2f2ced7
Cyril Cressent <cyril.cressent@sensu.io>
2018-11-20 15:47:40 -0800

    Merge the assets and handler environments
    Signed-off-by: Cyril Cressent <cyril.cressent@sensu.io>

--- backend/pipelined/handle.go
+++ backend/pipelined/handle.go
@@ -13,6 +13,7 @@ import (
 	"github.com/sensu/sensu-go/command"
 	"github.com/sensu/sensu-go/rpc"
 	"github.com/sensu/sensu-go/types"
+	"github.com/sensu/sensu-go/util/environment"
 	utillogging "github.com/sensu/sensu-go/util/logging"
 	"github.com/sirupsen/logrus"
 )
@@ -202,7 +203,7 @@ func (p *Pipelined) pipeHandler(handler *types.Handler, eventData []byte) (*comm
 		if err != nil {
 			logger.WithFields(fields).WithError(err).Error("failed to retrieve assets for handler")
 		} else {
-			handlerExec.Env = append(handlerExec.Env, assets.Env()...)
+			handlerExec.Env = environment.MergeEnvironments(assets.Env(), handlerExec.Env)
 		}
 	}
 
