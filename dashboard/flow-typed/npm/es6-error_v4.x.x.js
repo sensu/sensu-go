ad6abaa13c793cdd4eaa679a5be21af65c6d69ff
Neal Granger <neal@nealg.com>
2018-11-21 13:43:07 -0800

    [Web UI] Improve `NetworkError: undefined` message  (#2379)
    * Set up flow type checker
    
    Add configuration to the repo to support running flow. This does not introduce any changes to existing tests and will not affect CI. Flow is only active when run manually in development. Files without a `// @flow` pragma comment are fully ignored, allowing us to enable type checking on a per-file basis.
    
    Signed-off-by: Neal Granger <neal@nealg.com>
    
    * Improve `networkError: undefined` message
    
    This doesn't prevent the error from occurring, but does help make the messaging less cryptic.
    
    Signed-off-by: Neal Granger <neal@nealg.com>

---
+++ dashboard/flow-typed/npm/es6-error_v4.x.x.js
@@ -0,0 +1,8 @@
+// flow-typed signature: 59598aa3b00e126e674391832ba298ce
+// flow-typed version: da30fe6876/es6-error_v4.x.x/flow_>=v0.32.x
+
+declare module "es6-error" {
+  declare class ExtendableError extends Error {}
+
+  declare module.exports: Class<ExtendableError>;
+}
