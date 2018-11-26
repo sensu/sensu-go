8832d5a122eae3b72f89dc59271965c0b0e7a84a
Greg Poirier <greg.istehbest@gmail.com>
2018-11-26 12:46:14 -0800

    Additional controller cleanup
    Signed-off-by: Greg Poirier <greg.istehbest@gmail.com>

--- backend/apid/actions/rolebindings.go
+++ backend/apid/actions/rolebindings.go
@@ -94,19 +94,3 @@ func (a RoleBindingController) List(ctx context.Context) ([]*types.RoleBinding, 
 
 	return results, nil
 }
-
-// Update validates and persists changes to a role binding.
-func (a RoleBindingController) Update(ctx context.Context, role types.RoleBinding) error {
-	if err := a.Store.UpdateRoleBinding(ctx, &role); err != nil {
-		switch err := err.(type) {
-		case *store.ErrNotFound:
-			return NewErrorf(NotFound)
-		case *store.ErrNotValid:
-			return NewErrorf(InvalidArgument)
-		default:
-			return NewError(InternalErr, err)
-		}
-	}
-
-	return nil
-}
