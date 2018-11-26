5b4c9579ba4f1cffc5e1839f331995d6abcaa5dc
James Phillips <jamesdphillips@gmail.com>
2018-11-26 11:05:18 -0800

    add comment
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- cli/client/path.go
+++ cli/client/path.go
@@ -17,6 +17,19 @@ func createBasePath(group, version string, resType ...string) func(...string) st
 	}
 }
 
+// Returns an anonymous function that when given a namesampe and additional path
+// components returns a full path to a endpoint. If namespace is an empty string
+// namespaces prefix is omitted from the resulting path.
+//
+//    pathFn := createNSBasePath("core", "v1", "users")
+//    pathFn("sensu") // /api/core/v1/namespaces/sensu/users
+//    pathFn("sensu", "frank-west") // /api/core/v1/namespaces/sensu/users/frank-west
+//    pathFn("") // /api/core/v1/users
+//
+//    pathFn = createNSBasePath("core", "v1", "rbac", "rules")
+//    pathFn("sensu", "admin") // /api/core/v1/namespaces/sensu/rbac/rules/admin
+//    pathFn("") // /api/core/v1/namespaces/sensu/rbac/rules
+//
 func createNSBasePath(group, version string, resType ...string) func(string, ...string) string {
 	baseComponents := []string{"/api", group, version}
 
