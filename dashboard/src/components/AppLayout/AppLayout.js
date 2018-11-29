8b3a6b930415ebc057d85318e9f259256c90697c
Melissa P <73hl10n@gmail.com>
2018-11-29 11:14:16 -0800

    Remove the banner from applayout
    Signed-off-by: Melissa P <73hl10n@gmail.com>

--- dashboard/src/components/AppLayout/AppLayout.js
+++ dashboard/src/components/AppLayout/AppLayout.js
@@ -5,7 +5,6 @@ import ResizeObserver from "react-resize-observer";
 
 import ToastWell from "/components/relocation/ToastWell";
 import BannerWell from "/components/relocation/BannerWell";
-import RetryConnectionBanner from "/components/relocation/RetryConnectionBanner";
 
 import MobileFullWidthContent from "./MobileFullWidthContent";
 import Context from "./Context";
@@ -160,7 +159,6 @@ class AppLayout extends React.PureComponent {
             <ResizeObserver onResize={this.handleTopBarResize} />
             <div className={classes.topBar}>{topBar}</div>
             <div className={classes.banner}>
-              <RetryConnectionBanner />
               <BannerWell />
             </div>
             <div className={classes.quickNavContainer}>
