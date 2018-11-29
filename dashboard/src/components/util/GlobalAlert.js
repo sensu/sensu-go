245e442defe3cc2e94b0a8886923996df3b391ec
Neal Granger <neal@nealg.com>
2018-11-29 14:02:56 -0800

    [Web UI] Set up GlobalAlert component (#2456)
    This component provides a mechanism for declaring alert banners with life-cycles coupled to the current app state.
    
    Signed-off-by: Neal Granger <neal@nealg.com>

---
+++ dashboard/src/components/util/GlobalAlert.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+
+import BannerSink from "/components/relocation/BannerSink";
+import Banner from "/components/relocation/Banner";
+
+class GlobalAlert extends React.PureComponent {
+  static propTypes = {
+    data: PropTypes.object.isRequired,
+  };
+
+  render() {
+    const { data } = this.props;
+
+    return (
+      <React.Fragment>
+        {data.localNetwork.offline && (
+          <BannerSink>
+            <Banner variant="warning" message="offline" />
+          </BannerSink>
+        )}
+      </React.Fragment>
+    );
+  }
+}
+
+export default graphql(gql`
+  query GlobalAlertQuery {
+    localNetwork @client {
+      offline
+      retry
+    }
+  }
+`)(GlobalAlert);
