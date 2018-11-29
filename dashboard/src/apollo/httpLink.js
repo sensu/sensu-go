23f540c24c79bc4871e7df637fdabf8e16e0e025
Melissa Page <73hl10n@gmail.com>
2018-11-29 11:06:01 -0800

    [Web UI] Network error improvements - offline and retry fields (#2437)
    * Add offline and retry fields to localNetwork, add localNetwork to Query
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * Add resolves for localNetwork
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * Implement mutation for setting offline state
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * Deal with some merge annoyances
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * Minor errors
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * Check for right error
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * syntax
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * Fix bug with retry connection
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>

--- dashboard/src/apollo/httpLink.js
+++ dashboard/src/apollo/httpLink.js
@@ -1,10 +1,35 @@
 import { BatchHttpLink as HttpLink } from "apollo-link-batch-http";
 import doFetch from "/utils/fetch";
+import { FailedError } from "/errors/FetchError";
+import gql from "graphql-tag";
 
-const httpLink = () =>
+const mutation = gql`
+  mutation SetLocalNetworkOfflineMutation($offline: Boolean!) {
+    setLocalNetworkOffline(offline: $offline) @client
+  }
+`;
+
+const httpLink = ({ getClient }) =>
   new HttpLink({
     uri: "/graphql",
-    fetch: doFetch,
+    fetch: (url, init) =>
+      doFetch(url, init).then(
+        response => {
+          getClient().mutate({
+            mutation,
+            variables: { offline: false },
+          });
+          return response;
+        },
+        error => {
+          getClient().mutate({
+            mutation,
+            variables: { offline: error instanceof FailedError },
+          });
+
+          throw error;
+        },
+      ),
     credentials: "same-origin",
     batchMax: 25,
     batchInterval: 3,
