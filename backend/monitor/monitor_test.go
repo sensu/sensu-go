efe4a28469e2fda9e29d6bbdc0dfa5d6380efdd7
Eric Chlebek <eric@sensu.io>
2018-12-03 11:23:07 -0800

    Add a test that proves monitor prefix efficacy
    This commit adds a test in the monitor package that would time out
    if it were applied before the previous commit was made. (Assuming
    adjustments for prefix.)
    
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- backend/monitor/monitor_test.go
+++ backend/monitor/monitor_test.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/coreos/etcd/clientv3"
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
@@ -199,4 +200,72 @@ func TestWatchMonPut(t *testing.T) {
 	_, err = client.Put(context.Background(), mon.key, "test value")
 	require.NoError(t, err)
 	shutdownWait.Wait()
+}
+
+func newBlockingHandler() *blockingHandler {
+	return &blockingHandler{
+		executed: make(chan struct{}, 1),
+	}
+}
+
+type blockingHandler struct {
+	executed chan struct{}
+}
+
+func (r *blockingHandler) HandleFailure(e *corev2.Event) error {
+	r.executed <- struct{}{}
+	return nil
+}
+
+func (r *blockingHandler) HandleError(err error) {
+	panic(err)
+}
+
+func TestWritesDoNotConflict_GH2470(t *testing.T) {
+	t.Parallel()
+	e, cleanup := etcd.NewTestEtcd(t)
+	defer cleanup()
+	client, err := e.NewClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Close()
+
+	// Create two supervisors with different prefixes
+	facA := EtcdFactory(client, "A")
+	facB := EtcdFactory(client, "B")
+
+	handlerA := newBlockingHandler()
+	handlerB := newBlockingHandler()
+
+	superA := facA(handlerA)
+	superB := facB(handlerB)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		// Refresh the monitor forever, it should never fail
+		for {
+			err := superA.Monitor(ctx, "key", corev2.FixtureEvent("foo", "bar"), 1)
+			if err != nil && ctx.Err() == nil {
+				panic(err)
+			}
+			time.Sleep(10 * time.Millisecond)
+		}
+	}()
+
+	// Monitor the same key with the other supervisor, but allow this monitor to fail
+	err = superB.Monitor(context.Background(), "key", corev2.FixtureEvent("foo", "bar"), 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	select {
+	case <-time.After(10 * time.Second):
+		t.Fatal("test timed out")
+	case <-handlerA.executed:
+		t.Fatal("handler A should never execute")
+	case <-handlerB.executed:
+	}
 }
