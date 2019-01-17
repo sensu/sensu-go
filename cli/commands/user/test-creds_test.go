4ac58e419ad7d1b574a68082cd5b94421fc8f94e
Sean Porter <portertech@gmail.com>
2019-01-17 09:29:22 -0800

    removed commented out imports in test creds cli tests
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- cli/commands/user/test-creds_test.go
+++ cli/commands/user/test-creds_test.go
@@ -3,11 +3,8 @@ package user
 import (
 	"testing"
 
-	//	clientmock "github.com/sensu/sensu-go/cli/client/testing"
 	test "github.com/sensu/sensu-go/cli/commands/testing"
 	"github.com/stretchr/testify/assert"
-	//	"github.com/stretchr/testify/mock"
-	//	"github.com/stretchr/testify/require"
 )
 
 func TestTestCredsCommand(t *testing.T) {
