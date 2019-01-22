33077826981e72572b078ac684bd166819bb7e5f
Melissa Page <73hl10n@gmail.com>
2019-01-22 09:43:35 -0800

    [Web UI] Updated toolbar and hover buttons for dark mode (#2615)
    * unsetting colour allows it to be changed with the theme
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * Update changelog
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * Use theme colour instead of unset
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * Fix colour for disclosure
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * make hover disclosure match colour of resolve
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * Make hover buttons consistent colour
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * update changelog
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * Reenable text for button
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * Linter issue
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>
    
    * remove line that's unneeded
    
    Signed-off-by: Melissa P <73hl10n@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -24,6 +24,8 @@ An error is now logged instead, and extraction continues after a bad line is enc
 - Keepalive events will now continue to fire after cluster restarts.
 - Fixed a panic in the dashboardd shutdown routine.
 - Fixed a bug where deleting a non-existent entity with sensuctl would not return an error.
+- Web UI - toolbar menu buttons now switch with dark theme
+- Web UI - some buttons easier to see with dark theme
 
 ### Changed
 - Improved logging for errors in proxy check requests.
--- dashboard/src/components/partials/TableToolbarCell/FloatingTableToolbarCell.js
+++ dashboard/src/components/partials/TableToolbarCell/FloatingTableToolbarCell.js
@@ -1,11 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { withStyles } from "@material-ui/core/styles";
 import Media from "react-media";
 import TableToolbarCell from "./TableToolbarCell";
 
+const styles = theme => ({
+  soft: {
+    color: theme.palette.text.secondary,
+  },
+});
+
 class FloatingTableToolbarCell extends React.Component {
   static propTypes = {
+    // the linter isn't smart enough to see we use this
+    // eslint-disable-next-line
+    classes: PropTypes.object.isRequired,
     disabled: PropTypes.bool,
     hovered: PropTypes.bool,
     children: PropTypes.func.isRequired,
@@ -21,7 +31,11 @@ class FloatingTableToolbarCell extends React.Component {
     const disabled = disabledProp || (canHover && !hovered);
 
     return (
-      <TableToolbarCell floating={canHover} disabled={disabled}>
+      <TableToolbarCell
+        className={props.classes.soft}
+        floating={canHover}
+        disabled={disabled}
+      >
         {children}
       </TableToolbarCell>
     );
@@ -36,4 +50,4 @@ class FloatingTableToolbarCell extends React.Component {
   }
 }
 
-export default FloatingTableToolbarCell;
+export default withStyles(styles)(FloatingTableToolbarCell);
