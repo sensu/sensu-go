0499ef98c8e0bc871c84db9cacfb42963b5d554c
Simon Plourde <simon@sensu.io>
2019-01-28 10:53:09 -0500

    Use unicode funcs in snakeCase func
    Signed-off-by: Simon Plourde <simon@sensu.io>

--- scripts/make_typemap/make_typemap.go
+++ scripts/make_typemap/make_typemap.go
@@ -10,6 +10,8 @@ import (
 	"os/exec"
 	"regexp"
 	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
 var (
@@ -30,19 +32,26 @@ func snakeCase(camelCase string) string {
 		// Treat acronyms as single-word, e.g. LDAP -> ldap
 		var nextCharCaseChanges bool
 		if i+1 < len(camelCase) {
-			nextChar := camelCase[i+1]
+			nextChar, _ := utf8.DecodeRune([]byte{camelCase[i+1]})
 			// Check if the next character case differs from the current character
-			if (s >= 'a' && s <= 'a' && nextChar >= 'A' && nextChar <= 'Z') || (s >= 'A' && s <= 'Z' && nextChar >= 'a' && nextChar <= 'z') {
+			if (unicode.IsLower(s) && unicode.IsUpper(nextChar)) || (unicode.IsUpper(s) && unicode.IsLower(nextChar)) {
 				nextCharCaseChanges = true
 			}
 		}
 
 		// Add an underscore before the previous character only if it's not the
 		// first character, the next character case changes and we don't already
-		// have an underscore there
-		if i > 0 && nextCharCaseChanges && camelCase[i-1] != '_' {
-			result = append(result, '_')
-			result = append(result, []rune(tl)...)
+		// have an underscore in the result rune
+		if i > 0 && nextCharCaseChanges && result[len(result)-1] != '_' {
+			// Prepend the underscore if the next character is lowercase, otherwise
+			// append it
+			if unicode.IsUpper(s) {
+				result = append(result, '_')
+				result = append(result, []rune(tl)...)
+			} else if unicode.IsLower(s) {
+				result = append(result, []rune(tl)...)
+				result = append(result, '_')
+			}
 		} else {
 			result = append(result, []rune(tl)...)
 		}
