6ad15c9386d4371b211903d15fb926463f38bbef
James Phillips <jamesdphillips@gmail.com>
2019-01-28 09:28:20 -0800

    add silence / unsilence to event details page
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- dashboard/src/components/partials/EventDetailsContainer/EventDetailsToolbar.js
+++ dashboard/src/components/partials/EventDetailsContainer/EventDetailsToolbar.js
@@ -5,12 +5,16 @@ import gql from "graphql-tag";
 import DeleteMenuItem from "/components/partials/ToolbarMenuItems/Delete";
 import QueueMenuItem from "/components/partials/ToolbarMenuItems/QueueExecution";
 import ResolveMenuItem from "/components/partials/ToolbarMenuItems/Resolve";
+import SilenceMenuItem from "/components/partials/ToolbarMenuItems/Silence";
+import UnsilenceMenuItem from "/components/partials/ToolbarMenuItems/Unsilence";
 import Toolbar from "/components/partials/Toolbar";
 import ToolbarMenu from "/components/partials/ToolbarMenu";
 
 import DeleteAction from "./EventDetailsDeleteAction";
 import ResolveAction from "./EventDetailsResolveAction";
 import ReRunAction from "./EventDetailsReRunAction";
+import SilenceAction from "./EventDetailsSilenceAction";
+import UnsilenceAction from "./EventDetailsUnsilenceAction";
 
 class EventDetailsToolbar extends React.Component {
   static propTypes = {
@@ -23,11 +27,19 @@ class EventDetailsToolbar extends React.Component {
         ...EventDetailsDeleteAction_event
         ...EventDetailsResolveAction_event
         ...EventDetailsReRunAction_event
+        ...EventDetailsSilenceAction_event
+        ...EventDetailsUnsilenceAction_event
+
+        check {
+          silenced
+        }
       }
 
       ${DeleteAction.fragments.event}
       ${ResolveAction.fragments.event}
       ${ReRunAction.fragments.event}
+      ${SilenceAction.fragments.event}
+      ${UnsilenceAction.fragments.event}
     `,
   };
 
@@ -56,6 +68,17 @@ class EventDetailsToolbar extends React.Component {
                     />
                   )}
                 </ReRunAction>
+              )}
+            </ToolbarMenu.Item>
+            <ToolbarMenu.Item id="silence" visible="if-room">
+              {event.check.silenced.length === 0 ? (
+                <SilenceAction event={event}>
+                  {run => (
+                    <SilenceMenuItem
+                      onClick={run}
+                    />
+                  )}
+                </SilenceAction>
               )}
             </ToolbarMenu.Item>
             <ToolbarMenu.Item id="delete" visible="never">
