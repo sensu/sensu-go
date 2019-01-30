e0d282805b96e43b3c7121ab9c97ed2eb152364f
Justin Kolberg <amd.prophet@gmail.com>
2019-01-30 11:32:40 -0800

    remove packaging
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- build/ci/circle-ci-setup.sh
+++ build/ci/circle-ci-setup.sh
@@ -10,14 +10,5 @@ set -e
 set -x
 
 apt-get update
-apt-get install -y ruby ruby-dev build-essential rpm rpmlint wget git curl gcc
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
-apt-get install -y nodejs
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update
-apt-get install yarn
-# See https://github.com/jordansissel/fpm/issues/1592
-gem install childprocess -v 0.9.0
-gem install --no-ri --no-rdoc fpm
+apt-get install -y build-essential git
 apt-get clean
