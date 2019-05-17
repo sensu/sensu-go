d31640fa0ec5afb46c0d9fb64a1ea596875824ec
Simon Plourde <simon@sensu.io>
2019-05-17 11:16:01 -0400

    Use proto GenericObject for store tests
    Signed-off-by: Simon Plourde <simon@sensu.io>

--- backend/store/etcd/generic_object_test.go
+++ backend/store/etcd/generic_object_test.go
@@ -1,4 +1,16 @@
 package etcd
 
-//go:generate -command protoc protoc --gofast_out=plugins:. -I=../../../vendor/ -I=./  -I=../../../api/core/v2/ -I=../../../api/core/v2/meta.proto -I=../../../vendor/github.com/gogo/protobuf/protobuf/
+//go:generate -command protoc protoc --gofast_out=plugins:. -I=${GOPATH}/src:. -I=../../../vendor/ -I=./ -I=../../../vendor/github.com/gogo/protobuf/protobuf/
 //go:generate protoc generic_object.proto
+
+func (g *GenericObject) GetNamespace() string {
+	return g.Namespace
+}
+
+func (g *GenericObject) URIPath() string {
+	return ""
+}
+
+func (g *GenericObject) Validate() error {
+	return nil
+}
