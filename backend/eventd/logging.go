8b8da51835ae977515eb290579134fc95b4221c4
Simon Plourde <simon@sensu.io>
2019-05-21 11:29:17 -0400

    Add stop method to Logger
    Signed-off-by: Simon Plourde <simon@sensu.io>

--- backend/eventd/logging.go
+++ backend/eventd/logging.go
@@ -2,11 +2,15 @@ package eventd
 
 // Logger ...
 type Logger interface {
+	Stop()
 	Write(v interface{})
 }
 
 // RawLogger ...
 type RawLogger struct{}
+
+// Stop ...
+func (l *RawLogger) Stop() { return }
 
 // Write ...
 func (l *RawLogger) Write(v interface{}) { return }
