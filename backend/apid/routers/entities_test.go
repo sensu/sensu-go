464081588b836e8ca62097f611dbacd23dfb4596
Eric Chlebek <eric@sensu.io>
2019-06-18 13:57:20 -0700

    Fixup tests
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- backend/apid/routers/entities_test.go
+++ backend/apid/routers/entities_test.go
@@ -6,12 +6,16 @@ import (
 	"github.com/gorilla/mux"
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
 	"github.com/sensu/sensu-go/testing/mockstore"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestEntitiesRouter(t *testing.T) {
 	// Setup the router
 	s := &mockstore.MockStore{}
-	router := NewEntitiesRouter(s)
+	s.On("GetEventsByEntity", mock.Anything, "foo", mock.Anything).Return([]*corev2.Event{corev2.FixtureEvent("foo", "bar")}, nil)
+	s.On("DeleteEventByEntityCheck", mock.Anything, "foo", "bar").Return(nil)
+	s.On("DeleteEntityByName", mock.Anything, "foo").Return(nil)
+	router := NewEntitiesRouter(s, s)
 	parentRouter := mux.NewRouter().PathPrefix(corev2.URLPrefix).Subrouter()
 	router.Mount(parentRouter)
 
@@ -23,7 +27,11 @@ func TestEntitiesRouter(t *testing.T) {
 	tests = append(tests, listTestCases(empty)...)
 	tests = append(tests, createTestCases(empty)...)
 	tests = append(tests, updateTestCases(fixture)...)
-	tests = append(tests, deleteTestCases(fixture)...)
+	// NB: we avoid some of the generic deletion tests here since they
+	// are incompatible with the specialized deletion logic of the entity
+	// controller.
+	tests = append(tests, deleteResourceInvalidPathTestCase(fixture))
+	tests = append(tests, deleteResourceSuccessTestCase(fixture))
 	for _, tt := range tests {
 		run(t, tt, parentRouter, s)
 	}
