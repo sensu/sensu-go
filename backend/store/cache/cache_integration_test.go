4fcb2df90ba4f96a366e5f143aca89d3ada7e820
Simon Plourde <simon@sensu.io>
2019-06-28 13:23:39 -0400

    Part 2
    Signed-off-by: Simon Plourde <simon@sensu.io>

--- backend/store/cache/cache_integration_test.go
+++ backend/store/cache/cache_integration_test.go
@@ -97,7 +97,6 @@ func TestEntityCacheIntegration(t *testing.T) {
 	if err := store.UpdateEntity(ctx, newEntity); err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println("updated")
 	<-watcher
 
 	got := cache.Get("default")
