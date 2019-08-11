fba65893171c50532367e4dee0da8553a82e3245
Tony Lykke <tl@hudson-trading.com>
2019-08-11 12:41:49 -0400

    drop event for deleted entities
    Signed-off-by: Tony Lykke <tl@hudson-trading.com>

--- backend/agentd/entity.go
+++ backend/agentd/entity.go
@@ -7,6 +7,12 @@ import (
 	"github.com/sensu/sensu-go/types"
 )
 
+type ErrDeletedEntity string
+
+func (e ErrDeletedEntity) Error() string {
+	return fmt.Sprintf("%s was deleted after this event was scheduled, dropping event", string(e))
+}
+
 // addEntitySubscription appends the entity subscription (using the format
 // "entity:entityName") to the subscriptions of an entity
 func addEntitySubscription(entityName string, subscriptions []string) []string {
@@ -17,7 +23,7 @@ func addEntitySubscription(entityName string, subscriptions []string) []string {
 // getProxyEntity verifies if a proxy entity name was provided in the given event and if
 // so, retrieves the corresponding entity in the store in order to replace the
 // event's entity with it. In case no entity exists, we create an entity with
-// the proxy class
+// the proxy class only if the event doesn't already have an entity attached.
 func getProxyEntity(event *types.Event, s SessionStore) error {
 	ctx := context.WithValue(context.Background(), types.NamespaceKey, event.Entity.Namespace)
 
@@ -29,8 +35,26 @@ func getProxyEntity(event *types.Event, s SessionStore) error {
 			return fmt.Errorf("could not query the store for a proxy entity: %s", err)
 		}
 
-		// Check if an entity was found for this proxy entity. If not, we need to create it
+		// Check if an entity was found for this proxy entity.
+		// If not, we may need to create it.
+		// There are a few possible scenarios here:
+		//   1: Entity exists in event and in store (normal case, doesn't reach this point)
+		//   2: Entity exists in event and not in store (ie: a proxy check was scheduled
+		//      for this entity, and the entity was deleted between when the check was scheduled
+		//      and when the result was received). We should not create a new entity in this case.
+		//   3: Entity does not exist in event or the store (ie: check was not scheduled by
+		//      the backend, but was instead submitted via the agent's API). We should
+		//      create a new entity in this case.
 		if entity == nil {
+			// case 2: event has an entity but it doesn't exist in the store anymore
+			// because it was deleted.
+			if event.Entity != nil {
+				return ErrDeletedEntity(event.Check.ProxyEntityName)
+			}
+
+			// case 3: event has no entity, and neither does the store. This event was
+			// submitted via the agent API, not scheduled by the backend, so we can
+			// safely create an entity for it.
 			entity = &types.Entity{
 				EntityClass:   types.EntityProxyClass,
 				Subscriptions: addEntitySubscription(event.Check.ProxyEntityName, []string{}),
