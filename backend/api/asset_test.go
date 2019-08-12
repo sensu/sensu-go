8311dee5acf09da15c3d70b32c5b56460474de4d
Eric Chlebek <eric@sensu.io>
2019-08-12 12:25:32 -0700

    100% test coverage for AssetClient
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- backend/api/asset_test.go
+++ backend/api/asset_test.go
@@ -6,20 +6,25 @@ import (
 	"testing"
 
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
+	"github.com/sensu/sensu-go/backend/authorization"
 	"github.com/sensu/sensu-go/backend/authorization/rbac"
 	"github.com/sensu/sensu-go/backend/store"
 	"github.com/sensu/sensu-go/testing/mockstore"
+	"github.com/stretchr/testify/mock"
 )
 
 func defaultContext() context.Context {
 	return store.NamespaceContext(context.Background(), "default")
 }
 
+var defaultAsset = corev2.FixtureAsset("default")
+
 func TestListAssets(t *testing.T) {
 	tests := []struct {
 		Name   string
 		Ctx    func() context.Context
 		Store  func() store.Store
+		Auth   func() authorization.Authorizer
 		Exp    []*corev2.Asset
 		ExpErr bool
 	}{
@@ -30,14 +35,97 @@ func TestListAssets(t *testing.T) {
 				store := new(mockstore.MockStore)
 				return store
 			},
+			Auth: func() authorization.Authorizer {
+				return &rbac.Authorizer{}
+			},
 			ExpErr: true,
+		},
+		{
+			Name: "wrong user",
+			Ctx: func() context.Context {
+				return contextWithUser(defaultContext(), "haxor", nil)
+			},
+			Store: func() store.Store {
+				return new(mockstore.MockStore)
+			},
+			Auth: func() authorization.Authorizer {
+				auth := &mockAuth{
+					attrs: map[authorization.AttributesKey]bool{
+						authorization.AttributesKey{
+							APIGroup:   "core",
+							APIVersion: "v2",
+							Namespace:  "default",
+							Resource:   "assets",
+							UserName:   "legit",
+							Verb:       "list",
+						}: true,
+					},
+				}
+				return auth
+			},
+			ExpErr: true,
+		},
+		{
+			Name: "right user, wrong perms",
+			Ctx: func() context.Context {
+				return contextWithUser(defaultContext(), "haxor", nil)
+			},
+			Store: func() store.Store {
+				return new(mockstore.MockStore)
+			},
+			Auth: func() authorization.Authorizer {
+				auth := &mockAuth{
+					attrs: map[authorization.AttributesKey]bool{
+						authorization.AttributesKey{
+							APIGroup:   "core",
+							APIVersion: "v2",
+							Namespace:  "default",
+							Resource:   "assets",
+							UserName:   "legit",
+							Verb:       "create",
+						}: true,
+					},
+				}
+				return auth
+			},
+			ExpErr: true,
+		},
+		{
+			Name: "good auth",
+			Ctx: func() context.Context {
+				return contextWithUser(defaultContext(), "legit", nil)
+			},
+			Store: func() store.Store {
+				store := new(mockstore.MockStore)
+				store.On("ListResources", mock.Anything, (&corev2.Asset{}).StorePrefix(), mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+					arg := args.Get(2).(*[]*corev2.Asset)
+					*arg = []*corev2.Asset{defaultAsset}
+				}).Return(nil)
+				return store
+			},
+			Auth: func() authorization.Authorizer {
+				auth := &mockAuth{
+					attrs: map[authorization.AttributesKey]bool{
+						authorization.AttributesKey{
+							APIGroup:   "core",
+							APIVersion: "v2",
+							Namespace:  "default",
+							Resource:   "assets",
+							UserName:   "legit",
+							Verb:       "list",
+						}: true,
+					},
+				}
+				return auth
+			},
+			Exp: []*corev2.Asset{defaultAsset},
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.Name, func(t *testing.T) {
 			ctx := test.Ctx()
 			store := test.Store()
-			auth := &rbac.Authorizer{Store: store}
+			auth := test.Auth()
 			client := NewAssetClient(store, auth)
 			assets, err := client.ListAssets(ctx)
 			if err != nil && !test.ExpErr {
@@ -48,6 +136,365 @@ func TestListAssets(t *testing.T) {
 			}
 			if got, want := assets, test.Exp; !reflect.DeepEqual(got, want) {
 				t.Fatalf("bad assets: got %v, want %v", got, want)
+			}
+		})
+	}
+}
+
+func TestGetAsset(t *testing.T) {
+	tests := []struct {
+		Name   string
+		Ctx    func() context.Context
+		Store  func() store.Store
+		Auth   func() authorization.Authorizer
+		Exp    *corev2.Asset
+		ExpErr bool
+	}{
+		{
+			Name: "no auth",
+			Ctx:  defaultContext,
+			Store: func() store.Store {
+				store := new(mockstore.MockStore)
+				return store
+			},
+			Auth: func() authorization.Authorizer {
+				return &rbac.Authorizer{}
+			},
+			ExpErr: true,
+		},
+		{
+			Name: "wrong user",
+			Ctx: func() context.Context {
+				return contextWithUser(defaultContext(), "haxor", nil)
+			},
+			Store: func() store.Store {
+				return new(mockstore.MockStore)
+			},
+			Auth: func() authorization.Authorizer {
+				auth := &mockAuth{
+					attrs: map[authorization.AttributesKey]bool{
+						authorization.AttributesKey{
+							APIGroup:     "core",
+							APIVersion:   "v2",
+							Namespace:    "default",
+							Resource:     "assets",
+							ResourceName: "default",
+							UserName:     "legit",
+							Verb:         "get",
+						}: true,
+					},
+				}
+				return auth
+			},
+			ExpErr: true,
+		},
+		{
+			Name: "right user, wrong perms",
+			Ctx: func() context.Context {
+				return contextWithUser(defaultContext(), "haxor", nil)
+			},
+			Store: func() store.Store {
+				return new(mockstore.MockStore)
+			},
+			Auth: func() authorization.Authorizer {
+				auth := &mockAuth{
+					attrs: map[authorization.AttributesKey]bool{
+						authorization.AttributesKey{
+							APIGroup:     "core",
+							APIVersion:   "v2",
+							Namespace:    "default",
+							Resource:     "assets",
+							ResourceName: "default",
+							UserName:     "legit",
+							Verb:         "create",
+						}: true,
+					},
+				}
+				return auth
+			},
+			ExpErr: true,
+		},
+		{
+			Name: "good auth",
+			Ctx: func() context.Context {
+				return contextWithUser(defaultContext(), "legit", nil)
+			},
+			Store: func() store.Store {
+				store := new(mockstore.MockStore)
+				store.On("GetResource", mock.Anything, "default", mock.Anything).Run(func(args mock.Arguments) {
+					arg := args.Get(2).(*corev2.Asset)
+					*arg = *defaultAsset
+				}).Return(nil)
+				return store
+			},
+			Auth: func() authorization.Authorizer {
+				auth := &mockAuth{
+					attrs: map[authorization.AttributesKey]bool{
+						authorization.AttributesKey{
+							APIGroup:     "core",
+							APIVersion:   "v2",
+							Namespace:    "default",
+							Resource:     "assets",
+							ResourceName: "default",
+							UserName:     "legit",
+							Verb:         "get",
+						}: true,
+					},
+				}
+				return auth
+			},
+			Exp: defaultAsset,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			ctx := test.Ctx()
+			store := test.Store()
+			auth := test.Auth()
+			client := NewAssetClient(store, auth)
+			assets, err := client.FetchAsset(ctx, "default")
+			if err != nil && !test.ExpErr {
+				t.Fatal(err)
+			}
+			if err == nil && test.ExpErr {
+				t.Fatal("expected non-nil error")
+			}
+			if got, want := assets, test.Exp; !reflect.DeepEqual(got, want) {
+				t.Fatalf("bad assets: got %v, want %v", got, want)
+			}
+		})
+	}
+}
+
+func TestCreateAsset(t *testing.T) {
+	tests := []struct {
+		Name   string
+		Ctx    func() context.Context
+		Store  func() store.Store
+		Auth   func() authorization.Authorizer
+		ExpErr bool
+	}{
+		{
+			Name: "no auth",
+			Ctx:  defaultContext,
+			Store: func() store.Store {
+				store := new(mockstore.MockStore)
+				return store
+			},
+			Auth: func() authorization.Authorizer {
+				return &rbac.Authorizer{}
+			},
+			ExpErr: true,
+		},
+		{
+			Name: "wrong user",
+			Ctx: func() context.Context {
+				return contextWithUser(defaultContext(), "haxor", nil)
+			},
+			Store: func() store.Store {
+				return new(mockstore.MockStore)
+			},
+			Auth: func() authorization.Authorizer {
+				auth := &mockAuth{
+					attrs: map[authorization.AttributesKey]bool{
+						authorization.AttributesKey{
+							APIGroup:     "core",
+							APIVersion:   "v2",
+							Namespace:    "default",
+							Resource:     "assets",
+							ResourceName: "default",
+							UserName:     "legit",
+							Verb:         "create",
+						}: true,
+					},
+				}
+				return auth
+			},
+			ExpErr: true,
+		},
+		{
+			Name: "right user, wrong perms",
+			Ctx: func() context.Context {
+				return contextWithUser(defaultContext(), "haxor", nil)
+			},
+			Store: func() store.Store {
+				return new(mockstore.MockStore)
+			},
+			Auth: func() authorization.Authorizer {
+				auth := &mockAuth{
+					attrs: map[authorization.AttributesKey]bool{
+						authorization.AttributesKey{
+							APIGroup:     "core",
+							APIVersion:   "v2",
+							Namespace:    "default",
+							Resource:     "assets",
+							ResourceName: "default",
+							UserName:     "legit",
+							Verb:         "get",
+						}: true,
+					},
+				}
+				return auth
+			},
+			ExpErr: true,
+		},
+		{
+			Name: "good auth",
+			Ctx: func() context.Context {
+				return contextWithUser(defaultContext(), "legit", nil)
+			},
+			Store: func() store.Store {
+				store := new(mockstore.MockStore)
+				store.On("CreateResource", mock.Anything, defaultAsset).Return(nil)
+				return store
+			},
+			Auth: func() authorization.Authorizer {
+				auth := &mockAuth{
+					attrs: map[authorization.AttributesKey]bool{
+						authorization.AttributesKey{
+							APIGroup:     "core",
+							APIVersion:   "v2",
+							Namespace:    "default",
+							Resource:     "assets",
+							ResourceName: "default",
+							UserName:     "legit",
+							Verb:         "create",
+						}: true,
+					},
+				}
+				return auth
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			ctx := test.Ctx()
+			store := test.Store()
+			auth := test.Auth()
+			client := NewAssetClient(store, auth)
+			err := client.CreateAsset(ctx, defaultAsset)
+			if err != nil && !test.ExpErr {
+				t.Fatal(err)
+			}
+			if err == nil && test.ExpErr {
+				t.Fatal("expected non-nil error")
+			}
+		})
+	}
+}
+
+func TestUpdateAsset(t *testing.T) {
+	tests := []struct {
+		Name   string
+		Ctx    func() context.Context
+		Store  func() store.Store
+		Auth   func() authorization.Authorizer
+		ExpErr bool
+	}{
+		{
+			Name: "no auth",
+			Ctx:  defaultContext,
+			Store: func() store.Store {
+				store := new(mockstore.MockStore)
+				return store
+			},
+			Auth: func() authorization.Authorizer {
+				return &rbac.Authorizer{}
+			},
+			ExpErr: true,
+		},
+		{
+			Name: "wrong user",
+			Ctx: func() context.Context {
+				return contextWithUser(defaultContext(), "haxor", nil)
+			},
+			Store: func() store.Store {
+				return new(mockstore.MockStore)
+			},
+			Auth: func() authorization.Authorizer {
+				auth := &mockAuth{
+					attrs: map[authorization.AttributesKey]bool{
+						authorization.AttributesKey{
+							APIGroup:     "core",
+							APIVersion:   "v2",
+							Namespace:    "default",
+							Resource:     "assets",
+							ResourceName: "default",
+							UserName:     "legit",
+							Verb:         "update",
+						}: true,
+					},
+				}
+				return auth
+			},
+			ExpErr: true,
+		},
+		{
+			Name: "right user, wrong perms",
+			Ctx: func() context.Context {
+				return contextWithUser(defaultContext(), "haxor", nil)
+			},
+			Store: func() store.Store {
+				return new(mockstore.MockStore)
+			},
+			Auth: func() authorization.Authorizer {
+				auth := &mockAuth{
+					attrs: map[authorization.AttributesKey]bool{
+						authorization.AttributesKey{
+							APIGroup:     "core",
+							APIVersion:   "v2",
+							Namespace:    "default",
+							Resource:     "assets",
+							ResourceName: "default",
+							UserName:     "legit",
+							Verb:         "get",
+						}: true,
+					},
+				}
+				return auth
+			},
+			ExpErr: true,
+		},
+		{
+			Name: "good auth",
+			Ctx: func() context.Context {
+				return contextWithUser(defaultContext(), "legit", nil)
+			},
+			Store: func() store.Store {
+				store := new(mockstore.MockStore)
+				store.On("CreateOrUpdateResource", mock.Anything, defaultAsset).Return(nil)
+				return store
+			},
+			Auth: func() authorization.Authorizer {
+				auth := &mockAuth{
+					attrs: map[authorization.AttributesKey]bool{
+						authorization.AttributesKey{
+							APIGroup:     "core",
+							APIVersion:   "v2",
+							Namespace:    "default",
+							Resource:     "assets",
+							ResourceName: "default",
+							UserName:     "legit",
+							Verb:         "update",
+						}: true,
+					},
+				}
+				return auth
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			ctx := test.Ctx()
+			store := test.Store()
+			auth := test.Auth()
+			client := NewAssetClient(store, auth)
+			err := client.UpdateAsset(ctx, defaultAsset)
+			if err != nil && !test.ExpErr {
+				t.Fatal(err)
+			}
+			if err == nil && test.ExpErr {
+				t.Fatal("expected non-nil error")
 			}
 		})
 	}
