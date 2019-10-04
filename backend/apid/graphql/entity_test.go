sensu/sensu-go

commit c5a0ffe027a45f944ded4c337ffc1aec0ef9ed91

[GraphQL] Sanitize proxy entity labels & annotations (#3275)
Signed-off-by: James Phillips <jamesdphillips@gmail.com>

backend/apid/graphql/entity.go
@@ -32,10 +32,10 @@ func (*entityImpl) ID(p graphql.ResolveParams) (string, error) {
 	return globalid.EntityTranslator.EncodeToString(p.Source), nil
 }
 
-// ExtendedAttributes implements response to request for 'extendedAttributes' field.
-func (*entityImpl) ExtendedAttributes(p graphql.ResolveParams) (interface{}, error) {
+// Metadata implements response to request for 'metadata' field.
+func (*entityImpl) Metadata(p graphql.ResolveParams) (interface{}, error) {
 	entity := p.Source.(*corev2.Entity)
-	return wrapExtendedAttributes(entity.ExtendedAttributes), nil
+	return entity.GetRedactedEntity(), nil
 }
 
 // LastSeen implements response to request for 'executed' field.

backend/apid/graphql/scalars.go
@@ -46,13 +46,6 @@ func (w jsonWrapper) MarshalJSON() ([]byte, error) {
 	return w.data, nil
 }
 
-func wrapExtendedAttributes(j []byte) jsonWrapper {
-	if len(j) == 0 {
-		j = []byte("{}")
-	}
-	return jsonWrapper{data: j}
-}
-
 // Implement ScalarResolver for JSON type
 
 type unsignedIntegerImpl struct{}

backend/apid/graphql/check.go
@@ -29,12 +29,6 @@ func (r *checkCfgImpl) ID(p graphql.ResolveParams) (string, error) {
 	return globalid.CheckTranslator.EncodeToString(p.Source), nil
 }
 
-// ExtendedAttributes implements response to request for 'extendedAttributes' field.
-func (*checkCfgImpl) ExtendedAttributes(p graphql.ResolveParams) (interface{}, error) {
-	check := p.Source.(*corev2.CheckConfig)
-	return wrapExtendedAttributes(check.ExtendedAttributes), nil
-}
-
 // Handlers implements response to request for 'handlers' field.
 func (r *checkCfgImpl) Handlers(p graphql.ResolveParams) (interface{}, error) {
 	src := p.Source.(*corev2.CheckConfig)
@@ -145,12 +139,6 @@ func (r *checkImpl) NodeID(p graphql.ResolveParams) (string, error) {
 func (r *checkImpl) Executed(p graphql.ResolveParams) (time.Time, error) {
 	c := p.Source.(*corev2.Check)
 	return time.Unix(c.Executed, 0), nil
-}
-
-// ExtendedAttributes implements response to request for 'extendedAttributes' field.
-func (*checkImpl) ExtendedAttributes(p graphql.ResolveParams) (interface{}, error) {
-	check := p.Source.(*corev2.Check)
-	return wrapExtendedAttributes(check.ExtendedAttributes), nil
 }
 
 // LastOK implements response to request for 'lastOK' field.

backend/apid/graphql/schema/asset.graphql
@@ -1,7 +1,7 @@
 """
 Asset defines an archive, an agent will install as a dependency for a check.
 """
-type Asset implements Node, Namespaced, Resource {
+type Asset implements Node & Namespaced & Resource {
   "The globally unique identifier of the record"
   id: ID!
 

backend/apid/graphql/schema/check.gql.go
@@ -58,12 +58,6 @@ type CheckConfigEnvVarsFieldResolver interface {
 	EnvVars(p graphql.ResolveParams) ([]string, error)
 }
 
-// CheckConfigExtendedAttributesFieldResolver implement to resolve requests for the CheckConfig's extendedAttributes field.
-type CheckConfigExtendedAttributesFieldResolver interface {
-	// ExtendedAttributes implements response to request for extendedAttributes field.
-	ExtendedAttributes(p graphql.ResolveParams) (interface{}, error)
-}
-
 // CheckConfigHandlersFieldResolver implement to resolve requests for the CheckConfig's handlers field.
 type CheckConfigHandlersFieldResolver interface {
 	// Handlers implements response to request for handlers field.
@@ -248,7 +242,6 @@ type CheckConfigFieldResolvers interface {
 	CheckConfigCheckHooksFieldResolver
 	CheckConfigCronFieldResolver
 	CheckConfigEnvVarsFieldResolver
-	CheckConfigExtendedAttributesFieldResolver
 	CheckConfigHandlersFieldResolver
 	CheckConfigHighFlapThresholdFieldResolver
 	CheckConfigIntervalFieldResolver
@@ -405,12 +398,6 @@ func (_ CheckConfigAliases) EnvVars(p graphql.ResolveParams) ([]string, error) {
 		return ret, errors.New("unable to coerce value for field 'envVars'")
 	}
 	return ret, err
-}
-
-// ExtendedAttributes implements response to request for 'extendedAttributes' field.
-func (_ CheckConfigAliases) ExtendedAttributes(p graphql.ResolveParams) (interface{}, error) {
-	val, err := graphql.DefaultResolver(p.Source, p.Info.FieldName)
-	return val, err
 }
 
 // Handlers implements response to request for 'handlers' field.
@@ -674,13 +661,6 @@ func _ObjTypeCheckConfigEnvVarsHandler(impl interface{}) graphql1.FieldResolveFn
 	}
 }
 
-func _ObjTypeCheckConfigExtendedAttributesHandler(impl interface{}) graphql1.FieldResolveFn {
-	resolver := impl.(CheckConfigExtendedAttributesFieldResolver)
-	return func(frp graphql1.ResolveParams) (interface{}, error) {
-		return resolver.ExtendedAttributes(frp)
-	}
-}
-
 func _ObjTypeCheckConfigHandlersHandler(impl interface{}) graphql1.FieldResolveFn {
 	resolver := impl.(CheckConfigHandlersFieldResolver)
 	return func(frp graphql1.ResolveParams) (interface{}, error) {
@@ -845,13 +825,6 @@ func _ObjectTypeCheckConfigConfigFn() graphql1.ObjectConfig {
 				Description:       "EnvVars is the list of environment variables to set for the check's\nexecution environment.",
 				Name:              "envVars",
 				Type:              graphql1.NewNonNull(graphql1.NewList(graphql1.NewNonNull(graphql1.String))),
-			},
-			"extendedAttributes": &graphql1.Field{
-				Args:              graphql1.FieldConfigArgument{},
-				DeprecationReason: "",
-				Description:       "ExtendedAttributes store serialized arbitrary JSON-encoded data",
-				Name:              "extendedAttributes",
-				Type:              graphql1.NewNonNull(graphql.OutputType("JSON")),
 			},
 			"handlers": &graphql1.Field{
 				Args:              graphql1.FieldConfigArgument{},
@@ -1040,7 +1013,6 @@ var _ObjectTypeCheckConfigDesc = graphql.ObjectDesc{
 		"command":              _ObjTypeCheckConfigCommandHandler,
 		"cron":                 _ObjTypeCheckConfigCronHandler,
 		"envVars":              _ObjTypeCheckConfigEnvVarsHandler,
-		"extendedAttributes":   _ObjTypeCheckConfigExtendedAttributesHandler,
 		"handlers":             _ObjTypeCheckConfigHandlersHandler,
 		"highFlapThreshold":    _ObjTypeCheckConfigHighFlapThresholdHandler,
 		"id":                   _ObjTypeCheckConfigIDHandler,
@@ -1101,12 +1073,6 @@ type CheckCheckHooksFieldResolver interface {
 type CheckEnvVarsFieldResolver interface {
 	// EnvVars implements response to request for envVars field.
 	EnvVars(p graphql.ResolveParams) ([]string, error)
-}
-
-// CheckExtendedAttributesFieldResolver implement to resolve requests for the Check's extendedAttributes field.
-type CheckExtendedAttributesFieldResolver interface {
-	// ExtendedAttributes implements response to request for extendedAttributes field.
-	ExtendedAttributes(p graphql.ResolveParams) (interface{}, error)
 }
 
 // CheckHandlersFieldResolver implement to resolve requests for the Check's handlers field.
@@ -1386,7 +1352,6 @@ type CheckFieldResolvers interface {
 	CheckCommandFieldResolver
 	CheckCheckHooksFieldResolver
 	CheckEnvVarsFieldResolver
-	CheckExtendedAttributesFieldResolver
 	CheckHandlersFieldResolver
 	CheckHighFlapThresholdFieldResolver
 	CheckIntervalFieldResolver
@@ -1531,12 +1496,6 @@ func (_ CheckAliases) EnvVars(p graphql.ResolveParams) ([]string, error) {
 		return ret, errors.New("unable to coerce value for field 'envVars'")
 	}
 	return ret, err
-}
-
-// ExtendedAttributes implements response to request for 'extendedAttributes' field.
-func (_ CheckAliases) ExtendedAttributes(p graphql.ResolveParams) (interface{}, error) {
-	val, err := graphql.DefaultResolver(p.Source, p.Info.FieldName)
-	return val, err
 }
 
 // Handlers implements response to request for 'handlers' field.
@@ -1950,13 +1909,6 @@ func _ObjTypeCheckEnvVarsHandler(impl interface{}) graphql1.FieldResolveFn {
 	}
 }
 
-func _ObjTypeCheckExtendedAttributesHandler(impl interface{}) graphql1.FieldResolveFn {
-	resolver := impl.(CheckExtendedAttributesFieldResolver)
-	return func(frp graphql1.ResolveParams) (interface{}, error) {
-		return resolver.ExtendedAttributes(frp)
-	}
-}
-
 func _ObjTypeCheckHandlersHandler(impl interface{}) graphql1.FieldResolveFn {
 	resolver := impl.(CheckHandlersFieldResolver)
 	return func(frp graphql1.ResolveParams) (interface{}, error) {
@@ -2240,13 +2192,6 @@ func _ObjectTypeCheckConfigFn() graphql1.ObjectConfig {
 				Name:              "executed",
 				Type:              graphql1.NewNonNull(graphql1.DateTime),
 			},
-			"extendedAttributes": &graphql1.Field{
-				Args:              graphql1.FieldConfigArgument{},
-				DeprecationReason: "",
-				Description:       "ExtendedAttributes store serialized arbitrary JSON-encoded data",
-				Name:              "extendedAttributes",
-				Type:              graphql1.NewNonNull(graphql.OutputType("JSON")),
-			},
 			"handlers": &graphql1.Field{
 				Args:              graphql1.FieldConfigArgument{},
 				DeprecationReason: "",
@@ -2508,7 +2453,6 @@ var _ObjectTypeCheckDesc = graphql.ObjectDesc{
 		"duration":             _ObjTypeCheckDurationHandler,
 		"envVars":              _ObjTypeCheckEnvVarsHandler,
 		"executed":             _ObjTypeCheckExecutedHandler,
-		"extendedAttributes":   _ObjTypeCheckExtendedAttributesHandler,
 		"handlers":             _ObjTypeCheckHandlersHandler,
 		"highFlapThreshold":    _ObjTypeCheckHighFlapThresholdHandler,
 		"history":              _ObjTypeCheckHistoryHandler,

backend/apid/graphql/schema/check.graphql
@@ -1,7 +1,7 @@
 """
 CheckConfig is the specification of a check.
 """
-type CheckConfig implements Node, Namespaced, Silenceable, Resource {
+type CheckConfig implements Node & Namespaced & Silenceable & Resource {
   "The globally unique identifier of the check."
   id: ID!
 
@@ -28,9 +28,6 @@ type CheckConfig implements Node, Namespaced, Silenceable, Resource {
   execution environment.
   """
   envVars: [String!]!
-
-  "ExtendedAttributes store serialized arbitrary JSON-encoded data"
-  extendedAttributes: JSON!
 
   "handlers are the event handler for the check (incidents and/or metrics)."
   handlers: [Handler]!
@@ -116,7 +113,7 @@ type CheckConfig implements Node, Namespaced, Silenceable, Resource {
 A Check is a check specification and optionally the results of the check's
 execution.
 """
-type Check implements Silenceable, Resource {
+type Check implements Silenceable & Resource {
   "id of the check's configuration"
   nodeId: ID!
 
@@ -137,9 +134,6 @@ type Check implements Silenceable, Resource {
   execution environment.
   """
   envVars: [String!]!
-
-  "ExtendedAttributes store serialized arbitrary JSON-encoded data"
-  extendedAttributes: JSON!
 
   "handlers are the event handler for the check (incidents and/or metrics)."
   handlers: [Handler]!

backend/apid/graphql/schema/entity.gql.go
@@ -148,12 +148,6 @@ type EntitySilencesFieldResolver interface {
 	Silences(p graphql.ResolveParams) (interface{}, error)
 }
 
-// EntityExtendedAttributesFieldResolver implement to resolve requests for the Entity's extendedAttributes field.
-type EntityExtendedAttributesFieldResolver interface {
-	// ExtendedAttributes implements response to request for extendedAttributes field.
-	ExtendedAttributes(p graphql.ResolveParams) (interface{}, error)
-}
-
 // EntityToJSONFieldResolver implement to resolve requests for the Entity's toJSON field.
 type EntityToJSONFieldResolver interface {
 	// ToJSON implements response to request for toJSON field.
@@ -239,7 +233,6 @@ type EntityFieldResolvers interface {
 	EntityEventsFieldResolver
 	EntityIsSilencedFieldResolver
 	EntitySilencesFieldResolver
-	EntityExtendedAttributesFieldResolver
 	EntityToJSONFieldResolver
 }
 
@@ -462,12 +455,6 @@ func (_ EntityAliases) Silences(p graphql.ResolveParams) (interface{}, error) {
 	return val, err
 }
 
-// ExtendedAttributes implements response to request for 'extendedAttributes' field.
-func (_ EntityAliases) ExtendedAttributes(p graphql.ResolveParams) (interface{}, error) {
-	val, err := graphql.DefaultResolver(p.Source, p.Info.FieldName)
-	return val, err
-}
-
 // ToJSON implements response to request for 'toJSON' field.
 func (_ EntityAliases) ToJSON(p graphql.ResolveParams) (interface{}, error) {
 	val, err := graphql.DefaultResolver(p.Source, p.Info.FieldName)
@@ -615,13 +602,6 @@ func _ObjTypeEntitySilencesHandler(impl interface{}) graphql1.FieldResolveFn {
 	}
 }
 
-func _ObjTypeEntityExtendedAttributesHandler(impl interface{}) graphql1.FieldResolveFn {
-	resolver := impl.(EntityExtendedAttributesFieldResolver)
-	return func(frp graphql1.ResolveParams) (interface{}, error) {
-		return resolver.ExtendedAttributes(frp)
-	}
-}
-
 func _ObjTypeEntityToJSONHandler(impl interface{}) graphql1.FieldResolveFn {
 	resolver := impl.(EntityToJSONFieldResolver)
 	return func(frp graphql1.ResolveParams) (interface{}, error) {
@@ -671,13 +651,6 @@ func _ObjectTypeEntityConfigFn() graphql1.ObjectConfig {
 				Description:       "All events associated with the entity.",
 				Name:              "events",
 				Type:              graphql1.NewNonNull(graphql1.NewList(graphql1.NewNonNull(graphql.OutputType("Event")))),
-			},
-			"extendedAttributes": &graphql1.Field{
-				Args:              graphql1.FieldConfigArgument{},
-				DeprecationReason: "",
-				Description:       "Extended attributes includes arbitrary user-defined data",
-				Name:              "extendedAttributes",
-				Type:              graphql1.NewNonNull(graphql.OutputType("JSON")),
 			},
 			"id": &graphql1.Field{
 				Args:              graphql1.FieldConfigArgument{},
@@ -803,25 +776,24 @@ func _ObjectTypeEntityConfigFn() graphql1.ObjectConfig {
 var _ObjectTypeEntityDesc = graphql.ObjectDesc{
 	Config: _ObjectTypeEntityConfigFn,
 	FieldHandlers: map[string]graphql.FieldHandler{
-		"deregister":         _ObjTypeEntityDeregisterHandler,
-		"deregistration":     _ObjTypeEntityDeregistrationHandler,
-		"entityClass":        _ObjTypeEntityEntityClassHandler,
-		"events":             _ObjTypeEntityEventsHandler,
-		"extendedAttributes": _ObjTypeEntityExtendedAttributesHandler,
-		"id":                 _ObjTypeEntityIDHandler,
-		"isSilenced":         _ObjTypeEntityIsSilencedHandler,
-		"lastSeen":           _ObjTypeEntityLastSeenHandler,
-		"metadata":           _ObjTypeEntityMetadataHandler,
-		"name":               _ObjTypeEntityNameHandler,
-		"namespace":          _ObjTypeEntityNamespaceHandler,
-		"redact":             _ObjTypeEntityRedactHandler,
-		"related":            _ObjTypeEntityRelatedHandler,
-		"silences":           _ObjTypeEntitySilencesHandler,
-		"status":             _ObjTypeEntityStatusHandler,
-		"subscriptions":      _ObjTypeEntitySubscriptionsHandler,
-		"system":             _ObjTypeEntitySystemHandler,
-		"toJSON":             _ObjTypeEntityToJSONHandler,
-		"user":               _ObjTypeEntityUserHandler,
+		"deregister":     _ObjTypeEntityDeregisterHandler,
+		"deregistration": _ObjTypeEntityDeregistrationHandler,
+		"entityClass":    _ObjTypeEntityEntityClassHandler,
+		"events":         _ObjTypeEntityEventsHandler,
+		"id":             _ObjTypeEntityIDHandler,
+		"isSilenced":     _ObjTypeEntityIsSilencedHandler,
+		"lastSeen":       _ObjTypeEntityLastSeenHandler,
+		"metadata":       _ObjTypeEntityMetadataHandler,
+		"name":           _ObjTypeEntityNameHandler,
+		"namespace":      _ObjTypeEntityNamespaceHandler,
+		"redact":         _ObjTypeEntityRedactHandler,
+		"related":        _ObjTypeEntityRelatedHandler,
+		"silences":       _ObjTypeEntitySilencesHandler,
+		"status":         _ObjTypeEntityStatusHandler,
+		"subscriptions":  _ObjTypeEntitySubscriptionsHandler,
+		"system":         _ObjTypeEntitySystemHandler,
+		"toJSON":         _ObjTypeEntityToJSONHandler,
+		"user":           _ObjTypeEntityUserHandler,
 	},
 }
 

backend/apid/graphql/schema/entity.graphql
@@ -2,7 +2,7 @@
 Entity is the Entity supplying the event. The default Entity for any
 Event is the running Agent process--if the Event is sent by an Agent.
 """
-type Entity implements Node, Namespaced, Silenceable, Resource {
+type Entity implements Node & Namespaced & Silenceable & Resource {
   "The globally unique identifier of the record"
   id: ID!
 
@@ -63,9 +63,6 @@ type Entity implements Node, Namespaced, Silenceable, Resource {
   matches all checks.
   """
   silences: [Silenced!]!
-
-  "Extended attributes includes arbitrary user-defined data"
-  extendedAttributes: JSON!
 
   """
   toJSON returns a REST API compatible representation of the resource. Handy for

commit 4f65ef8fd599a4ff74118c3c2be6054fc664184d

[GraphQL] Fixup entity object meta regression (#3314)
Fixes regression introduced in #3275.

Signed-off-by: James Phillips <jamesdphillips@gmail.com>

backend/apid/graphql/entity.go
@@ -35,7 +35,8 @@ func (*entityImpl) ID(p graphql.ResolveParams) (string, error) {
 // Metadata implements response to request for 'metadata' field.
 func (*entityImpl) Metadata(p graphql.ResolveParams) (interface{}, error) {
 	entity := p.Source.(*corev2.Entity)
-	return entity.GetRedactedEntity(), nil
+	entity = entity.GetRedactedEntity()
+	return entity.GetObjectMeta(), nil
 }
 
 // LastSeen implements response to request for 'executed' field.

backend/apid/graphql/scalars.go
@@ -38,14 +38,6 @@ func (jsonImpl) Serialize(val interface{}) interface{} {
 	return val
 }
 
-type jsonWrapper struct {
-	data []byte
-}
-
-func (w jsonWrapper) MarshalJSON() ([]byte, error) {
-	return w.data, nil
-}
-
 // Implement ScalarResolver for JSON type
 
 type unsignedIntegerImpl struct{}

backend/apid/graphql/entity_test.go
@@ -6,12 +6,23 @@ import (
 	"time"
 
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
+	v2 "github.com/sensu/sensu-go/api/core/v2"
 	"github.com/sensu/sensu-go/backend/apid/graphql/schema"
 	"github.com/sensu/sensu-go/graphql"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
+
+func TestEntityTypeMetadataField(t *testing.T) {
+	src := corev2.FixtureEntity("bug")
+	impl := entityImpl{}
+
+	res, err := impl.Metadata(graphql.ResolveParams{Source: src})
+	require.NoError(t, err)
+	assert.NotEmpty(t, res)
+	assert.IsType(t, v2.ObjectMeta{}, res)
+}
 
 func TestEntityTypeRelatedField(t *testing.T) {
 	source := corev2.FixtureEntity("c")