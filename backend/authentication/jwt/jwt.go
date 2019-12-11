sensu/sensu-go

commit 6df9362dfe57c605608a1da9e384a9900f5dc2a4

Add the 'sensu-backend init' subcommand (#3414)
* Don't seed the cluster automatically

Move seeding to the sensu-backend init command

* Allow setting the admin username/password in init

The sensu-backend init command now accepts --cluster-admin-username and
--cluster-admin-password flags.

These flags are best supplied via a config file, or via environment
variables, for security reasons. For example,

sensu-backend init -c config.yml

Where config.yml contains

cluster-admin-username: username
cluster-admin-password: password

The corresponding environment variables are:

SENSU_BACKEND_CLUSTER_ADMIN_USERNAME
SENSU_BACKEND_CLUSTER_ADMIN_PASSWORD

Signed-off-by: Eric Chlebek <eric@sensu.io>

* Add --etcd-client-urls flag

We were somewhat conflating this flag with
--etcd-advertise-client-urls before, so this commit adds a new flag
called --etcd-client-urls, which falls back to
--etcd-advertise-client-urls if not supplied.

Signed-off-by: Eric Chlebek <eric@sensu.io>

backend/backend.go
@@ -33,7 +33,6 @@ import (
 	"github.com/sensu/sensu-go/backend/queue"
 	"github.com/sensu/sensu-go/backend/ringv2"
 	"github.com/sensu/sensu-go/backend/schedulerd"
-	"github.com/sensu/sensu-go/backend/seeds"
 	"github.com/sensu/sensu-go/backend/store"
 	etcdstore "github.com/sensu/sensu-go/backend/store/etcd"
 	"github.com/sensu/sensu-go/backend/tessend"
@@ -72,10 +71,15 @@ func newClient(config *Config, backend *Backend) (*clientv3.Client, error) {
 			return nil, err
 		}
 
+		clientURLs := config.EtcdClientURLs
+		if len(clientURLs) == 0 {
+			clientURLs = config.EtcdAdvertiseClientURLs
+		}
+
 		// Don't start up an embedded etcd, return a client that connects to an
 		// external etcd instead.
 		return clientv3.New(clientv3.Config{
-			Endpoints:   config.EtcdAdvertiseClientURLs,
+			Endpoints:   clientURLs,
 			DialTimeout: 5 * time.Second,
 			TLS:         tlsConfig,
 		})
@@ -144,17 +148,11 @@ func Initialize(config *Config) (*Backend, error) {
 		return nil, err
 	}
 
-	// Initialize the store, which lives on top of etcd
-	logger.Debug("Initializing store...")
+	// Create the store, which lives on top of etcd
 	stor := etcdstore.NewStore(b.Client, config.EtcdName)
-	if err = seeds.SeedInitialData(stor); err != nil {
-		return nil, fmt.Errorf("error initializing the store: %s", err)
-	}
-	logger.Debug("Done initializing store")
 	b.Store = stor
 
-	_, err = stor.GetClusterID(b.ctx)
-	if err != nil {
+	if _, err := stor.GetClusterID(b.ctx); err != nil {
 		switch err := err.(type) {
 		case *store.ErrNotFound:
 			if storeErr := stor.CreateClusterID(b.ctx, uuid.New().String()); storeErr != nil {
@@ -193,8 +191,8 @@ func Initialize(config *Config) (*Backend, error) {
 
 	// Initialize pipelined
 	pipeline, err := pipelined.New(pipelined.Config{
-		Store:                   stor,
-		Bus:                     bus,
+		Store: stor,
+		Bus:   bus,
 		ExtensionExecutorGetter: rpc.NewGRPCExtensionExecutor,
 		AssetGetter:             assetGetter,
 		BufferSize:              viper.GetInt(FlagPipelinedBufferSize),
@@ -264,13 +262,13 @@ func Initialize(config *Config) (*Backend, error) {
 	// Initialize keepalived
 	keepalive, err := keepalived.New(keepalived.Config{
 		DeregistrationHandler: config.DeregistrationHandler,
-		Bus:                   bus,
-		Store:                 stor,
-		EventStore:            stor,
-		LivenessFactory:       liveness.EtcdFactory(b.ctx, b.Client),
-		RingPool:              ringPool,
-		BufferSize:            viper.GetInt(FlagKeepalivedBufferSize),
-		WorkerCount:           viper.GetInt(FlagKeepalivedWorkers),
+		Bus:             bus,
+		Store:           stor,
+		EventStore:      stor,
+		LivenessFactory: liveness.EtcdFactory(b.ctx, b.Client),
+		RingPool:        ringPool,
+		BufferSize:      viper.GetInt(FlagKeepalivedBufferSize),
+		WorkerCount:     viper.GetInt(FlagKeepalivedWorkers),
 	})
 	if err != nil {
 		return nil, fmt.Errorf("error initializing %s: %s", keepalive.Name(), err)

backend/seeds/seeds.go
@@ -2,6 +2,7 @@ package seeds
 
 import (
 	"context"
+	"time"
 
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
 	"github.com/sensu/sensu-go/backend/authentication/bcrypt"
@@ -10,66 +11,106 @@ import (
 	"github.com/sensu/sensu-go/types"
 )
 
+type Config struct {
+	// AdminUsername is the username of the cluster admin.
+	AdminUsername string
+
+	// AdminPassword is the password of the cluster admin.
+	AdminPassword string
+}
+
+// SeedCluster seeds the cluster according to the provided config.
+func SeedCluster(ctx context.Context, store store.Store, config Config) error {
+	errs := make(chan error, 1)
+	go func() {
+		var err error
+		defer func() {
+			if err != nil {
+				errs <- err
+			}
+			close(errs)
+		}()
+		initializer, err := store.NewInitializer()
+		if err != nil {
+			return
+		}
+		logger := logger.WithField("component", "backend.seeds")
+
+		// Lock initialization key to avoid competing installations
+		if err = initializer.Lock(); err != nil {
+			return
+		}
+		defer func() {
+			e := initializer.Close()
+			if err == nil {
+				err = e
+			}
+		}()
+
+		// Initialize the JWT secret. This method is idempotent and needs to be ran
+		// at every startup so the JWT signatures remain valid
+		if err = jwt.InitSecret(store); err != nil {
+			return
+		}
+
+		// Check that the store hasn't already been seeded
+		initialized, err := initializer.IsInitialized()
+		if err != nil || initialized {
+			return
+		}
+		logger.Info("seeding etcd store w/ intial data")
+
+		// Create the default namespace
+		if err = setupDefaultNamespace(store); err != nil {
+			logger.WithError(err).Error("unable to setup 'default' namespace")
+			return
+		}
+
+		// Create the admin user
+		if err = setupAdminUser(store, config.AdminUsername, config.AdminPassword); err != nil {
+			logger.WithError(err).Error("could not initialize the admin user")
+			return
+		}
+
+		// Create the agent user
+		if err = setupAgentUser(store, "agent", "P@ssw0rd!"); err != nil {
+			logger.WithError(err).Error("could not initialize the agent user")
+			return
+		}
+
+		// Create the default ClusterRoles
+		if err = setupClusterRoles(store); err != nil {
+			logger.WithError(err).Error("could not initialize the default ClusterRoles and Roles")
+			return
+		}
+
+		// Create the default ClusterRoleBindings
+		if err = setupClusterRoleBindings(store); err != nil {
+			logger.WithError(err).Error("could not initialize the default ClusterRoles and Roles")
+			return
+		}
+
+		// Set initialized flag
+		err = initializer.FlagAsInitialized()
+	}()
+	select {
+	case err := <-errs:
+		return err
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
 // SeedInitialData will seed a store with initial data. This method is
 // idempotent and can be safely run every time the backend starts.
 func SeedInitialData(store store.Store) (err error) {
-	initializer, err := store.NewInitializer()
-	if err != nil {
-		return err
+	config := Config{
+		AdminUsername: "admin",
+		AdminPassword: "P@ssw0rd!",
 	}
-	logger := logger.WithField("component", "backend.seeds")
-
-	// Lock initialization key to avoid competing installations
-	if err = initializer.Lock(); err != nil {
-		return err
-	}
-	defer func() {
-		e := initializer.Close()
-		if err == nil {
-			err = e
-		}
-	}()
-
-	// Initialize the JWT secret. This method is idempotent and needs to be ran
-	// at every startup so the JWT signatures remain valid
-	if err := jwt.InitSecret(store); err != nil {
-		return err
-	}
-
-	// Check that the store hasn't already been seeded
-	if initialized, err := initializer.IsInitialized(); err != nil {
-		return err
-	} else if initialized {
-		return nil
-	}
-	logger.Info("seeding etcd store w/ intial data")
-
-	// Create the default namespace
-	if err := setupDefaultNamespace(store); err != nil {
-		logger.WithError(err).Error("unable to setup 'default' namespace")
-		return err
-	}
-
-	// Create the default users
-	if err := setupUsers(store); err != nil {
-		logger.WithError(err).Error("could not initialize the default users")
-		return err
-	}
-
-	// Create the default ClusterRoles
-	if err := setupClusterRoles(store); err != nil {
-		logger.WithError(err).Error("could not initialize the default ClusterRoles and Roles")
-		return err
-	}
-
-	// Create the default ClusterRoleBindings
-	if err := setupClusterRoleBindings(store); err != nil {
-		logger.WithError(err).Error("could not initialize the default ClusterRoles and Roles")
-		return err
-	}
-
-	// Set initialized flag
-	return initializer.FlagAsInitialized()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	return SeedCluster(ctx, store, config)
 }
 
 func setupDefaultNamespace(store store.Store) error {
@@ -254,23 +295,28 @@ func setupClusterRoles(store store.Store) error {
 	return store.CreateClusterRole(context.Background(), systemUser)
 }
 
-func setupUsers(store store.Store) error {
-	hash, err := bcrypt.HashPassword("P@ssw0rd!")
+func setupAdminUser(store store.Store, username, password string) error {
+	hash, err := bcrypt.HashPassword(password)
 	if err != nil {
 		return err
 	}
 
 	admin := &types.User{
-		Username: "admin",
+		Username: username,
 		Password: hash,
 		Groups:   []string{"cluster-admins"},
 	}
-	if err := store.CreateUser(admin); err != nil {
+	return store.CreateUser(admin)
+}
+
+func setupAgentUser(store store.Store, username, password string) error {
+	hash, err := bcrypt.HashPassword("P@ssw0rd!")
+	if err != nil {
 		return err
 	}
 
 	agent := &types.User{
-		Username: "agent",
+		Username: username,
 		Password: hash,
 		Groups:   []string{"system:agents"},
 	}

backend/backend_test.go
@@ -12,6 +12,8 @@ import (
 	"time"
 
 	"github.com/sensu/sensu-go/backend/etcd"
+	"github.com/sensu/sensu-go/backend/seeds"
+	etcdstore "github.com/sensu/sensu-go/backend/store/etcd"
 	"github.com/sensu/sensu-go/testing/testutil"
 	"github.com/sensu/sensu-go/transport"
 	"github.com/sensu/sensu-go/types"
@@ -76,7 +78,7 @@ func TestBackendHTTPListener(t *testing.T) {
 				}
 			}
 
-			b, err := Initialize(&Config{
+			cfg := &Config{
 				AgentHost:        "127.0.0.1",
 				AgentPort:        agentPort,
 				APIListenAddress: fmt.Sprintf("127.0.0.1:%d", apiPort),
@@ -94,10 +96,15 @@ func TestBackendHTTPListener(t *testing.T) {
 				EtcdName:                     "default",
 				EtcdClientTLSInfo:            tlsInfo,
 				EtcdPeerTLSInfo:              tlsInfo,
-			})
-			assert.NoError(t, err)
+			}
+			b, err := Initialize(cfg)
 			if err != nil {
-				assert.FailNow(t, "failed to start backend")
+				t.Fatalf("failed to start backend: %s", err)
+			}
+
+			store := etcdstore.NewStore(b.Client, cfg.EtcdName)
+			if err := seeds.SeedInitialData(store); err != nil {
+				t.Fatal(err)
 			}
 
 			var runError error

backend/cmd/init.go
@@ -1,0 +1,173 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/AlecAivazis/survey"
+	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/pkg/transport"
+	corev2 "github.com/sensu/sensu-go/api/core/v2"
+	"github.com/sensu/sensu-go/backend"
+	"github.com/sensu/sensu-go/backend/etcd"
+	"github.com/sensu/sensu-go/backend/seeds"
+	etcdstore "github.com/sensu/sensu-go/backend/store/etcd"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const (
+	flagInitAdminUsername = "cluster-admin-username"
+	flagInitAdminPassword = "cluster-admin-password"
+	flagInteractive       = "interactive"
+)
+
+type seedConfig struct {
+	backend.Config
+	SeedConfig seeds.Config
+}
+
+type initOpts struct {
+	AdminUsername string `survey:"cluster-admin-username"`
+	AdminPassword string `survey:"cluster-admin-password"`
+}
+
+func (i *initOpts) administerQuestionnaire() error {
+	qs := []*survey.Question{
+		{
+			Name: "cluster-admin-username",
+			Prompt: &survey.Input{
+				Message: "Cluster Admin Username:",
+			},
+			Validate: survey.Required,
+		},
+		{
+			Name: "cluster-admin-password",
+			Prompt: &survey.Password{
+				Message: "Cluster Admin Password:",
+			},
+			Validate: survey.Required,
+		},
+	}
+
+	return survey.Ask(qs, i)
+}
+
+// InitCommand is the 'sensu-backend init' subcommand.
+func InitCommand() *cobra.Command {
+	var setupErr error
+	cmd := &cobra.Command{
+		Use:           "init",
+		Short:         "initialize a new sensu installation",
+		SilenceErrors: true,
+		SilenceUsage:  true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			_ = viper.BindPFlags(cmd.Flags())
+			if setupErr != nil {
+				return setupErr
+			}
+
+			cfg := &backend.Config{
+				EtcdClientURLs:      fallbackStringSlice(flagEtcdClientURLs, flagEtcdAdvertiseClientURLs),
+				EtcdName:            viper.GetString(flagEtcdNodeName),
+				EtcdCipherSuites:    viper.GetStringSlice(flagEtcdCipherSuites),
+				EtcdMaxRequestBytes: viper.GetUint(flagEtcdMaxRequestBytes),
+				NoEmbedEtcd:         true,
+			}
+
+			// Sensu APIs TLS config
+			certFile := viper.GetString(flagCertFile)
+			keyFile := viper.GetString(flagKeyFile)
+			insecureSkipTLSVerify := viper.GetBool(flagInsecureSkipTLSVerify)
+			trustedCAFile := viper.GetString(flagTrustedCAFile)
+
+			if certFile != "" && keyFile != "" {
+				cfg.TLS = &corev2.TLSOptions{
+					CertFile:           certFile,
+					KeyFile:            keyFile,
+					TrustedCAFile:      trustedCAFile,
+					InsecureSkipVerify: insecureSkipTLSVerify,
+				}
+			} else if certFile != "" || keyFile != "" {
+				return fmt.Errorf(
+					"tls configuration error, both flags --%s & --%s are required",
+					flagCertFile, flagKeyFile)
+			}
+
+			// Etcd TLS config
+			cfg.EtcdClientTLSInfo = etcd.TLSInfo{
+				CertFile:       viper.GetString(flagEtcdCertFile),
+				KeyFile:        viper.GetString(flagEtcdKeyFile),
+				TrustedCAFile:  viper.GetString(flagEtcdTrustedCAFile),
+				ClientCertAuth: viper.GetBool(flagEtcdClientCertAuth),
+			}
+
+			// Convert the TLS config into etcd's transport.TLSInfo
+			tlsInfo := (transport.TLSInfo)(cfg.EtcdClientTLSInfo)
+			tlsConfig, err := tlsInfo.ClientConfig()
+			if err != nil {
+				return err
+			}
+
+			clientURLs := viper.GetStringSlice(flagEtcdClientURLs)
+			if len(clientURLs) == 0 {
+				clientURLs = viper.GetStringSlice(flagEtcdAdvertiseClientURLs)
+			}
+
+			client, err := clientv3.New(clientv3.Config{
+				Endpoints:   clientURLs,
+				DialTimeout: 5 * time.Second,
+				TLS:         tlsConfig,
+			})
+
+			if err != nil {
+				return fmt.Errorf("error connecting to cluster: %s", err)
+			}
+
+			uname := viper.GetString(flagInitAdminUsername)
+			pword := viper.GetString(flagInitAdminPassword)
+
+			if viper.GetBool(flagInteractive) {
+				var opts initOpts
+				if err := opts.administerQuestionnaire(); err != nil {
+					return err
+				}
+				uname = opts.AdminUsername
+				pword = opts.AdminPassword
+			}
+
+			if uname == "" || pword == "" {
+				return fmt.Errorf("both %s and %s are required to be set", flagInitAdminUsername, flagInitAdminPassword)
+			}
+
+			seedConfig := seedConfig{
+				Config: *cfg,
+				SeedConfig: seeds.Config{
+					AdminUsername: uname,
+					AdminPassword: pword,
+				},
+			}
+
+			return seedCluster(client, seedConfig)
+		},
+	}
+
+	cmd.Flags().String(flagInitAdminUsername, "", "cluster admin username")
+	cmd.Flags().String(flagInitAdminPassword, "", "cluster admin password")
+	cmd.Flags().Bool(flagInteractive, false, "interactive mode")
+
+	setupErr = handleConfig(cmd, false)
+
+	return cmd
+}
+
+func seedCluster(client *clientv3.Client, config seedConfig) error {
+	store := etcdstore.NewStore(client, config.EtcdName)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	if err := seeds.SeedCluster(ctx, store, config.SeedConfig); err != nil {
+		return fmt.Errorf("error initializing cluster: %s", err)
+	}
+	return nil
+}

backend/config.go
@@ -71,6 +71,7 @@ type Config struct {
 	EtcdInitialClusterToken      string
 	EtcdInitialClusterState      string
 	EtcdInitialCluster           string
+	EtcdClientURLs               []string
 	EtcdListenClientURLs         []string
 	EtcdListenPeerURLs           []string
 	EtcdName                     string

cmd/sensu-backend/main.go
@@ -21,6 +21,7 @@ func main() {
 	}
 	rootCmd.AddCommand(cmd.StartCommand(backend.Initialize))
 	rootCmd.AddCommand(cmd.VersionCommand())
+	rootCmd.AddCommand(cmd.InitCommand())
 
 	if err := rootCmd.Execute(); err != nil {
 		logger.WithError(err).Fatal("error executing sensu-backend")

commit ca97f71169bfc0b7cc2fbb94b937afc147ed4b17

Fix a bug where jwt secret was not initialized
This commit fixes a bug where the jwt secret was not correctly
being loaded on startup. The result was that jwts were being
created with a 0-byte secret.

This regression occurred in commit 6df9362dfe57c605608a1da9e384a9900f5dc2a4

Signed-off-by: Eric Chlebek <eric@sensu.io>

backend/authentication/jwt/jwt.go
@@ -35,6 +35,15 @@ var (
 
 func init() {
 	signingMethod = jwt.SigningMethodHS256
+
+	var err error
+
+	// secret should be initialized and persisted, but in case that fails,
+	// it's critical to have a non-empty jwt secret
+	secret, err = utilbytes.Random(32)
+	if err != nil {
+		panic(err)
+	}
 }
 
 // AccessToken creates a new access token and returns it in both JWT and
@@ -174,26 +183,10 @@ func LoadKeyPair(privatePath, publicPath string) error {
 
 // InitSecret initializes and retrieves the secret for our signing tokens
 func InitSecret(store store.Store) error {
-	var s []byte
-	var err error
-
 	// Retrieve the secret
-	if secret == nil {
-		s, err = store.GetJWTSecret()
-		if err != nil {
-			// The secret does not exist, we need to create one
-			s, err = utilbytes.Random(32)
-			if err != nil {
-				return err
-			}
-
-			// Add the secret to the store
-			err = store.CreateJWTSecret(s)
-			if err != nil {
-				return err
-			}
-		}
-
+	if s, err := store.GetJWTSecret(); err != nil {
+		return err
+	} else {
 		// Set the secret so it's available accross the package
 		secret = s
 	}

backend/backend.go
@@ -154,6 +154,12 @@ func Initialize(config *Config) (*Backend, error) {
 	stor := etcdstore.NewStore(b.Client, config.EtcdName)
 	b.Store = stor
 
+	// Initialize the JWT secret. This method is idempotent and needs to be ran
+	// at every startup so the JWT signatures remain valid
+	if err := jwt.InitSecret(b.Store); err != nil {
+		return nil, err
+	}
+
 	if _, err := stor.GetClusterID(b.ctx); err != nil {
 		switch err := err.(type) {
 		case *store.ErrNotFound:
@@ -193,8 +199,8 @@ func Initialize(config *Config) (*Backend, error) {
 
 	// Initialize pipelined
 	pipeline, err := pipelined.New(pipelined.Config{
-		Store: stor,
-		Bus:   bus,
+		Store:                   stor,
+		Bus:                     bus,
 		ExtensionExecutorGetter: rpc.NewGRPCExtensionExecutor,
 		AssetGetter:             assetGetter,
 		BufferSize:              viper.GetInt(FlagPipelinedBufferSize),
@@ -264,13 +270,13 @@ func Initialize(config *Config) (*Backend, error) {
 	// Initialize keepalived
 	keepalive, err := keepalived.New(keepalived.Config{
 		DeregistrationHandler: config.DeregistrationHandler,
-		Bus:             bus,
-		Store:           stor,
-		EventStore:      stor,
-		LivenessFactory: liveness.EtcdFactory(b.ctx, b.Client),
-		RingPool:        ringPool,
-		BufferSize:      viper.GetInt(FlagKeepalivedBufferSize),
-		WorkerCount:     viper.GetInt(FlagKeepalivedWorkers),
+		Bus:                   bus,
+		Store:                 stor,
+		EventStore:            stor,
+		LivenessFactory:       liveness.EtcdFactory(b.ctx, b.Client),
+		RingPool:              ringPool,
+		BufferSize:            viper.GetInt(FlagKeepalivedBufferSize),
+		WorkerCount:           viper.GetInt(FlagKeepalivedWorkers),
 	})
 	if err != nil {
 		return nil, fmt.Errorf("error initializing %s: %s", keepalive.Name(), err)

backend/seeds/seeds.go
@@ -6,7 +6,6 @@ import (
 
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
 	"github.com/sensu/sensu-go/backend/authentication/bcrypt"
-	"github.com/sensu/sensu-go/backend/authentication/jwt"
 	"github.com/sensu/sensu-go/backend/store"
 	"github.com/sensu/sensu-go/types"
 )
@@ -47,18 +46,12 @@ func SeedCluster(ctx context.Context, store store.Store, config Config) error {
 			}
 		}()
 
-		// Initialize the JWT secret. This method is idempotent and needs to be ran
-		// at every startup so the JWT signatures remain valid
-		if err = jwt.InitSecret(store); err != nil {
-			return
-		}
-
 		// Check that the store hasn't already been seeded
 		initialized, err := initializer.IsInitialized()
 		if err != nil || initialized {
 			return
 		}
-		logger.Info("seeding etcd store w/ intial data")
+		logger.Info("seeding etcd store with intial data")
 
 		// Create the default namespace
 		if err = setupDefaultNamespace(store); err != nil {

backend/store/etcd/authentication.go
@@ -2,48 +2,59 @@ package etcd
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"time"
 
 	"github.com/coreos/etcd/clientv3"
+	"github.com/sensu/sensu-go/backend/store"
+	utilbytes "github.com/sensu/sensu-go/util/bytes"
 )
 
 func getAuthenticationPath(id string) string {
 	return fmt.Sprintf("%s/authentication/%s", EtcdRoot, id)
 }
 
-// CreateJWTSecret creates a new JWT secret
+// CreateJWTSecret creates a new JWT secret. DEPRECATED. Returns non-nil error
+// in all circumstances. Use UpdateJWTSecret to replace an exist jwt secret.
 func (s *Store) CreateJWTSecret(secret []byte) error {
-	// We need to prepare a transaction to verify the version of the key
-	// corresponding to the user in etcd in order to ensure we only put the key
-	// if it does not exist
-	cmp := clientv3.Compare(clientv3.Version(getAuthenticationPath("secret")), "=", 0)
-	req := clientv3.OpPut(getAuthenticationPath("secret"), string(secret))
-	res, err := s.client.Txn(context.TODO()).If(cmp).Then(req).Commit()
-	if err != nil {
-		return err
-	}
-	if !res.Succeeded {
-		return fmt.Errorf("a secret already exist")
-	}
-
-	return nil
+	return errors.New("deprecated method, use GetJWTSecret instead")
 }
 
-// GetJWTSecret retrieves the JWT signing secret
+// GetJWTSecret retrieves the JWT signing secret, creating it if it does not
+// already exist.
 func (s *Store) GetJWTSecret() ([]byte, error) {
-	resp, err := s.client.Get(context.TODO(), getAuthenticationPath("secret"), clientv3.WithLimit(1))
+	randomBytes, err := utilbytes.Random(32)
 	if err != nil {
-		return nil, err
+		return nil, &store.ErrInternal{Message: err.Error()}
 	}
-	if len(resp.Kvs) != 1 {
-		return nil, fmt.Errorf("secret does not exist")
-	}
+	opGet := clientv3.OpGet(getAuthenticationPath("secret"), clientv3.WithLimit(1))
+	opPut := clientv3.OpPut(getAuthenticationPath("secret"), string(randomBytes))
+	cmp := clientv3.Compare(clientv3.Version(getAuthenticationPath("secret")), "=", 0)
 
-	return resp.Kvs[0].Value, nil
+	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*5)
+	defer cancel()
+
+	resp, err := s.client.Txn(ctx).If(cmp).Then(opPut).Else(opGet).Commit()
+	if err != nil {
+		return nil, &store.ErrInternal{Message: err.Error()}
+	}
+	if resp.Succeeded {
+		return randomBytes, nil
+	}
+	getResp := resp.Responses[0].GetResponseRange()
+	if len(getResp.Kvs) != 1 {
+		return nil, &store.ErrInternal{Message: "secret response is empty"}
+	}
+	return getResp.Kvs[0].Value, nil
 }
 
 // UpdateJWTSecret replaces the jwt secret with a new one.
 func (s *Store) UpdateJWTSecret(secret []byte) error {
-	_, err := s.client.Put(context.TODO(), getAuthenticationPath("secret"), string(secret))
-	return err
+	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*5)
+	defer cancel()
+	if _, err := s.client.Put(ctx, getAuthenticationPath("secret"), string(secret)); err != nil {
+		return &store.ErrInternal{Message: err.Error()}
+	}
+	return nil
 }

backend/authentication/jwt/jwt_test.go
@@ -85,17 +85,6 @@ func TestInitSecret(t *testing.T) {
 	assert.NotEqual(t, nil, secret)
 }
 
-func TestInitSecretMissingSecret(t *testing.T) {
-	secret = nil
-	store := &mockstore.MockStore{}
-	store.On("GetJWTSecret").Return("", fmt.Errorf(""))
-	store.On("CreateJWTSecret").Return(nil)
-
-	err := InitSecret(store)
-	assert.NoError(t, err)
-	assert.NotEqual(t, nil, secret)
-}
-
 func TestInitSecretEtcdError(t *testing.T) {
 	secret = nil
 	store := &mockstore.MockStore{}

backend/store/etcd/authentication_test.go
@@ -3,41 +3,59 @@
 package etcd
 
 import (
+	"bytes"
 	"testing"
+	"time"
 
 	"github.com/sensu/sensu-go/backend/store"
 	utilbytes "github.com/sensu/sensu-go/util/bytes"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestAuthenticationStorage(t *testing.T) {
 	testWithEtcd(t, func(store store.Store) {
-		// Secret does not exist
-		_, err := store.GetJWTSecret()
-		assert.Error(t, err)
+		// Secret is created
+		secret, err := store.GetJWTSecret()
+		if err != nil {
+			t.Fatal(err)
+		}
 
-		// Create a secret
-		secret, _ := utilbytes.Random(32)
-		err = store.CreateJWTSecret(secret)
-		assert.NoError(t, err)
+		if got, want := len(secret), 32; got != want {
+			t.Fatalf("bad secret length: got %d, want %d", got, want)
+		}
 
-		// Retrieve the secret
+		time.Sleep(time.Second)
+
+		// Retrieve the secret again, it should be the same
 		result, err := store.GetJWTSecret()
-		assert.NoError(t, err)
-		assert.Equal(t, secret, result)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-		// We should not be able to create it again
-		err = store.CreateJWTSecret(secret)
-		assert.Error(t, err)
+		if got, want := result, secret; !bytes.Equal(got, want) {
+			t.Errorf("bad secret result: got %x, want %x", got, want)
+		}
 
 		// We should be able to update it
-		newSecret, _ := utilbytes.Random(32)
-		err = store.UpdateJWTSecret(newSecret)
-		assert.NoError(t, err)
+		newSecret, err := utilbytes.Random(32)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err := store.UpdateJWTSecret(newSecret); err != nil {
+			t.Fatal(err)
+		}
 
 		// The old and new secrets should not match
 		result, err = store.GetJWTSecret()
-		assert.NoError(t, err)
-		assert.NotEqual(t, result, secret)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if got, want := result, secret; bytes.Equal(got, want) {
+			t.Errorf("bad secret result: got %x, should not equal %x", got, want)
+		}
+		if got, want := result, newSecret; !bytes.Equal(got, want) {
+			t.Errorf("bad secret result: got %x, want %x", got, want)
+		}
 	})
 }