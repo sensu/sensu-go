ce237d0e4716ac19b68c2f8e77af8bfadeeab69b
Eric Chlebek <eric@sensu.io>
2019-12-13 14:37:07 -0800

    Use a much longer timeout for seeding
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- backend/seeds/seeds.go
+++ backend/seeds/seeds.go
@@ -101,7 +101,7 @@ func SeedInitialData(store store.Store) (err error) {
 		AdminUsername: "admin",
 		AdminPassword: "P@ssw0rd!",
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer cancel()
 	return SeedCluster(ctx, store, config)
 }
