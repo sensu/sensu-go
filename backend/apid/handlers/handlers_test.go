7bb0ca2c08a1c6dbe63c648a8bdf7475167b1124
Simon Plourde <simon@sensu.io>
2020-02-20 19:16:49 -0500

    Fix event payload validation on the backend events API (#3575)
    Signed-off-by: Simon Plourde <simon@sensu.io>

--- backend/apid/handlers/handlers_test.go
+++ backend/apid/handlers/handlers_test.go
@@ -35,7 +35,7 @@ func TestCheckMeta(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := CheckMeta(tt.resource, tt.vars); (err != nil) != tt.wantErr {
+			if err := CheckMeta(tt.resource, tt.vars, "id"); (err != nil) != tt.wantErr {
 				t.Errorf("checkMeta() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
