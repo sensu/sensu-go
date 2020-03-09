sensu/sensu-go

commit 572b4b5facc4a4f7b310dcf0eb34ef711e9583e5

Restart backend daemons on store failure (#3446)
* Stop and restart daemons on error

When the backend error group receives an error, the sensu-backend
now stops and restarts all of its internal daemons.

Previously, if a daemon wrote an error to its error channel, the
entire service would stop running.

This change should make it easier to ensure that the system does
not end up in a half-working state. If an daemon writes an error
to its error channel, the rest of the daemons will stop and
restart.

* Return structured errors from the store

This commit modifies the store to return structured errors for
each error condition. The intent is that the user of the store
will always be able to determine the reason for store failure.

If ErrInternal is returned, the user can assume that the error is
not related to the request, but rather is an error emanating from
the underlying database.

The intent is that consumers will be able to distinguish between
request format errors, and errors that would represent a loss of
service.

* Emit fatal errors on store ErrInternal

This commit modifies agentd, eventd, keepalived, and pipelined to
emit fatal errors to the backend supervisor, when they receive
ErrInternal on any store method.

This will result in the shutdown and restart of all daemons, which
will give the system a "clean slate" to start from, and will also
sever all existing agent connections.

If the backend is experiencing trouble and can't use the store,
this gives agents a chance to connect to other systems that might
be able to offer service.

* Refactor pipelined

This commit moves the pipeline business logic out of pipelined and
into a new package called pipeline.

The intent is that future work will use the pipeline directly.

* Terminate sessions when ErrInternal is seen
* Use the retry package for restarting the daemons
* Add store timeouts to several daemons
* Fix a bug where tessend did not completely stop

backend/agentd/agentd.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"strings"
 	"sync"
@@ -29,6 +30,9 @@ var (
 	// upgrader is safe for concurrent use, and we don't need any particularly
 	// specialized configurations for different uses.
 	upgrader = &websocket.Upgrader{}
+
+	// used for registering prometheus session counter
+	sessionCounterOnce sync.Once
 )
 
 // Agentd is the backend HTTP API.
@@ -39,17 +43,18 @@ type Agentd struct {
 	// Port is the port Agentd is running on.
 	Port int
 
-	stopping   chan struct{}
-	running    *atomic.Value
-	wg         *sync.WaitGroup
-	errChan    chan error
-	httpServer *http.Server
-	store      store.Store
-	bus        messaging.MessageBus
-	tls        *corev2.TLSOptions
-	ringPool   *ringv2.Pool
-	ctx        context.Context
-	cancel     context.CancelFunc
+	stopping     chan struct{}
+	running      *atomic.Value
+	wg           *sync.WaitGroup
+	errChan      chan error
+	httpServer   *http.Server
+	store        store.Store
+	bus          messaging.MessageBus
+	tls          *corev2.TLSOptions
+	ringPool     *ringv2.Pool
+	ctx          context.Context
+	cancel       context.CancelFunc
+	writeTimeout int
 }
 
 // Config configures an Agentd.
@@ -70,18 +75,19 @@ type Option func(*Agentd) error
 func New(c Config, opts ...Option) (*Agentd, error) {
 	ctx, cancel := context.WithCancel(context.Background())
 	a := &Agentd{
-		Host:     c.Host,
-		Port:     c.Port,
-		bus:      c.Bus,
-		store:    c.Store,
-		tls:      c.TLS,
-		stopping: make(chan struct{}, 1),
-		running:  &atomic.Value{},
-		wg:       &sync.WaitGroup{},
-		errChan:  make(chan error, 1),
-		ringPool: c.RingPool,
-		ctx:      ctx,
-		cancel:   cancel,
+		Host:         c.Host,
+		Port:         c.Port,
+		bus:          c.Bus,
+		store:        c.Store,
+		tls:          c.TLS,
+		stopping:     make(chan struct{}, 1),
+		running:      &atomic.Value{},
+		wg:           &sync.WaitGroup{},
+		errChan:      make(chan error, 1),
+		ringPool:     c.RingPool,
+		ctx:          ctx,
+		cancel:       cancel,
+		writeTimeout: c.WriteTimeout,
 	}
 
 	// prepare server TLS config
@@ -125,6 +131,11 @@ func New(c Config, opts ...Option) (*Agentd, error) {
 // Start Agentd.
 func (a *Agentd) Start() error {
 	logger.Info("starting agentd on address: ", a.httpServer.Addr)
+	ln, err := net.Listen("tcp", a.httpServer.Addr)
+	if err != nil {
+		return fmt.Errorf("failed to start agentd: %s", err)
+	}
+
 	a.wg.Add(1)
 
 	go func() {
@@ -132,16 +143,21 @@ func (a *Agentd) Start() error {
 		var err error
 		if a.tls != nil {
 			// TLS configuration comes from ToServerTLSConfig
-			err = a.httpServer.ListenAndServeTLS("", "")
+			err = a.httpServer.ServeTLS(ln, "", "")
 		} else {
-			err = a.httpServer.ListenAndServe()
+			err = a.httpServer.Serve(ln)
 		}
 		if err != nil && err != http.ErrServerClosed {
-			logger.WithError(err).Error("failed to start http/https server")
+			a.errChan <- fmt.Errorf("agentd failed while serving: %s", err)
 		}
 	}()
 
-	_ = prometheus.Register(sessionCounter)
+	sessionCounterOnce.Do(func() {
+		if err := prometheus.Register(sessionCounter); err != nil {
+			logger.WithError(err).Error("error registering session counter")
+			a.errChan <- err
+		}
+	})
 
 	return nil
 }
@@ -212,6 +228,7 @@ func (a *Agentd) webSocketHandler(w http.ResponseWriter, r *http.Request) {
 		Subscriptions: strings.Split(r.Header.Get(transport.HeaderKeySubscriptions), ","),
 		RingPool:      a.ringPool,
 		ContentType:   contentType,
+		WriteTimeout:  a.writeTimeout,
 	}
 
 	cfg.Subscriptions = addEntitySubscription(cfg.AgentName, cfg.Subscriptions)
@@ -220,13 +237,27 @@ func (a *Agentd) webSocketHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		logger.WithError(err).Error("failed to create session")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		// There was an error retrieving the namespace from
+		// etcd, indicating that this backend has a potentially
+		// unrecoverable issue.
+		if _, ok := err.(*store.ErrInternal); ok {
+			select {
+			case a.errChan <- err:
+			case <-a.ctx.Done():
+			}
+		}
 		return
 	}
 
-	err = session.Start()
-	if err != nil {
+	if err := session.Start(); err != nil {
 		logger.WithError(err).Error("failed to start session")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		if _, ok := err.(*store.ErrInternal); ok {
+			select {
+			case a.errChan <- err:
+			case <-a.ctx.Done():
+			}
+		}
 		return
 	}
 }
@@ -244,6 +275,14 @@ func (a *Agentd) AuthenticationMiddleware(next http.Handler) http.Handler {
 		// Authenticate against the provider
 		user, err := a.store.AuthenticateUser(r.Context(), username, password)
 		if err != nil {
+			if _, ok := err.(*store.ErrInternal); ok {
+				select {
+				case a.errChan <- err:
+				case <-a.ctx.Done():
+				}
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				return
+			}
 			logger.
 				WithField("user", username).
 				WithError(err).
@@ -294,7 +333,15 @@ func (a *Agentd) AuthorizationMiddleware(next http.Handler) http.Handler {
 		}
 		authorized, err := auth.Authorize(ctx, attrs)
 		if err != nil {
-			logger.WithError(err).Error("unexpected error while authorization the session")
+			if _, ok := err.(*store.ErrInternal); ok {
+				select {
+				case a.errChan <- err:
+				case <-a.ctx.Done():
+				}
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				return
+			}
+			logger.WithError(err).Error("unexpected error while authorizing the session")
 			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
 			return
 		}

backend/backend.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/coreos/etcd/clientv3"
 	"github.com/coreos/etcd/pkg/transport"
-	"github.com/google/uuid"
 	"github.com/prometheus/client_golang/prometheus"
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
 	"github.com/sensu/sensu-go/asset"
@@ -40,9 +39,19 @@ import (
 	"github.com/sensu/sensu-go/backend/tessend"
 	"github.com/sensu/sensu-go/rpc"
 	"github.com/sensu/sensu-go/system"
+	"github.com/sensu/sensu-go/util/retry"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 )
+
+type ErrStartup struct {
+	Err  error
+	Name string
+}
+
+func (e ErrStartup) Error() string {
+	return fmt.Sprintf("error starting %s: %s", e.Name, e.Err)
+}
 
 // Backend represents the backend server, which is used to hold the datastore
 // and coordinating the daemons
@@ -55,9 +64,10 @@ type Backend struct {
 	GraphQLService         *graphql.Service
 	SecretsProviderManager *secrets.ProviderManager
 
-	done   chan struct{}
-	ctx    context.Context
-	cancel context.CancelFunc
+	ctx       context.Context
+	runCtx    context.Context
+	runCancel context.CancelFunc
+	cfg       *Config
 }
 
 // EventStoreUpdater offers a way to update an event store to a different
@@ -68,6 +78,7 @@ type EventStoreUpdater interface {
 
 func newClient(ctx context.Context, config *Config, backend *Backend) (*clientv3.Client, error) {
 	if config.NoEmbedEtcd {
+		logger.Info("dialing etcd server")
 		tlsInfo := (transport.TLSInfo)(config.EtcdClientTLSInfo)
 		tlsConfig, err := tlsInfo.ClientConfig()
 		if err != nil {
@@ -157,13 +168,13 @@ func newClient(ctx context.Context, config *Config, backend *Backend) (*clientv3
 // configuring etcd and establishing a list of daemons, which constitute our
 // backend. The daemons will later be started according to their position in the
 // b.Daemons list, and stopped in reverse order
-func Initialize(config *Config) (*Backend, error) {
+func Initialize(ctx context.Context, config *Config) (*Backend, error) {
 	var err error
 	// Initialize a Backend struct
-	b := &Backend{}
+	b := &Backend{cfg: config}
 
-	b.done = make(chan struct{})
-	b.ctx, b.cancel = context.WithCancel(context.Background())
+	b.ctx = ctx
+	b.runCtx, b.runCancel = context.WithCancel(b.ctx)
 
 	b.Client, err = newClient(b.ctx, config, b)
 	if err != nil {
@@ -174,29 +185,24 @@ func Initialize(config *Config) (*Backend, error) {
 	stor := etcdstore.NewStore(b.Client, config.EtcdName)
 	b.Store = stor
 
+	if _, err := stor.GetClusterID(b.runCtx); err != nil {
+		return nil, err
+	}
+
 	// Initialize the JWT secret. This method is idempotent and needs to be ran
 	// at every startup so the JWT signatures remain valid
 	if err := jwt.InitSecret(b.Store); err != nil {
 		return nil, err
 	}
 
-	if _, err := stor.GetClusterID(b.ctx); err != nil {
-		switch err := err.(type) {
-		case *store.ErrNotFound:
-			if storeErr := stor.CreateClusterID(b.ctx, uuid.New().String()); storeErr != nil {
-				return nil, fmt.Errorf("error assigning a sensu cluster id: %s", err)
-			}
-		default:
-			return nil, fmt.Errorf("error retrieving sensu cluster id: %s", err)
-		}
-	}
-
 	eventStoreProxy := store.NewEventStoreProxy(stor)
 	b.EventStore = eventStoreProxy
 
 	logger.Debug("Registering backend...")
-	backendID := etcd.NewBackendIDGetter(b.ctx, b.Client)
+
+	backendID := etcd.NewBackendIDGetter(b.runCtx, b.Client)
 	logger.Debug("Done registering backend.")
+	b.Daemons = append(b.Daemons, backendID)
 
 	// Initialize an etcd getter
 	queueGetter := queue.EtcdGetter{Client: b.Client, BackendIDGetter: backendID}
@@ -212,7 +218,7 @@ func Initialize(config *Config) (*Backend, error) {
 	backendEntity := b.getBackendEntity(config)
 	logger.WithField("entity", backendEntity).Info("backend entity information")
 	assetManager := asset.NewManager(config.CacheDir, backendEntity, &sync.WaitGroup{})
-	assetGetter, err := assetManager.StartAssetManager(b.ctx)
+	assetGetter, err := assetManager.StartAssetManager(b.runCtx)
 	if err != nil {
 		return nil, fmt.Errorf("error initializing asset manager: %s", err)
 	}
@@ -228,6 +234,7 @@ func Initialize(config *Config) (*Backend, error) {
 		AssetGetter:             assetGetter,
 		BufferSize:              viper.GetInt(FlagPipelinedBufferSize),
 		WorkerCount:             viper.GetInt(FlagPipelinedWorkers),
+		StoreTimeout:            2 * time.Minute,
 		SecretsProviderManager:  b.SecretsProviderManager,
 	})
 	if err != nil {
@@ -237,15 +244,16 @@ func Initialize(config *Config) (*Backend, error) {
 
 	// Initialize eventd
 	event, err := eventd.New(
-		b.ctx,
+		b.runCtx,
 		eventd.Config{
 			Store:           stor,
 			EventStore:      eventStoreProxy,
 			Bus:             bus,
-			LivenessFactory: liveness.EtcdFactory(b.ctx, b.Client),
+			LivenessFactory: liveness.EtcdFactory(b.runCtx, b.Client),
 			Client:          b.Client,
 			BufferSize:      viper.GetInt(FlagEventdBufferSize),
 			WorkerCount:     viper.GetInt(FlagEventdWorkers),
+			StoreTimeout:    2 * time.Minute,
 		},
 	)
 	if err != nil {
@@ -257,7 +265,7 @@ func Initialize(config *Config) (*Backend, error) {
 
 	// Initialize schedulerd
 	scheduler, err := schedulerd.New(
-		b.ctx,
+		b.runCtx,
 		schedulerd.Config{
 			Store:                  stor,
 			Bus:                    bus,
@@ -298,10 +306,11 @@ func Initialize(config *Config) (*Backend, error) {
 		Bus:                   bus,
 		Store:                 stor,
 		EventStore:            stor,
-		LivenessFactory:       liveness.EtcdFactory(b.ctx, b.Client),
+		LivenessFactory:       liveness.EtcdFactory(b.runCtx, b.Client),
 		RingPool:              ringPool,
 		BufferSize:            viper.GetInt(FlagKeepalivedBufferSize),
 		WorkerCount:           viper.GetInt(FlagKeepalivedWorkers),
+		StoreTimeout:          2 * time.Minute,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("error initializing %s: %s", keepalive.Name(), err)
@@ -381,7 +390,7 @@ func Initialize(config *Config) (*Backend, error) {
 
 	// Initialize tessend
 	tessen, err := tessend.New(
-		b.ctx,
+		b.runCtx,
 		tessend.Config{
 			Store:    stor,
 			RingPool: ringPool,
@@ -423,8 +432,24 @@ func Initialize(config *Config) (*Backend, error) {
 	return b, nil
 }
 
-// Run starts all of the Backend server's daemons
-func (b *Backend) Run() error {
+func (b *Backend) runOnce() error {
+	var derr error
+
+	if b.Etcd != nil {
+		defer func() {
+			logger.Info("shutting down etcd")
+			if err := recover(); err != nil {
+				trace := string(debug.Stack())
+				logger.WithField("panic", trace).WithError(err.(error)).
+					Error("recovering from panic due to error, shutting down etcd")
+			}
+			err := b.Etcd.Shutdown()
+			if derr == nil {
+				derr = err
+			}
+		}()
+	}
+
 	eg := errGroup{
 		out: make(chan error),
 	}
@@ -433,7 +458,7 @@ func (b *Backend) Run() error {
 	// Loop across the daemons in order to start them, then add them to our groups
 	for _, d := range b.Daemons {
 		if err := d.Start(); err != nil {
-			return fmt.Errorf("error starting %s: %s", d.Name(), err)
+			return ErrStartup{Err: err, Name: d.Name()}
 		}
 
 		// Add the daemon to our errGroup
@@ -461,39 +486,74 @@ func (b *Backend) Run() error {
 
 	select {
 	case err := <-eg.Err():
-		logger.WithError(err).Error("error in error group")
-	case <-b.ctx.Done():
+		logger.WithError(err).Error("backend stopped working and is restarting")
+	case <-b.runCtx.Done():
 		logger.Info("backend shutting down")
 	}
-
-	var derr error
-
 	if err := sg.Stop(); err != nil {
 		if derr == nil {
 			derr = err
 		}
 	}
 
-	if b.Etcd != nil {
-		logger.Info("shutting down etcd")
-		defer func() {
-			if err := recover(); err != nil {
-				trace := string(debug.Stack())
-				logger.WithField("panic", trace).WithError(err.(error)).
-					Error("recovering from panic due to error, shutting down etcd")
-			}
-			err := b.Etcd.Shutdown()
-			if derr == nil {
-				derr = err
-			}
-		}()
+	if derr == nil {
+		derr = b.runCtx.Err()
 	}
 
+	return derr
+}
+
+// Run starts all of the Backend server's daemons
+func (b *Backend) Run() error {
 	// we allow inErrChan to leak to avoid panics from other
 	// goroutines writing errors to either after shutdown has been initiated.
-	close(b.done)
+	backoff := retry.ExponentialBackoff{
+		Ctx:                  b.ctx,
+		InitialDelayInterval: time.Second,
+		MaxDelayInterval:     time.Second,
+		Multiplier:           1,
+	}
 
-	return derr
+	err := backoff.Retry(func(int) (bool, error) {
+		err := b.runOnce()
+		b.Stop()
+		if err != nil {
+			if b.ctx.Err() != nil {
+				logger.Warn("shutting down")
+				return true, b.ctx.Err()
+			}
+			logger.Error(err)
+			if _, ok := err.(ErrStartup); ok {
+				return true, err
+			}
+		}
+
+		// Yes, two levels of retry... this could improve. Unfortunately Intialize()
+		// is called elsewhere.
+		err = backoff.Retry(func(int) (bool, error) {
+			backend, err := Initialize(b.ctx, b.cfg)
+			if err != nil && err != context.Canceled {
+				logger.Error(err)
+				return false, nil
+			} else if err == context.Canceled {
+				return true, err
+			}
+			// Replace b with a new backend - this is done to ensure that there is
+			// no side effects from the execution of b that have carried over
+			*b = *backend
+			return true, nil
+		})
+		if err != nil {
+			return true, err
+		}
+		return false, nil
+	})
+
+	if err == context.Canceled {
+		return nil
+	}
+
+	return err
 }
 
 type stopper interface {
@@ -542,8 +602,7 @@ func (e errGroup) Err() <-chan error {
 
 // Stop the Backend cleanly.
 func (b *Backend) Stop() {
-	b.cancel()
-	<-b.done
+	b.runCancel()
 }
 
 func (b *Backend) getBackendEntity(config *Config) *corev2.Entity {

backend/agentd/entity.go
@@ -2,7 +2,6 @@ package agentd
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/sensu/sensu-go/types"
 )
@@ -26,7 +25,7 @@ func getProxyEntity(event *types.Event, s SessionStore) error {
 		// Query the store for an entity using the given proxy entity name
 		entity, err := s.GetEntityByName(ctx, event.Check.ProxyEntityName)
 		if err != nil {
-			return fmt.Errorf("could not query the store for a proxy entity: %s", err)
+			return err
 		}
 
 		// Check if an entity was found for this proxy entity. If not, we need to create it
@@ -41,7 +40,7 @@ func getProxyEntity(event *types.Event, s SessionStore) error {
 			}
 
 			if err := s.UpdateEntity(ctx, entity); err != nil {
-				return fmt.Errorf("could not create a proxy entity: %s", err)
+				return err
 			}
 		}
 

backend/agentd/session.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/gogo/protobuf/proto"
 	"github.com/google/uuid"
@@ -64,6 +65,7 @@ type Session struct {
 	store        SessionStore
 	handler      *handler.MessageHandler
 	wg           *sync.WaitGroup
+	stopWG       sync.WaitGroup
 	sendq        chan *transport.Message
 	checkChannel chan interface{}
 	bus          messaging.MessageBus
@@ -104,9 +106,7 @@ type SessionConfig struct {
 func NewSession(ctx context.Context, cfg SessionConfig, conn transport.Transport, bus messaging.MessageBus, store store.Store, unmarshal UnmarshalFunc, marshal MarshalFunc) (*Session, error) {
 	// Validate the agent namespace
 	if _, err := store.GetNamespace(ctx, cfg.Namespace); err != nil {
-		return nil, fmt.Errorf(
-			"could not retrieve the namespace '%s': %s", cfg.Namespace, err.Error(),
-		)
+		return nil, err
 	}
 
 	logger.WithFields(logrus.Fields{
@@ -166,11 +166,18 @@ func (s *Session) receiver() {
 			}
 			return
 		}
-		if err := s.handler.Handle(s.ctx, msg.Type, msg.Payload); err != nil {
+		ctx, cancel := context.WithTimeout(s.ctx, time.Duration(s.cfg.WriteTimeout)*time.Second)
+		if err := s.handler.Handle(ctx, msg.Type, msg.Payload); err != nil {
 			logger.WithError(err).WithFields(logrus.Fields{
 				"type":    msg.Type,
 				"payload": string(msg.Payload)}).Error("error handling message")
+			if _, ok := err.(*store.ErrInternal); ok {
+				// Fatal error - boot the agent out of the session
+				logger.Error("internal error - stopping session")
+				go s.Stop()
+			}
 		}
+		cancel()
 	}
 }
 
@@ -222,6 +229,7 @@ func (s *Session) Start() (err error) {
 	sessionCounter.WithLabelValues(s.cfg.Namespace).Inc()
 	s.wg = &sync.WaitGroup{}
 	s.wg.Add(2)
+	s.stopWG.Add(1)
 	go s.sender()
 	go s.receiver()
 	go func() {
@@ -263,9 +271,11 @@ func (s *Session) Start() (err error) {
 func (s *Session) Stop() {
 	s.cancel()
 	s.wg.Wait()
+	s.stopWG.Wait()
 }
 
 func (s *Session) stop() {
+	defer s.stopWG.Done()
 	defer func() {
 		if err := s.conn.Close(); err != nil {
 			logger.WithError(err).Error("error closing session")
@@ -286,16 +296,24 @@ func (s *Session) stop() {
 		// of the tests.
 		return
 	}
+	var ringWG sync.WaitGroup
+	ringWG.Add(len(s.cfg.Subscriptions))
 	for _, sub := range s.cfg.Subscriptions {
-		ring := s.ringPool.Get(ringv2.Path(s.cfg.Namespace, sub))
-		logger.WithFields(logrus.Fields{
-			"namespace": s.cfg.Namespace,
-			"agent":     s.cfg.AgentName,
-		}).Info("removing agent from ring")
-		if err := ring.Remove(context.Background(), s.cfg.AgentName); err != nil {
-			logger.WithError(err).Error("unable to remove agent from ring")
-		}
+		go func(sub string) {
+			defer ringWG.Done()
+			ring := s.ringPool.Get(ringv2.Path(s.cfg.Namespace, sub))
+			logger.WithFields(logrus.Fields{
+				"namespace": s.cfg.Namespace,
+				"agent":     s.cfg.AgentName,
+			}).Info("removing agent from ring")
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+			if err := ring.Remove(ctx, s.cfg.AgentName); err != nil {
+				logger.WithError(err).Error("unable to remove agent from ring")
+			}
+		}(sub)
 	}
+	ringWG.Wait()
 }
 
 // handleKeepalive is the keepalive message handler.

backend/apid/apid.go
@@ -5,6 +5,7 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"net"
 	"net/http"
 	"sync"
 	"sync/atomic"
@@ -262,6 +263,11 @@ func notFoundHandler(w http.ResponseWriter, req *http.Request) {
 // Start APId.
 func (a *APId) Start() error {
 	logger.Info("starting apid on address: ", a.HTTPServer.Addr)
+	ln, err := net.Listen("tcp", a.HTTPServer.Addr)
+	if err != nil {
+		return fmt.Errorf("failed to start apid: %s", err)
+	}
+
 	a.wg.Add(1)
 
 	go func() {
@@ -269,12 +275,12 @@ func (a *APId) Start() error {
 		var err error
 		if a.tls != nil {
 			// TLS configuration comes from ToServerTLSConfig
-			err = a.HTTPServer.ListenAndServeTLS("", "")
+			err = a.HTTPServer.ServeTLS(ln, "", "")
 		} else {
-			err = a.HTTPServer.ListenAndServe()
+			err = a.HTTPServer.Serve(ln)
 		}
 		if err != nil && err != http.ErrServerClosed {
-			a.errChan <- fmt.Errorf("failed to start http/https server %s", err)
+			a.errChan <- fmt.Errorf("failure while serving api: %s", err)
 		}
 	}()
 

backend/apid/graphql/suggest/field.go
@@ -1,8 +1,6 @@
 package suggest
 
 import (
-	"fmt"
-
 	v2 "github.com/sensu/sensu-go/api/core/v2"
 )
 
@@ -55,7 +53,6 @@ func (f *MapField) Value(res v2.Resource, path string) []string {
 		return collectKeys(fld)
 	}
 	val, ok := fld[key]
-	fmt.Println("key", val, key, ok)
 	if ok {
 		return []string{val}
 	}

backend/backend_test.go
@@ -3,6 +3,7 @@
 package backend
 
 import (
+	"context"
 	"encoding/base64"
 	"fmt"
 	"net"
@@ -79,14 +80,14 @@ func TestBackendHTTPListener(t *testing.T) {
 			}
 
 			cfg := &Config{
-				AgentHost:        "127.0.0.1",
-				AgentPort:        agentPort,
-				APIListenAddress: fmt.Sprintf("127.0.0.1:%d", apiPort),
-				DashboardHost:    "127.0.0.1",
-				DashboardPort:    dashboardPort,
-				StateDir:         dataPath,
-				CacheDir:         cachePath,
-				TLS:              tc.tls,
+				AgentHost:                    "127.0.0.1",
+				AgentPort:                    agentPort,
+				APIListenAddress:             fmt.Sprintf("127.0.0.1:%d", apiPort),
+				DashboardHost:                "127.0.0.1",
+				DashboardPort:                dashboardPort,
+				StateDir:                     dataPath,
+				CacheDir:                     cachePath,
+				TLS:                          tc.tls,
 				EtcdAdvertiseClientURLs:      []string{clURL},
 				EtcdListenClientURLs:         []string{clURL},
 				EtcdListenPeerURLs:           []string{apURL},
@@ -97,7 +98,8 @@ func TestBackendHTTPListener(t *testing.T) {
 				EtcdClientTLSInfo:            tlsInfo,
 				EtcdPeerTLSInfo:              tlsInfo,
 			}
-			b, err := Initialize(cfg)
+			ctx, cancel := context.WithCancel(context.Background())
+			b, err := Initialize(ctx, cfg)
 			if err != nil {
 				t.Fatalf("failed to start backend: %s", err)
 			}
@@ -137,8 +139,7 @@ func TestBackendHTTPListener(t *testing.T) {
 			require.NotNil(t, client)
 
 			assert.NoError(t, client.Close())
-			b.Stop()
-
+			cancel()
 		})
 	}
 }

backend/cmd/start.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -119,7 +120,7 @@ Use "{{.CommandPath}} [command] --help" for more information about a command.{{e
 
 // initializeFunc represents the signature of an initialization function, used
 // to initialize the backend
-type initializeFunc func(*backend.Config) (*backend.Backend, error)
+type InitializeFunc func(context.Context, *backend.Config) (*backend.Backend, error)
 
 func fallbackStringSlice(newFlag, oldFlag string) []string {
 	slice := viper.GetStringSlice(newFlag)
@@ -130,7 +131,7 @@ func fallbackStringSlice(newFlag, oldFlag string) []string {
 }
 
 // StartCommand ...
-func StartCommand(initialize initializeFunc) *cobra.Command {
+func StartCommand(initialize InitializeFunc) *cobra.Command {
 	var setupErr error
 
 	cmd := &cobra.Command{
@@ -228,7 +229,9 @@ func StartCommand(initialize initializeFunc) *cobra.Command {
 				ClientCertAuth: viper.GetBool(flagEtcdPeerClientCertAuth),
 			}
 
-			sensuBackend, err := initialize(cfg)
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			sensuBackend, err := initialize(ctx, cfg)
 			if err != nil {
 				return err
 			}
@@ -238,8 +241,8 @@ func StartCommand(initialize initializeFunc) *cobra.Command {
 			signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 			go func() {
 				sig := <-sigs
-				logger.Info("signal received: ", sig)
-				sensuBackend.Stop()
+				logger.Warn("signal received: ", sig)
+				cancel()
 			}()
 
 			if viper.GetBool(flagDebug) {

commit 79838d39d152291b99360e1d86909093c317521a

Fix a bug that caused sensu-backend restart
This commit fixes a bug that caused sensu-backend to restart
whenever an agent terminates its connection. The bug was
introduced in 572b4b5facc4a4f7b310dcf0eb34ef711e9583e5, but went
unnoticed for the most part, due to the use of the embedded etcd
client, which appears to have different cancellation semantics than
the gRPC client.

Now that the gRPC client has been put back into place, this error
was exposed and made reproducible.

The nature of the error is that when RBAC authorization returned
an error, agentd would propagate the error back to its supervisor
in all cases, leading to an internal backend restart.

When a session's context is canceled, whether due to its client
disconnecting or otherwise, agentd should not propagate any
errors back to the backend supervisor.

Signed-off-by: Eric Chlebek <eric@sensu.io>

backend/agentd/agentd.go
@@ -333,7 +333,8 @@ func (a *Agentd) AuthorizationMiddleware(next http.Handler) http.Handler {
 		}
 		authorized, err := auth.Authorize(ctx, attrs)
 		if err != nil {
-			if _, ok := err.(*store.ErrInternal); ok {
+			if _, ok := err.(*store.ErrInternal); ok && ctx.Err() == nil {
+				logger.WithError(err).Error("unexpected error while authorizing the session")
 				select {
 				case a.errChan <- err:
 				case <-a.ctx.Done():
@@ -341,7 +342,6 @@ func (a *Agentd) AuthorizationMiddleware(next http.Handler) http.Handler {
 				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 				return
 			}
-			logger.WithError(err).Error("unexpected error while authorizing the session")
 			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
 			return
 		}

backend/authorization/rbac/rbac.go
@@ -136,7 +136,9 @@ func (a *Authorizer) Authorize(ctx context.Context, attrs *authorization.Attribu
 				// No ClusterRoleBindings founds, let's continue with the RoleBindings
 				logger.WithError(err).Debug("no bindings found")
 			default:
-				logger.WithError(err).Warning("could not retrieve the ClusterRoleBindings or RoleBindings")
+				if ctx.Err() == nil {
+					logger.WithError(err).Warning("could not retrieve the ClusterRoleBindings or RoleBindings")
+				}
 				visitErr = err
 				return false
 			}

backend/backend.go
@@ -234,8 +234,8 @@ func Initialize(ctx context.Context, config *Config) (*Backend, error) {
 
 	// Initialize pipelined
 	pipeline, err := pipelined.New(pipelined.Config{
-		Store:                   stor,
-		Bus:                     bus,
+		Store: stor,
+		Bus:   bus,
 		ExtensionExecutorGetter: rpc.NewGRPCExtensionExecutor,
 		AssetGetter:             assetGetter,
 		BufferSize:              viper.GetInt(FlagPipelinedBufferSize),
@@ -309,14 +309,14 @@ func Initialize(ctx context.Context, config *Config) (*Backend, error) {
 	// Initialize keepalived
 	keepalive, err := keepalived.New(keepalived.Config{
 		DeregistrationHandler: config.DeregistrationHandler,
-		Bus:                   bus,
-		Store:                 stor,
-		EventStore:            stor,
-		LivenessFactory:       liveness.EtcdFactory(b.runCtx, b.Client),
-		RingPool:              ringPool,
-		BufferSize:            viper.GetInt(FlagKeepalivedBufferSize),
-		WorkerCount:           viper.GetInt(FlagKeepalivedWorkers),
-		StoreTimeout:          2 * time.Minute,
+		Bus:             bus,
+		Store:           stor,
+		EventStore:      stor,
+		LivenessFactory: liveness.EtcdFactory(b.runCtx, b.Client),
+		RingPool:        ringPool,
+		BufferSize:      viper.GetInt(FlagKeepalivedBufferSize),
+		WorkerCount:     viper.GetInt(FlagKeepalivedWorkers),
+		StoreTimeout:    2 * time.Minute,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("error initializing %s: %s", keepalive.Name(), err)
@@ -505,6 +505,8 @@ func (b *Backend) runOnce() error {
 	if derr == nil {
 		derr = b.runCtx.Err()
 	}
+
+	_ = b.Client.Close()
 
 	return derr
 }