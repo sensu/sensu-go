d48b7d6ce135f0c22cad9d28af2c4a1a62e46d9a
Eric Chlebek <eric@sensu.io>
2020-04-30 17:17:49 -0700

    Use retry logic in the rest of the store
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- backend/store/etcd/keepalive_store.go
+++ backend/store/etcd/keepalive_store.go
@@ -5,43 +5,46 @@ import (
 	"path"
 
 	"github.com/coreos/etcd/clientv3"
-	"github.com/gogo/protobuf/proto"
+	corev2 "github.com/sensu/sensu-go/api/core/v2"
 	"github.com/sensu/sensu-go/backend/store"
-	"github.com/sensu/sensu-go/types"
 )
 
 const (
 	keepalivesPathPrefix = "keepalives"
 )
 
-func getKeepalivePath(keepalivesPath string, entity *types.Entity) string {
+func getKeepalivePath(keepalivesPath string, entity *corev2.Entity) string {
 	return path.Join(keepalivesPath, entity.Namespace, entity.Name)
 }
 
 // DeleteFailingKeepalive deletes a failing KeepaliveRecord.
-func (s *Store) DeleteFailingKeepalive(ctx context.Context, entity *types.Entity) error {
-	_, err := s.client.Delete(ctx, getKeepalivePath(s.keepalivesPath, entity))
+func (s *Store) DeleteFailingKeepalive(ctx context.Context, entity *corev2.Entity) error {
+	err := Delete(ctx, s.client, getKeepalivePath(s.keepalivesPath, entity))
+	if err != nil {
+		if _, ok := err.(*store.ErrNotFound); ok {
+			err = nil
+		}
+	}
 	return err
 }
 
 // GetFailingKeepalives gets all of the failing KeepaliveRecords.
-func (s *Store) GetFailingKeepalives(ctx context.Context) ([]*types.KeepaliveRecord, error) {
-	resp, err := s.client.Get(ctx, s.keepalivesPath, clientv3.WithPrefix())
+func (s *Store) GetFailingKeepalives(ctx context.Context) ([]*corev2.KeepaliveRecord, error) {
+	var resp *clientv3.GetResponse
+	err := Backoff(ctx).Retry(func(n int) (done bool, err error) {
+		resp, err = s.client.Get(ctx, s.keepalivesPath, clientv3.WithPrefix())
+		return RetryRequest(n, err)
+	})
+
 	if err != nil {
-		return nil, &store.ErrInternal{Message: err.Error()}
+		return nil, err
 	}
 
-	keepalives := []*types.KeepaliveRecord{}
+	keepalives := []*corev2.KeepaliveRecord{}
 	for _, kv := range resp.Kvs {
-		keepalive := &types.KeepaliveRecord{}
+		keepalive := &corev2.KeepaliveRecord{}
 		if err := unmarshal(kv.Value, keepalive); err != nil {
-			// if we have a problem deserializing a keepalive record, delete that record
-			// ignoring any errors we have along the way.
-			// TODO(eric): the above is questionable, consider removing this logic
-			if _, err := s.client.Delete(ctx, string(kv.Key)); err != nil {
-				logger.Debug(err)
-			}
-			continue
+			return nil, &store.ErrNotValid{Err: err}
 		}
 		keepalives = append(keepalives, keepalive)
 	}
@@ -50,22 +53,7 @@ func (s *Store) GetFailingKeepalives(ctx context.Context) ([]*types.KeepaliveRec
 }
 
 // UpdateFailingKeepalive updates a failing KeepaliveRecord.
-func (s *Store) UpdateFailingKeepalive(ctx context.Context, entity *types.Entity, expiration int64) error {
-	kr := types.NewKeepaliveRecord(entity, expiration)
-	krBytes, err := proto.Marshal(kr)
-	if err != nil {
-		return &store.ErrDecode{Err: err}
-	}
-
-	cmp := clientv3.Compare(clientv3.Version(getNamespacePath(entity.Namespace)), ">", 0)
-	req := clientv3.OpPut(getKeepalivePath(s.keepalivesPath, entity), string(krBytes))
-	res, err := s.client.Txn(ctx).If(cmp).Then(req).Commit()
-	if err != nil {
-		return &store.ErrInternal{Message: err.Error()}
-	}
-	if !res.Succeeded {
-		return &store.ErrNamespaceMissing{Namespace: entity.Namespace}
-	}
-
-	return err
+func (s *Store) UpdateFailingKeepalive(ctx context.Context, entity *corev2.Entity, expiration int64) error {
+	kr := corev2.NewKeepaliveRecord(entity, expiration)
+	return CreateOrUpdate(ctx, s.client, getKeepalivePath(s.keepalivesPath, entity), entity.Namespace, kr)
 }
