2d7f65553455fdc4ea9e2ee1659ef21ddf56ce19
Simon Plourde <simon@sensu.io>
2020-05-19 16:14:35 -0400

    Fix a bug where users could not change password (#3769)

--- backend/apid/routers/users_test.go
+++ backend/apid/routers/users_test.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"path"
 	"testing"
 
 	"github.com/gorilla/mux"
@@ -210,6 +211,27 @@ func TestUsersRouter(t *testing.T) {
 				c.On("CreateOrReplace", mock.Anything, mock.Anything).
 					Return(nil).
 					Once()
+			},
+			wantStatusCode: http.StatusCreated,
+		},
+		{
+			name:   "it only pass the password hash to CreateOrReplace when updating a password",
+			method: http.MethodPut,
+			path:   path.Join(fixture.URIPath(), "password"),
+			body:   []byte(`{"password":"P@ssw0rd!","password_hash":"$2a$10$PdP2LURUHv7PylQtu8haL.8ZBSr5fjDmWXacNGWL6juiR4fRaRSNS"}`),
+			controllerFunc: func(c *mockUserController) {
+				c.On("AuthenticateUser", mock.Anything, mock.Anything, mock.Anything).
+					Return(&corev2.User{Username: "foo", Password: "password_hash", PasswordHash: "password_hash"}, nil).
+					Once()
+				c.On("CreateOrReplace", mock.Anything, mock.Anything).
+					Return(nil).
+					Once().
+					Run(func(args mock.Arguments) {
+						user := args.Get(1).(*corev2.User)
+						if user.Password != "" {
+							t.Fatal("only the password hash should be passed to the CreateOrReplace controller")
+						}
+					})
 			},
 			wantStatusCode: http.StatusCreated,
 		},
