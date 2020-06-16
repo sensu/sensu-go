c8bdb344490d264fcf059203ee0d297d15bbdc7e
Eric Chlebek <eric@sensu.io>
2020-06-16 12:51:21 -0700

    Fix go generate
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- backend/store/v2/wrap/wrapper.go
+++ backend/store/v2/wrap/wrapper.go
@@ -13,10 +13,10 @@ import (
 	"github.com/sensu/sensu-go/types"
 )
 
-//go:generate go run ../../../scripts/check_protoc/main.go
+//go:generate go run ../../../../scripts/check_protoc/main.go
 //go:generate go build -o $GOPATH/bin/protoc-gen-gofast github.com/gogo/protobuf/protoc-gen-gofast
 //go:generate -command protoc protoc --plugin $GOPATH/bin/protoc-gen-gofast --gofast_out=plugins:$GOPATH/src -I=$GOPATH/pkg/mod -I=$GOPATH/pkg/mod/github.com/gogo/protobuf@v1.3.1/protobuf -I=$GOPATH/src
-//go:generate protoc github.com/sensu/sensu-go/backend/store/wrap/wrapper.proto
+//go:generate protoc github.com/sensu/sensu-go/backend/store/v2/wrap/wrapper.proto
 
 // tmGetter is useful for types that want to explicitly provide their
 // TypeMeta - this is useful for lifters.
