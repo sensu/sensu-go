cada1181c02663476a5955acc061ff4312c7dd66
Eric Chlebek <eric@sensu.io>
2020-06-17 14:14:58 -0700

    Add String and GoString methods
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- backend/store/context_keys.go
+++ backend/store/context_keys.go
@@ -6,3 +6,11 @@ type ContextKeyTimeoutT struct{}
 
 // ContextKeyTimeout is used to specify a timeout as a time.Duration.
 var ContextKeyTimeout = ContextKeyTimeoutT{}
+
+func (ContextKeyTimeoutT) String() string {
+	return "timeout"
+}
+
+func (ContextKeyTimeoutT) GoString() string {
+	return "timeout"
+}
