66fad2b9abb71ab0ab035ffbe6d762f354066c49
Simon Plourde <simon@sensu.io>
2020-07-15 15:40:48 -0300

    Send the entity from local config in keepalives
    Signed-off-by: Simon Plourde <simon@sensu.io>

--- agent/agent.go
+++ agent/agent.go
@@ -49,29 +49,30 @@ func GetDefaultAgentName() string {
 
 // An Agent receives and acts on messages from a Sensu Backend.
 type Agent struct {
-	allowList       []allowList
-	api             *http.Server
-	assetGetter     asset.Getter
-	backendSelector BackendSelector
-	config          *Config
-	connected       bool
-	connectedMu     sync.RWMutex
-	contentType     string
-	entityConfig    *corev3.EntityConfig
-	entityMu        sync.Mutex
-	executor        command.Executor
-	handler         *handler.MessageHandler
-	header          http.Header
-	inProgress      map[string]*corev2.CheckConfig
-	inProgressMu    *sync.Mutex
-	statsdServer    StatsdServer
-	sendq           chan *transport.Message
-	systemInfo      *corev2.System
-	systemInfoMu    sync.RWMutex
-	wg              sync.WaitGroup
-	apiQueue        queue
-	marshal         agentd.MarshalFunc
-	unmarshal       agentd.UnmarshalFunc
+	allowList         []allowList
+	api               *http.Server
+	assetGetter       asset.Getter
+	backendSelector   BackendSelector
+	config            *Config
+	connected         bool
+	connectedMu       sync.RWMutex
+	contentType       string
+	entityConfig      *corev3.EntityConfig
+	entityMu          sync.Mutex
+	executor          command.Executor
+	handler           *handler.MessageHandler
+	header            http.Header
+	inProgress        map[string]*corev2.CheckConfig
+	inProgressMu      *sync.Mutex
+	localEntityConfig *corev3.EntityConfig
+	statsdServer      StatsdServer
+	sendq             chan *transport.Message
+	systemInfo        *corev2.System
+	systemInfoMu      sync.RWMutex
+	wg                sync.WaitGroup
+	apiQueue          queue
+	marshal           agentd.MarshalFunc
+	unmarshal         agentd.UnmarshalFunc
 
 	// ProcessGetter gets information about local agent processes.
 	ProcessGetter process.Getter
@@ -408,7 +409,10 @@ func (a *Agent) newKeepalive() *transport.Message {
 	msg := &transport.Message{
 		Type: transport.MessageTypeKeepalive,
 	}
-	entity := a.getAgentEntity()
+
+	// We want to send the entity from the local configuration, in case we need to
+	// register this agent, which should use the local entity configuration
+	entity := a.getLocalEntity()
 	uid, _ := uuid.NewRandom()
 
 	keepalive := &corev2.Event{
@@ -422,7 +426,7 @@ func (a *Agent) newKeepalive() *transport.Message {
 		Timeout:    a.config.KeepaliveWarningTimeout,
 		Ttl:        int64(a.config.KeepaliveCriticalTimeout),
 	}
-	keepalive.Entity = a.getAgentEntity()
+	keepalive.Entity = entity
 	keepalive.Timestamp = time.Now().Unix()
 
 	logEvent(keepalive)
