6f26a014377bf67c66c2150d5f408ff7f6a7607f
Simon Plourde <simon@sensu.io>
2020-07-24 18:00:34 -0400

    Use a RWMutex in the caches (#3944)

--- backend/store/cache/v2/cache.go
+++ backend/store/cache/v2/cache.go
@@ -9,12 +9,12 @@ import (
 	"sync/atomic"
 	"time"
 
-	"go.etcd.io/etcd/clientv3"
 	corev3 "github.com/sensu/sensu-go/api/core/v3"
 	"github.com/sensu/sensu-go/backend/store"
 	storev2 "github.com/sensu/sensu-go/backend/store/v2"
 	"github.com/sensu/sensu-go/backend/store/v2/etcdstore"
 	"github.com/sensu/sensu-go/types/dynamic"
+	"go.etcd.io/etcd/clientv3"
 )
 
 // Value contains a cached value, and its synthesized companion.
@@ -94,7 +94,7 @@ type cacheWatcher struct {
 type Resource struct {
 	count      int64
 	cache      cache
-	cacheMu    sync.Mutex
+	cacheMu    sync.RWMutex
 	watchers   []cacheWatcher
 	watchersMu sync.Mutex
 	synthesize bool
@@ -141,23 +141,23 @@ func New(ctx context.Context, client *clientv3.Client, resource corev3.Resource,
 // inject resources directly into the cache without an actual store
 func NewFromResources(resources []corev3.Resource, synthesize bool) *Resource {
 	return &Resource{
-		cacheMu: sync.Mutex{},
+		cacheMu: sync.RWMutex{},
 		cache:   buildCache(resources, synthesize),
 	}
 }
 
 // Get returns all cached resources in a namespace.
 func (r *Resource) Get(namespace string) []Value {
-	r.cacheMu.Lock()
-	defer r.cacheMu.Unlock()
+	r.cacheMu.RLock()
+	defer r.cacheMu.RUnlock()
 	return r.cache[namespace]
 }
 
 // GetAll returns all cached resources across all namespaces.
 func (r *Resource) GetAll() []Value {
 	values := []Value{}
-	r.cacheMu.Lock()
-	defer r.cacheMu.Unlock()
+	r.cacheMu.RLock()
+	defer r.cacheMu.RUnlock()
 	for _, n := range r.cache {
 		values = append(values, n...)
 	}
