f36611482910a31c56fc82a3da00aaef3a365d8a
Sean Porter <portertech@gmail.com>
2020-07-24 17:34:05 -0700

    prometheus gofmt
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- agent/transformers/prometheus_test.go
+++ agent/transformers/prometheus_test.go
@@ -4,9 +4,9 @@ import (
 	"testing"
 	"time"
 
+	"github.com/prometheus/common/model"
 	"github.com/sensu/sensu-go/types"
 	"github.com/stretchr/testify/assert"
-	"github.com/prometheus/common/model"
 )
 
 func TestParseProm(t *testing.T) {
@@ -49,12 +49,12 @@ func TestTransformProm(t *testing.T) {
 	assert := assert.New(t)
 	ts := time.Now().Unix()
 
- 	testCases := []struct {
- 		metric         PromList
- 		expectedFormat []*types.MetricPoint
- 	}{
- 		{
- 			metric: PromList{
+	testCases := []struct {
+		metric         PromList
+		expectedFormat []*types.MetricPoint
+	}{
+		{
+			metric: PromList{
 				&model.Sample{
 					Metric: model.Metric{
 						model.MetricNameLabel: "go_gc_duration_seconds",
@@ -63,27 +63,27 @@ func TestTransformProm(t *testing.T) {
 					Value:     3.3722e-05,
 					Timestamp: model.TimeFromUnix(ts),
 				},
- 			},
- 			expectedFormat: []*types.MetricPoint{
- 				{
- 					Name:      "go_gc_duration_seconds",
- 					Value:     3.3722e-05,
- 					Timestamp: ts,
- 					Tags: []*types.MetricTag{
- 						{
- 							Name:  "quantile",
- 							Value: "0",
- 						},
- 					},
- 				},
- 			},
- 		},
- 	}
+			},
+			expectedFormat: []*types.MetricPoint{
+				{
+					Name:      "go_gc_duration_seconds",
+					Value:     3.3722e-05,
+					Timestamp: ts,
+					Tags: []*types.MetricTag{
+						{
+							Name:  "quantile",
+							Value: "0",
+						},
+					},
+				},
+			},
+		},
+	}
 
- 	for _, tc := range testCases {
- 		t.Run("transform", func(t *testing.T) {
- 			prom := tc.metric.Transform()
- 			assert.Equal(tc.expectedFormat, prom)
- 		})
- 	}
+	for _, tc := range testCases {
+		t.Run("transform", func(t *testing.T) {
+			prom := tc.metric.Transform()
+			assert.Equal(tc.expectedFormat, prom)
+		})
+	}
 }
