5d275e0c3bd09663b38d44e17874cd2f390d903b
Nikki Attea <nikki@sensu.io>
2020-08-31 16:57:33 -0400

    Support generic query params in sensuctl dump (#3989)
    Signed-off-by: Nikki Attea <nikki@sensu.io>

--- cli/commands/dump/dump.go
+++ cli/commands/dump/dump.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"net/url"
 	"os"
 	"reflect"
 
@@ -161,7 +162,8 @@ func execute(cli *cli.SensuCli) func(*cobra.Command, []string) error {
 
 			val := reflect.New(reflect.SliceOf(reflect.TypeOf(req)))
 			err = cli.Client.List(
-				req.URIPath(), val.Interface(), &client.ListOptions{
+				fmt.Sprintf("%s?types=%s", req.URIPath(), url.QueryEscape(types.WrapResource(req).Type)),
+				val.Interface(), &client.ListOptions{
 					ChunkSize: ChunkSize,
 				}, nil)
 			if err != nil {
