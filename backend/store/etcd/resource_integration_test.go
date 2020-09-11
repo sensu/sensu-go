be0f4f16326862c0613a3d2619333418fb6a57c1
Simon Plourde <simon@sensu.io>
2020-09-11 16:46:26 -0400

    Add unit tests around condition matching
    Signed-off-by: Simon Plourde <simon@sensu.io>

---
+++ backend/store/etcd/resource_integration_test.go
@@ -0,0 +1,42 @@
+// +build integration,!race
+
+package etcd
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/coreos/etcd/clientv3"
+	corev2 "github.com/sensu/sensu-go/api/core/v2"
+	"github.com/sensu/sensu-go/backend/store"
+	"github.com/sensu/sensu-go/backend/store/patch"
+)
+
+func TestStore_PatchResource(t *testing.T) {
+	testWithEtcdClient(t, func(s store.Store, client *clientv3.Client) {
+		// Create a resource
+		obj := &GenericObject{Revision: 42, ObjectMeta: corev2.ObjectMeta{Name: "foo", Namespace: "default"}}
+		ctx := context.WithValue(context.Background(), corev2.NamespaceKey, "default")
+		if err := s.CreateOrUpdateResource(ctx, obj); err != nil {
+			t.Fatalf("could not create a resource: %s", err)
+		}
+
+		// Patch the resource
+		patchedObj := GenericObject{}
+		patcher := &patch.Merge{MergePatch: []byte(`{"metadata":{"labels":{"42":"answer to life"}}}`)}
+		err := s.PatchResource(ctx, &patchedObj, "foo", patcher, nil)
+		if err != nil {
+			t.Fatalf("could not apply the patch: %s", err)
+		}
+
+		// Make sure the stored and patched resources are the same
+		storedObj := GenericObject{}
+		if err := s.GetResource(ctx, "foo", &storedObj); err != nil {
+			t.Fatalf("could not retrieve the stored resource: %s", err)
+		}
+		if !reflect.DeepEqual(patchedObj, storedObj) {
+			t.Errorf("Store.PatchResource() = %#v, want %#v", patchedObj, storedObj)
+		}
+	})
+}
--- backend/store/etcd/resource_test.go
+++ backend/store/etcd/resource_test.go
@@ -1,45 +1,8 @@
-// +build integration,!race
-
 package etcd
 
 import (
-	"context"
-	"reflect"
 	"testing"
-
-	"github.com/coreos/etcd/clientv3"
-	corev2 "github.com/sensu/sensu-go/api/core/v2"
-	"github.com/sensu/sensu-go/backend/store"
-	"github.com/sensu/sensu-go/backend/store/patch"
 )
-
-func TestStore_PatchResource(t *testing.T) {
-	testWithEtcdClient(t, func(s store.Store, client *clientv3.Client) {
-		// Create a resource
-		obj := &GenericObject{Revision: 42, ObjectMeta: corev2.ObjectMeta{Name: "foo", Namespace: "default"}}
-		ctx := context.WithValue(context.Background(), corev2.NamespaceKey, "default")
-		if err := s.CreateOrUpdateResource(ctx, obj); err != nil {
-			t.Fatalf("could not create a resource: %s", err)
-		}
-
-		// Patch the resource
-		patchedObj := GenericObject{}
-		patcher := &patch.Merge{MergePatch: []byte(`{"metadata":{"labels":{"42":"answer to life"}}}`)}
-		err := s.PatchResource(ctx, &patchedObj, "foo", patcher, nil)
-		if err != nil {
-			t.Fatalf("could not apply the patch: %s", err)
-		}
-
-		// Make sure the stored and patched resources are the same
-		storedObj := GenericObject{}
-		if err := s.GetResource(ctx, "foo", &storedObj); err != nil {
-			t.Fatalf("could not retrieve the stored resource: %s", err)
-		}
-		if !reflect.DeepEqual(patchedObj, storedObj) {
-			t.Errorf("Store.PatchResource() = %#v, want %#v", patchedObj, storedObj)
-		}
-	})
-}
 
 func Test_checkIfMatch(t *testing.T) {
 	tests := []struct {
@@ -49,15 +12,91 @@ func Test_checkIfMatch(t *testing.T) {
 		want   bool
 	}{
 		{
-			name: "empty header should pass",
+			name: "empty header should return true",
 			etag: `"12345"`,
 			want: true,
+		},
+		{
+			name:   "empty etag should return false",
+			header: `""`,
+			etag:   `"12345"`,
+			want:   false,
+		},
+		{
+			name:   "the asterisk is a special value that represents any resource",
+			header: "*",
+			etag:   `"12345"`,
+			want:   true,
+		},
+		{
+			name:   "matching etag",
+			header: `"12345"`,
+			etag:   `"12345"`,
+			want:   true,
+		},
+		{
+			name:   "multiple etags can be passed in the header",
+			header: `"000", "12345"`,
+			etag:   `"12345"`,
+			want:   true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := checkIfMatch(tt.args.header, tt.args.etag); got != tt.want {
+			if got := checkIfMatch(tt.header, tt.etag); got != tt.want {
 				t.Errorf("checkIfMatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_checkIfNoneMatch(t *testing.T) {
+	tests := []struct {
+		name   string
+		header string
+		etag   string
+		want   bool
+	}{
+		{
+			name: "empty header should return true",
+			etag: `"12345"`,
+			want: true,
+		},
+		{
+			name:   "empty etag should return true",
+			header: `""`,
+			etag:   `"12345"`,
+			want:   true,
+		},
+		{
+			name:   "the asterisk is a special value that represents any resource",
+			header: "*",
+			etag:   `"12345"`,
+			want:   false,
+		},
+		{
+			name:   "matching etag",
+			header: `"12345"`,
+			etag:   `"12345"`,
+			want:   false,
+		},
+		{
+			name:   "multiple etags can be passed in the header",
+			header: `"000", "12345"`,
+			etag:   `"12345"`,
+			want:   false,
+		},
+		{
+			name:   "the W/ prefix is handled",
+			header: `W/"12345"`,
+			etag:   `"12345"`,
+			want:   false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := checkIfNoneMatch(tt.header, tt.etag); got != tt.want {
+				t.Errorf("checkIfNoneMatch() = %v, want %v", got, tt.want)
 			}
 		})
 	}
