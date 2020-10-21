sensu/sensu-go

commit 0de0baef58f49aa786cc0db58052b3e93dd02f49

Don't use leases for silenced entries (#4034)
This commit changes how silences work in Sensu.

Before, silences were governed by etcd leases. The idea was that a
silenced entry would be created and leased to expire, if it had the
Expire field set.

This was fine in theory, and often in practice, but leases have a subtle
characteristic - they are reset when etcd holds an election, if they are
less than 5 minutes long.

This can cause havoc when Sensu is restarted. For a feature like
silences, it is not reliable enough.

Now, silences store their expiry information directly in their own data
structure. When silences are listed, expired results are simply
discarded from the result set, and asynchronously garbage collected.
This should make silences perfectly reliable, as they will no longer
rely on any state in etcd or Sensu, but rather be self-contained in
their own data structure.

The core/v2.Silenced datatype has a new field, called ExpireAt, which is
the time in seconds since the Unix epoch. When this field is set, Sensu
will use it to determine if the entry has expired. The field will be
automatically set by the store when a new silence is created, and will
therefore be API-compatible with the previous implementation.

If the ExpireAt field is not set on a silenced entry read from the
database, then Sensu will assume that the entry pre-dates the new system,
and fall back to requesting its lease.

Signed-off-by: Eric Chlebek <eric@sensu.io>

backend/apid/routers/silenced.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
@@ -46,14 +47,36 @@ func (r *SilencedRouter) Mount(parent *mux.Router) {
 	routes.Get(r.handlers.GetResource)
 	routes.Post(r.create)
 	routes.Put(r.createOrReplace)
-	routes.List(r.handlers.ListResources, corev2.SilencedFields)
-	routes.ListAllNamespaces(r.handlers.ListResources, "/{resource:silenced}", corev2.SilencedFields)
+	routes.List(trimExpiredSilences(r.handlers.ListResources), corev2.SilencedFields)
+	routes.ListAllNamespaces(trimExpiredSilences(r.handlers.ListResources), "/{resource:silenced}", corev2.SilencedFields)
 
 	// Custom routes for listing by subscription and checks for a specific
 	// namespace, in addition to all namespaces for checks.
 	routes.Router.HandleFunc("/{resource:silenced}/checks/{check}", listHandler(r.list)).Methods(http.MethodGet)
 	routes.Router.HandleFunc(routes.PathPrefix+"/subscriptions/{subscription}", listHandler(r.list)).Methods(http.MethodGet)
 	routes.Router.HandleFunc(routes.PathPrefix+"/checks/{check}", listHandler(r.list)).Methods(http.MethodGet)
+}
+
+func trimExpiredSilences(lister ListControllerFunc) func(context.Context, *store.SelectionPredicate) ([]corev2.Resource, error) {
+	return func(ctx context.Context, pred *store.SelectionPredicate) ([]corev2.Resource, error) {
+		resources, err := lister(ctx, pred)
+		if err != nil {
+			return resources, err
+		}
+		trimmed := make([]corev2.Resource, 0, len(resources))
+		for _, resource := range resources {
+			silenced := resource.(*corev2.Silenced)
+			if silenced.ExpireAt > 0 {
+				silenced.Expire = int64(time.Until(time.Unix(silenced.ExpireAt, 0)) / time.Second)
+				if silenced.Expire > 0 {
+					trimmed = append(trimmed, silenced)
+				}
+			} else {
+				trimmed = append(trimmed, silenced)
+			}
+		}
+		return trimmed, nil
+	}
 }
 
 func (r *SilencedRouter) create(req *http.Request) (interface{}, error) {

backend/eventd/silenced.go
@@ -2,6 +2,7 @@ package eventd
 
 import (
 	"context"
+	"time"
 
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
 	stringsutil "github.com/sensu/sensu-go/util/strings"
@@ -25,9 +26,14 @@ func getSilenced(ctx context.Context, event *corev2.Event, cache Cache) {
 	}
 
 	resources := cache.Get(event.Check.Namespace)
-	entries := make([]*corev2.Silenced, len(resources))
-	for i, resource := range resources {
-		entries[i] = resource.Resource.(*corev2.Silenced)
+	entries := make([]*corev2.Silenced, 0, len(resources))
+	for _, resource := range resources {
+		silenced := resource.Resource.(*corev2.Silenced)
+		if silenced.ExpireAt > 0 && time.Unix(silenced.ExpireAt, 0).Before(time.Now()) {
+			// the entry has expired, and is just a stale cache member
+			continue
+		}
+		entries = append(entries, silenced)
 	}
 
 	// Determine which entries silence this event

agent/agent.go
@@ -319,8 +319,17 @@ func (a *Agent) Run(ctx context.Context) error {
 		cancel()
 	}
 
-	// Wait for all goroutines to gracefully shutdown
-	a.wg.Wait()
+	// Wait for all goroutines to gracefully shutdown, but not too long
+	done := make(chan struct{})
+	go func() {
+		a.wg.Wait()
+		close(done)
+	}()
+	select {
+	case <-done:
+	case <-time.After(time.Second):
+		logger.Warn("agent did not shut down gracefully")
+	}
 	return nil
 }
 

agent/check_handler_internal_test.go
@@ -3,7 +3,6 @@ package agent
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"testing"
 	"time"
 
@@ -305,7 +304,6 @@ func TestExecuteCheckDiscardOutput(t *testing.T) {
 	}
 
 	if got, want := event.Check.Output, ""; got != want {
-		fmt.Println(got)
 		t.Fatal("check output not discarded")
 	}
 }

api/core/v2/silenced.pb.go
@@ -44,7 +44,9 @@ type Silenced struct {
 	// Subscription is the name of the subscription to which the entry applies.
 	Subscription string `protobuf:"bytes,7,opt,name=subscription,proto3" json:"subscription,omitempty"`
 	// Begin is a timestamp at which the silenced entry takes effect.
-	Begin                int64    `protobuf:"varint,10,opt,name=begin,proto3" json:"begin"`
+	Begin int64 `protobuf:"varint,10,opt,name=begin,proto3" json:"begin"`
+	// ExpireAt is a timestamp at which the silenced entry will expire.
+	ExpireAt             int64    `protobuf:"varint,11,opt,name=expire_at,json=expireAt,proto3" json:"expire_at"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -92,32 +94,34 @@ func init() {
 }
 
 var fileDescriptor_768e9755d200fb5d = []byte{
-	// 399 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x91, 0xbf, 0x6e, 0xd5, 0x30,
-	0x14, 0xc6, 0xaf, 0x6f, 0x7b, 0xd3, 0xd4, 0xfc, 0x13, 0x16, 0x83, 0x5b, 0x21, 0x27, 0x62, 0x40,
-	0x19, 0xc0, 0xa1, 0x29, 0x13, 0x13, 0x8a, 0xc4, 0x88, 0x2a, 0x05, 0xb1, 0xb0, 0x54, 0x89, 0x7b,
-	0x48, 0x0d, 0x4d, 0x1c, 0x39, 0x4e, 0x04, 0x6f, 0xc0, 0x23, 0x30, 0x76, 0xec, 0x23, 0xf0, 0x08,
-	0x77, 0xbc, 0x4f, 0x10, 0x41, 0xd8, 0xee, 0x13, 0xc0, 0x86, 0x12, 0xe7, 0xa2, 0x7b, 0xc5, 0xc2,
-	0x92, 0x73, 0xf2, 0xfb, 0xbe, 0xef, 0xf8, 0x58, 0xc6, 0xcf, 0x73, 0x69, 0x2e, 0x9b, 0x8c, 0x0b,
-	0x55, 0x84, 0x35, 0x94, 0x75, 0x63, 0xbf, 0x4f, 0x73, 0x15, 0xa6, 0x95, 0x0c, 0x85, 0xd2, 0x10,
-	0xb6, 0x51, 0x58, 0xcb, 0x2b, 0x28, 0x05, 0x5c, 0xf0, 0x4a, 0x2b, 0xa3, 0xc8, 0x9d, 0xd1, 0xc4,
-	0x07, 0x95, 0xb7, 0xd1, 0xf1, 0xf6, 0x90, 0x5c, 0xe5, 0x2a, 0x1c, 0x5d, 0x59, 0xf3, 0xfe, 0x65,
-	0x7b, 0xc2, 0x4f, 0xf9, 0xc9, 0x08, 0x47, 0x36, 0x76, 0x76, 0xc8, 0xf1, 0xb3, 0xff, 0x3b, 0xba,
-	0x00, 0x93, 0xda, 0xc4, 0xa3, 0xdf, 0x73, 0xec, 0xbe, 0x99, 0x36, 0x21, 0x6f, 0xb1, 0x3b, 0x48,
-	0x17, 0xa9, 0x49, 0x29, 0xf2, 0x51, 0x70, 0x2b, 0x3a, 0xe2, 0x3b, 0x6b, 0xf1, 0xb3, 0xec, 0x03,
-	0x08, 0xf3, 0x1a, 0x4c, 0x1a, 0xb3, 0x65, 0xe7, 0xcd, 0x56, 0x9d, 0x87, 0xd6, 0x9d, 0x47, 0x36,
-	0xb1, 0x27, 0xaa, 0x90, 0x06, 0x8a, 0xca, 0x7c, 0x4e, 0xfe, 0x8e, 0x22, 0x8f, 0xb1, 0x03, 0x9f,
-	0x2a, 0xa9, 0x81, 0xce, 0x7d, 0x14, 0xec, 0xc5, 0x77, 0x97, 0x36, 0x35, 0xd1, 0x64, 0xaa, 0xe4,
-	0x15, 0xbe, 0x6f, 0xbb, 0x73, 0x55, 0x9e, 0x6b, 0xa8, 0xd5, 0x55, 0x0b, 0x74, 0xcf, 0x47, 0x81,
-	0x1b, 0x1f, 0x4d, 0x91, 0x7f, 0x0d, 0xc9, 0x3d, 0x8b, 0xce, 0xca, 0xc4, 0x02, 0xc2, 0xf0, 0x81,
-	0xd0, 0x90, 0x1a, 0xa5, 0xe9, 0xbe, 0x8f, 0x82, 0xc3, 0x78, 0x7f, 0x08, 0x27, 0x1b, 0x48, 0x1e,
-	0xe0, 0x85, 0xb8, 0x04, 0xf1, 0x91, 0x2e, 0x06, 0x35, 0xb1, 0x3f, 0xe4, 0x21, 0x76, 0x34, 0xa4,
-	0xb5, 0x2a, 0xa9, 0xb3, 0x15, 0x9a, 0x18, 0x09, 0xf0, 0xed, 0xba, 0xc9, 0x6a, 0xa1, 0x65, 0x65,
-	0xa4, 0x2a, 0xe9, 0xc1, 0x96, 0x67, 0x47, 0x21, 0x1e, 0x5e, 0x64, 0x90, 0xcb, 0x92, 0xe2, 0xf1,
-	0xae, 0x87, 0xeb, 0xce, 0xb3, 0x20, 0xb1, 0xe5, 0x85, 0xfb, 0xe5, 0xda, 0x9b, 0xdd, 0x5c, 0x7b,
-	0x28, 0xf6, 0x7f, 0xfd, 0x60, 0xe8, 0xa6, 0x67, 0xe8, 0x5b, 0xcf, 0xd0, 0xb2, 0x67, 0x68, 0xd5,
-	0x33, 0xf4, 0xbd, 0x67, 0xe8, 0xeb, 0x4f, 0x36, 0x7b, 0x37, 0x6f, 0xa3, 0xcc, 0x19, 0x1f, 0xe9,
-	0xf4, 0x4f, 0x00, 0x00, 0x00, 0xff, 0xff, 0x63, 0xba, 0xbc, 0x06, 0x53, 0x02, 0x00, 0x00,
+	// 420 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x91, 0x31, 0x6e, 0xd4, 0x40,
+	0x14, 0x86, 0x77, 0xb2, 0xd9, 0x8d, 0x77, 0x42, 0x40, 0x8c, 0x28, 0x26, 0x11, 0x1a, 0x5b, 0x14,
+	0xc8, 0x42, 0x60, 0x93, 0x0d, 0x15, 0x15, 0x58, 0xa2, 0x44, 0x91, 0x06, 0xd1, 0xd0, 0x44, 0xe3,
+	0xc9, 0xc3, 0x19, 0xc8, 0x7a, 0xac, 0xf1, 0xac, 0x05, 0x37, 0xe0, 0x08, 0x94, 0x69, 0x90, 0x72,
+	0x04, 0x8e, 0xb0, 0x65, 0x4e, 0x60, 0x81, 0xe9, 0xf6, 0x04, 0x94, 0x68, 0x67, 0x1c, 0xd8, 0x15,
+	0x4d, 0x1a, 0xbf, 0xe7, 0xef, 0xff, 0xff, 0xe7, 0xf7, 0x64, 0xfc, 0xac, 0x50, 0xf6, 0x6c, 0x9e,
+	0x27, 0x52, 0xcf, 0xd2, 0x1a, 0xca, 0x7a, 0xee, 0x9f, 0x4f, 0x0a, 0x9d, 0x8a, 0x4a, 0xa5, 0x52,
+	0x1b, 0x48, 0x9b, 0x69, 0x5a, 0xab, 0x73, 0x28, 0x25, 0x9c, 0x26, 0x95, 0xd1, 0x56, 0x93, 0x3d,
+	0x67, 0x4a, 0x56, 0x6a, 0xd2, 0x4c, 0x0f, 0xd6, 0x87, 0x14, 0xba, 0xd0, 0xa9, 0x73, 0xe5, 0xf3,
+	0xf7, 0x2f, 0x9a, 0xc3, 0xe4, 0x28, 0x39, 0x74, 0xd0, 0x31, 0xd7, 0xf9, 0x21, 0x07, 0x4f, 0x6f,
+	0xf6, 0xe9, 0x19, 0x58, 0xe1, 0x13, 0x0f, 0xbe, 0x0d, 0x71, 0xf0, 0xa6, 0xdf, 0x84, 0xbc, 0xc5,
+	0xc1, 0x4a, 0x3a, 0x15, 0x56, 0x50, 0x14, 0xa1, 0x78, 0x77, 0xba, 0x9f, 0x6c, 0xac, 0x95, 0x1c,
+	0xe7, 0x1f, 0x40, 0xda, 0xd7, 0x60, 0x45, 0xc6, 0x16, 0x6d, 0x38, 0xb8, 0x6a, 0x43, 0xb4, 0x6c,
+	0x43, 0x72, 0x1d, 0x7b, 0xac, 0x67, 0xca, 0xc2, 0xac, 0xb2, 0x9f, 0xf9, 0xdf, 0x51, 0xe4, 0x21,
+	0x1e, 0xc3, 0xa7, 0x4a, 0x19, 0xa0, 0x5b, 0x11, 0x8a, 0x87, 0xd9, 0xed, 0x85, 0x4f, 0xf5, 0x94,
+	0xf7, 0x95, 0xbc, 0xc2, 0x77, 0x7d, 0x77, 0xa2, 0xcb, 0x13, 0x03, 0xb5, 0x3e, 0x6f, 0x80, 0x0e,
+	0x23, 0x14, 0x07, 0xd9, 0x7e, 0x1f, 0xf9, 0xdf, 0xc0, 0xef, 0x78, 0x74, 0x5c, 0x72, 0x0f, 0x08,
+	0xc3, 0x3b, 0xd2, 0x80, 0xb0, 0xda, 0xd0, 0xed, 0x08, 0xc5, 0x93, 0x6c, 0x7b, 0x15, 0xe6, 0xd7,
+	0x90, 0xdc, 0xc3, 0x23, 0x79, 0x06, 0xf2, 0x23, 0x1d, 0xad, 0x54, 0xee, 0x5f, 0xc8, 0x7d, 0x3c,
+	0x36, 0x20, 0x6a, 0x5d, 0xd2, 0xf1, 0x5a, 0xa8, 0x67, 0x24, 0xc6, 0xb7, 0xea, 0x79, 0x5e, 0x4b,
+	0xa3, 0x2a, 0xab, 0x74, 0x49, 0x77, 0xd6, 0x3c, 0x1b, 0x0a, 0x09, 0xf1, 0x28, 0x87, 0x42, 0x95,
+	0x14, 0xbb, 0x5b, 0x27, 0xcb, 0x36, 0xf4, 0x80, 0xfb, 0x42, 0x1e, 0xe1, 0x49, 0x7f, 0x84, 0xb0,
+	0x74, 0xd7, 0x99, 0xf6, 0x96, 0x6d, 0xf8, 0x0f, 0xf2, 0xc0, 0xb7, 0x2f, 0xed, 0xf3, 0xe0, 0xcb,
+	0x45, 0x38, 0xb8, 0xbc, 0x08, 0x51, 0x16, 0xfd, 0xfe, 0xc9, 0xd0, 0x65, 0xc7, 0xd0, 0xf7, 0x8e,
+	0xa1, 0x45, 0xc7, 0xd0, 0x55, 0xc7, 0xd0, 0x8f, 0x8e, 0xa1, 0xaf, 0xbf, 0xd8, 0xe0, 0xdd, 0x56,
+	0x33, 0xcd, 0xc7, 0xee, 0x87, 0x1e, 0xfd, 0x09, 0x00, 0x00, 0xff, 0xff, 0x27, 0x07, 0xa7, 0x6c,
+	0x7f, 0x02, 0x00, 0x00,
 }
 
 func (this *Silenced) Equal(that interface{}) bool {
@@ -163,6 +167,9 @@ func (this *Silenced) Equal(that interface{}) bool {
 	if this.Begin != that1.Begin {
 		return false
 	}
+	if this.ExpireAt != that1.ExpireAt {
+		return false
+	}
 	if !bytes.Equal(this.XXX_unrecognized, that1.XXX_unrecognized) {
 		return false
 	}
@@ -179,6 +186,7 @@ type SilencedFace interface {
 	GetReason() string
 	GetSubscription() string
 	GetBegin() int64
+	GetExpireAt() int64
 }
 
 func (this *Silenced) Proto() github_com_golang_protobuf_proto.Message {
@@ -221,6 +229,10 @@ func (this *Silenced) GetBegin() int64 {
 	return this.Begin
 }
 
+func (this *Silenced) GetExpireAt() int64 {
+	return this.ExpireAt
+}
+
 func NewSilencedFromFace(that SilencedFace) *Silenced {
 	this := &Silenced{}
 	this.ObjectMeta = that.GetObjectMeta()
@@ -231,6 +243,7 @@ func NewSilencedFromFace(that SilencedFace) *Silenced {
 	this.Reason = that.GetReason()
 	this.Subscription = that.GetSubscription()
 	this.Begin = that.GetBegin()
+	this.ExpireAt = that.GetExpireAt()
 	return this
 }
 
@@ -257,6 +270,11 @@ func (m *Silenced) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	if m.XXX_unrecognized != nil {
 		i -= len(m.XXX_unrecognized)
 		copy(dAtA[i:], m.XXX_unrecognized)
+	}
+	if m.ExpireAt != 0 {
+		i = encodeVarintSilenced(dAtA, i, uint64(m.ExpireAt))
+		i--
+		dAtA[i] = 0x58
 	}
 	if m.Begin != 0 {
 		i = encodeVarintSilenced(dAtA, i, uint64(m.Begin))
@@ -347,8 +365,12 @@ func NewPopulatedSilenced(r randySilenced, easy bool) *Silenced {
 	if r.Intn(2) == 0 {
 		this.Begin *= -1
 	}
+	this.ExpireAt = int64(r.Int63())
+	if r.Intn(2) == 0 {
+		this.ExpireAt *= -1
+	}
 	if !easy && r.Intn(10) != 0 {
-		this.XXX_unrecognized = randUnrecognizedSilenced(r, 11)
+		this.XXX_unrecognized = randUnrecognizedSilenced(r, 12)
 	}
 	return this
 }
@@ -457,6 +479,9 @@ func (m *Silenced) Size() (n int) {
 	}
 	if m.Begin != 0 {
 		n += 1 + sovSilenced(uint64(m.Begin))
+	}
+	if m.ExpireAt != 0 {
+		n += 1 + sovSilenced(uint64(m.ExpireAt))
 	}
 	if m.XXX_unrecognized != nil {
 		n += len(m.XXX_unrecognized)
@@ -714,6 +739,25 @@ func (m *Silenced) Unmarshal(dAtA []byte) error {
 				b := dAtA[iNdEx]
 				iNdEx++
 				m.Begin |= int64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		case 11:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field ExpireAt", wireType)
+			}
+			m.ExpireAt = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowSilenced
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.ExpireAt |= int64(b&0x7F) << shift
 				if b < 0x80 {
 					break
 				}

api/core/v2/silenced.proto
@@ -43,4 +43,7 @@ message Silenced {
 
   // Begin is a timestamp at which the silenced entry takes effect.
   int64 begin = 10 [ (gogoproto.jsontag) = "begin" ];
+
+  // ExpireAt is a timestamp at which the silenced entry will expire.
+  int64 expire_at = 11 [ (gogoproto.jsontag) = "expire_at" ];
 }

backend/store/etcd/silenced_store.go
@@ -167,39 +167,20 @@ func (s *Store) UpdateSilencedEntry(ctx context.Context, silenced *corev2.Silenc
 		return &store.ErrNotValid{Err: err}
 	}
 
+	if silenced.ExpireAt == 0 && silenced.Expire > 0 {
+		start := time.Now()
+		if silenced.Begin > 0 {
+			start = time.Unix(silenced.Begin, 0)
+		}
+		silenced.ExpireAt = start.Add(time.Duration(silenced.Expire) * time.Second).Unix()
+	}
+
 	silencedBytes, err := proto.Marshal(silenced)
 	if err != nil {
 		return &store.ErrEncode{Err: err}
 	}
-	var req clientv3.Op
 	cmp := clientv3.Compare(clientv3.Version(getNamespacePath(silenced.Namespace)), ">", 0)
-	if silenced.Expire > 0 {
-		// add expire time to begin time, that is the ttl for the lease
-		var expireTime int64
-		// Check begin time against current time to get an offset for the ttl
-		currentTime := time.Now().Unix()
-		timeDelta := silenced.Begin - currentTime
-		// Add the delta to the expire time unless it is negative (begin time is
-		// in the past)
-		if timeDelta > 0 {
-			expireTime = silenced.Expire + timeDelta
-		} else {
-			expireTime = silenced.Expire
-		}
-
-		var lease *clientv3.LeaseGrantResponse
-		err := kvc.Backoff(ctx).Retry(func(n int) (done bool, err error) {
-			lease, err = s.client.Grant(ctx, expireTime)
-			return kvc.RetryRequest(n, err)
-		})
-		if err != nil {
-			return err
-		}
-
-		req = clientv3.OpPut(GetSilencedPath(ctx, silenced.Name), string(silencedBytes), clientv3.WithLease(lease.ID))
-	} else {
-		req = clientv3.OpPut(GetSilencedPath(ctx, silenced.Name), string(silencedBytes))
-	}
+	req := clientv3.OpPut(GetSilencedPath(ctx, silenced.Name), string(silencedBytes))
 	var res *clientv3.TxnResponse
 	err = kvc.Backoff(ctx).Retry(func(n int) (done bool, err error) {
 		res, err = s.client.Txn(ctx).If(cmp).Then(req).Commit()
@@ -217,45 +198,91 @@ func (s *Store) UpdateSilencedEntry(ctx context.Context, silenced *corev2.Silenc
 
 // arraySilencedEntries is a helper function to unmarshal serialized entries and
 // return them as an array
+//
+// if all is true, then arraySilencedEntries will also return expired entries
 func (s *Store) arraySilencedEntries(ctx context.Context, resp *clientv3.GetResponse) ([]*corev2.Silenced, error) {
 	if len(resp.Kvs) == 0 {
 		return []*corev2.Silenced{}, nil
 	}
-	silencedArray := make([]*corev2.Silenced, len(resp.Kvs))
-	for i, kv := range resp.Kvs {
-		leaseID := clientv3.LeaseID(kv.Lease)
-		ttl, err := s.client.TimeToLive(ctx, leaseID)
-		if err != nil {
-			logger.WithError(err).Error("error setting TTL on silenced")
-			continue
-		}
-		silencedEntry := &corev2.Silenced{}
-		err = unmarshal(kv.Value, silencedEntry)
-		if err != nil {
+	result := make([]*corev2.Silenced, 0, len(resp.Kvs))
+	rejects := []string{}
+	for _, kv := range resp.Kvs {
+		silenced := &corev2.Silenced{}
+		if err := unmarshal(kv.Value, silenced); err != nil {
 			return nil, &store.ErrDecode{Err: err}
 		}
-		silencedEntry.Expire = ttl.TTL
-		silencedArray[i] = silencedEntry
-	}
-	return silencedArray, nil
-}
-
-func (s *Store) arrayTxnSilencedEntries(ctx context.Context, resp *clientv3.TxnResponse) ([]*corev2.Silenced, error) {
-	results := []*corev2.Silenced{}
-	for _, resp := range resp.Responses {
-		for _, kv := range resp.GetResponseRange().Kvs {
+		leaseID := clientv3.LeaseID(kv.Lease)
+		if leaseID > 0 {
+			// legacy expiry mechanism
 			leaseID := clientv3.LeaseID(kv.Lease)
 			ttl, err := s.client.TimeToLive(ctx, leaseID)
 			if err != nil {
 				logger.WithError(err).Error("error setting TTL on silenced")
 				continue
 			}
+			silenced.Expire = ttl.TTL
+			result = append(result, silenced)
+		} else if silenced.ExpireAt > 0 {
+			// new expiry mechanism
+			silenced.Expire = int64(time.Until(time.Unix(silenced.ExpireAt, 0)) / time.Second)
+			if silenced.Expire > 0 {
+				result = append(result, silenced)
+			} else {
+				rejects = append(rejects, silenced.Name)
+			}
+		} else {
+			// no expiry
+			silenced.Expire = -1
+			result = append(result, silenced)
+		}
+	}
+	if len(rejects) > 0 {
+		logger.Infof("deleting %d expired silenced entries", len(rejects))
+		if err := s.DeleteSilencedEntryByName(ctx, rejects...); err != nil {
+			logger.WithError(err).Error("error deleting expired silenced entries")
+		}
+	}
+	return result, nil
+}
+
+func (s *Store) arrayTxnSilencedEntries(ctx context.Context, resp *clientv3.TxnResponse) ([]*corev2.Silenced, error) {
+	results := []*corev2.Silenced{}
+	rejects := []string{}
+	for _, resp := range resp.Responses {
+		for _, kv := range resp.GetResponseRange().Kvs {
 			var silenced corev2.Silenced
 			if err := unmarshal(kv.Value, &silenced); err != nil {
 				return nil, &store.ErrDecode{Err: fmt.Errorf("couldn't get silenced entries: %s", err)}
 			}
-			silenced.Expire = ttl.TTL
-			results = append(results, &silenced)
+			leaseID := clientv3.LeaseID(kv.Lease)
+			if leaseID > 0 {
+				// legacy expiry mechanism
+				ttl, err := s.client.TimeToLive(ctx, leaseID)
+				if err != nil {
+					logger.WithError(err).Error("error setting TTL on silenced")
+					continue
+				}
+				silenced.Expire = ttl.TTL
+				results = append(results, &silenced)
+			} else if silenced.ExpireAt > 0 {
+				// new expiry mechanism
+				silenced.Expire = int64(time.Until(time.Unix(silenced.ExpireAt, 0)) / time.Second)
+				if silenced.Expire > 0 {
+					results = append(results, &silenced)
+				} else {
+					rejects = append(rejects, silenced.Name)
+				}
+			} else {
+				// no expiry
+				silenced.Expire = -1
+				results = append(results, &silenced)
+			}
+		}
+	}
+	if len(rejects) > 0 {
+		logger.Infof("deleting %d expired silenced entries", len(rejects))
+		if err := s.DeleteSilencedEntryByName(ctx, rejects...); err != nil {
+			logger.WithError(err).Error("error deleting expired silenced entries")
 		}
 	}
 	return results, nil

backend/store/etcd/silenced_store_test.go
@@ -33,9 +33,9 @@ func TestSilencedStorage(t *testing.T) {
 
 		// Get all silenced entries
 		entries, err := store.GetSilencedEntries(ctx)
-		assert.NoError(t, err)
-		assert.NotNil(t, entries)
-		assert.Equal(t, 1, len(entries))
+		require.NoError(t, err)
+		require.NotNil(t, entries)
+		require.Equal(t, 1, len(entries))
 
 		// Get silenced entry by name
 		entry, err := store.GetSilencedEntryByName(ctx, silenced.Name)
@@ -108,8 +108,8 @@ func TestSilencedStorageWithExpire(t *testing.T) {
 
 		// Get silenced entry and check that expire time is not zero
 		entry, err := store.GetSilencedEntryByName(ctx, silenced.Name)
-		assert.NoError(t, err)
-		assert.NotNil(t, entry)
+		require.NoError(t, err)
+		require.NotNil(t, entry)
 		assert.NotZero(t, entry.Expire)
 	})
 }
@@ -132,7 +132,7 @@ func TestSilencedStorageWithBegin(t *testing.T) {
 		// Get silenced entry and check that it is not yet ready to start
 		// silencing
 		entry, err := store.GetSilencedEntryByName(ctx, silenced.Name)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 		require.NotNil(t, entry)
 		assert.False(t, entry.StartSilence(currentTime))
 

commit afce9924ff623c0cc1cc3e12731a4a7efac9778f

Fix a bug introduced by #4034
This commit fixes a bug in the silenced HTTP API where expired silenced
entries can appear, even though they should not.

Signed-off-by: Eric Chlebek <eric@sensu.io>

backend/apid/actions/silenced.go
@@ -2,6 +2,7 @@ package actions
 
 import (
 	"context"
+	"errors"
 
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
 	"github.com/sensu/sensu-go/backend/authentication/jwt"
@@ -88,4 +89,15 @@ func (c SilencedController) CreateOrReplace(ctx context.Context, entry *corev2.S
 	}
 
 	return nil
+}
+
+func (c SilencedController) Get(ctx context.Context, name string) (*corev2.Silenced, error) {
+	entry, err := c.Store.GetSilencedEntryByName(ctx, name)
+	if err != nil {
+		return nil, NewError(InternalErr, err)
+	}
+	if entry == nil {
+		return nil, NewError(NotFound, errors.New("silenced entry not found"))
+	}
+	return entry, nil
 }

backend/apid/routers/silenced.go
@@ -3,7 +3,7 @@ package routers
 import (
 	"context"
 	"net/http"
-	"time"
+	"net/url"
 
 	"github.com/gorilla/mux"
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
@@ -23,6 +23,7 @@ type silencedController interface {
 	Create(ctx context.Context, entry *corev2.Silenced) error
 	CreateOrReplace(ctx context.Context, entry *corev2.Silenced) error
 	List(ctx context.Context, sub, check string) ([]*corev2.Silenced, error)
+	Get(ctx context.Context, name string) (*corev2.Silenced, error)
 }
 
 // NewSilencedRouter instantiates new router for controlling user resources
@@ -44,11 +45,11 @@ func (r *SilencedRouter) Mount(parent *mux.Router) {
 	}
 
 	routes.Del(r.handlers.DeleteResource)
-	routes.Get(r.handlers.GetResource)
+	routes.Get(r.get)
 	routes.Post(r.create)
 	routes.Put(r.createOrReplace)
-	routes.List(trimExpiredSilences(r.handlers.ListResources), corev2.SilencedFields)
-	routes.ListAllNamespaces(trimExpiredSilences(r.handlers.ListResources), "/{resource:silenced}", corev2.SilencedFields)
+	routes.List(r.listr, corev2.SilencedFields)
+	routes.ListAllNamespaces(r.listr, "/{resource:silenced}", corev2.SilencedFields)
 
 	// Custom routes for listing by subscription and checks for a specific
 	// namespace, in addition to all namespaces for checks.
@@ -57,26 +58,12 @@ func (r *SilencedRouter) Mount(parent *mux.Router) {
 	routes.Router.HandleFunc(routes.PathPrefix+"/checks/{check}", listHandler(r.list)).Methods(http.MethodGet)
 }
 
-func trimExpiredSilences(lister ListControllerFunc) func(context.Context, *store.SelectionPredicate) ([]corev2.Resource, error) {
-	return func(ctx context.Context, pred *store.SelectionPredicate) ([]corev2.Resource, error) {
-		resources, err := lister(ctx, pred)
-		if err != nil {
-			return resources, err
-		}
-		trimmed := make([]corev2.Resource, 0, len(resources))
-		for _, resource := range resources {
-			silenced := resource.(*corev2.Silenced)
-			if silenced.ExpireAt > 0 {
-				silenced.Expire = int64(time.Until(time.Unix(silenced.ExpireAt, 0)) / time.Second)
-				if silenced.Expire > 0 {
-					trimmed = append(trimmed, silenced)
-				}
-			} else {
-				trimmed = append(trimmed, silenced)
-			}
-		}
-		return trimmed, nil
+func (r *SilencedRouter) get(req *http.Request) (interface{}, error) {
+	id, err := url.PathUnescape(mux.Vars(req)["id"])
+	if err != nil {
+		return nil, actions.NewError(actions.InvalidArgument, err)
 	}
+	return r.controller.Get(req.Context(), id)
 }
 
 func (r *SilencedRouter) create(req *http.Request) (interface{}, error) {
@@ -105,6 +92,18 @@ func (r *SilencedRouter) createOrReplace(req *http.Request) (interface{}, error)
 
 	err := r.controller.CreateOrReplace(req.Context(), entry)
 	return nil, err
+}
+
+func (r *SilencedRouter) listr(ctx context.Context, pred *store.SelectionPredicate) ([]corev2.Resource, error) {
+	entries, err := r.controller.List(ctx, "", "")
+	if err != nil {
+		return nil, err
+	}
+	result := make([]corev2.Resource, 0, len(entries))
+	for _, e := range entries {
+		result = append(result, e)
+	}
+	return result, nil
 }
 
 func (r *SilencedRouter) list(w http.ResponseWriter, req *http.Request) (interface{}, error) {

backend/apid/routers/silenced_test.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"bytes"
 	"context"
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -22,13 +23,10 @@ func TestSilencedRouter(t *testing.T) {
 	parentRouter := mux.NewRouter().PathPrefix(corev2.URLPrefix).Subrouter()
 	router.Mount(parentRouter)
 
-	empty := &corev2.Silenced{}
 	fixture := corev2.FixtureSilenced("*:bar")
 
 	tests := []routerTestCase{}
-	tests = append(tests, getTestCases(fixture)...)
 	tests = append(tests, deleteTestCases(fixture)...)
-	tests = append(tests, listTestCases(empty)...)
 	for _, tt := range tests {
 		run(t, tt, parentRouter, s)
 	}
@@ -49,6 +47,11 @@ func (m *mockSilencedController) CreateOrReplace(ctx context.Context, entry *cor
 func (m *mockSilencedController) List(ctx context.Context, sub, check string) ([]*corev2.Silenced, error) {
 	args := m.Called(ctx, sub, check)
 	return args.Get(0).([]*corev2.Silenced), args.Error(1)
+}
+
+func (m *mockSilencedController) Get(ctx context.Context, name string) (*corev2.Silenced, error) {
+	args := m.Called(ctx, name)
+	return args.Get(0).(*corev2.Silenced), args.Error(1)
 }
 
 func TestSilencedRouterCustomRoutes(t *testing.T) {
@@ -147,6 +150,66 @@ func TestSilencedRouterCustomRoutes(t *testing.T) {
 					Once()
 			},
 			wantStatusCode: http.StatusCreated,
+		},
+		{
+			name:   "it returns 200 on successful get",
+			method: http.MethodGet,
+			path:   corev2.FixtureSilenced("*:foo").URIPath(),
+			body:   nil,
+			controllerFunc: func(c *mockSilencedController) {
+				c.On("Get", mock.Anything, "*:foo").
+					Return(corev2.FixtureSilenced(":foo"), nil).
+					Once()
+			},
+			wantStatusCode: http.StatusOK,
+		},
+		{
+			name:   "it returns 404 on unsuccessful get",
+			method: http.MethodGet,
+			path:   corev2.FixtureSilenced("not:exists").URIPath(),
+			body:   nil,
+			controllerFunc: func(c *mockSilencedController) {
+				c.On("Get", mock.Anything, mock.Anything).
+					Return((*corev2.Silenced)(nil), actions.NewError(actions.NotFound, errors.New("not found"))).
+					Once()
+			},
+			wantStatusCode: http.StatusNotFound,
+		},
+		{
+			name:   "it returns 500 on get error",
+			method: http.MethodGet,
+			path:   corev2.FixtureSilenced("not:exists").URIPath(),
+			body:   nil,
+			controllerFunc: func(c *mockSilencedController) {
+				c.On("Get", mock.Anything, mock.Anything).
+					Return((*corev2.Silenced)(nil), errors.New("hi")).
+					Once()
+			},
+			wantStatusCode: http.StatusInternalServerError,
+		},
+		{
+			name:   "it returns 200 on successful list",
+			method: http.MethodGet,
+			path:   new(corev2.Silenced).URIPath(),
+			body:   nil,
+			controllerFunc: func(c *mockSilencedController) {
+				c.On("List", mock.Anything, "", "").
+					Return([]*corev2.Silenced{}, nil).
+					Once()
+			},
+			wantStatusCode: http.StatusOK,
+		},
+		{
+			name:   "it returns 500 on error list",
+			method: http.MethodGet,
+			path:   new(corev2.Silenced).URIPath(),
+			body:   nil,
+			controllerFunc: func(c *mockSilencedController) {
+				c.On("List", mock.Anything, "", "").
+					Return(([]*corev2.Silenced)(nil), errors.New("hi")).
+					Once()
+			},
+			wantStatusCode: http.StatusInternalServerError,
 		},
 	}
 	for _, tt := range tests {