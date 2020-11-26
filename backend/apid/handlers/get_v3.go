e85fe737821607057d93f3ddc1e26ba9e2331700
Cyril Cressent <cyril@sensu.io>
2020-11-26 15:48:00 -0800

    Add GetV3Resource
    Signed-off-by: Cyril Cressent <cyril@sensu.io>

---
+++ backend/apid/handlers/get_v3.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/gorilla/mux"
+
+	corev3 "github.com/sensu/sensu-go/api/core/v3"
+	"github.com/sensu/sensu-go/backend/apid/actions"
+	"github.com/sensu/sensu-go/backend/store"
+	storev2 "github.com/sensu/sensu-go/backend/store/v2"
+)
+
+func (h Handlers) GetV3Resource(r *http.Request) (corev3.Resource, error) {
+	params := mux.Vars(r)
+	name, err := url.PathUnescape(params["id"])
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := r.Context()
+	namespace := store.NewNamespaceFromContext(ctx)
+	storeName := h.V3Resource.StoreName()
+
+	req := storev2.NewResourceRequest(ctx, namespace, name, storeName)
+	w, err := h.StoreV2.Get(req)
+	if err != nil {
+		switch err := err.(type) {
+		case *store.ErrNotFound:
+			return nil, actions.NewErrorf(actions.NotFound)
+		case *store.ErrNotValid:
+			return nil, actions.NewError(actions.InvalidArgument, err)
+		default:
+			return nil, actions.NewError(actions.InternalErr, err)
+		}
+	}
+	return w.Unwrap()
+}
---
+++ backend/apid/handlers/get_v3_test.go
@@ -0,0 +1,88 @@
+package handlers
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/mock"
+
+	corev2 "github.com/sensu/sensu-go/api/core/v2"
+	"github.com/sensu/sensu-go/backend/store"
+	"github.com/sensu/sensu-go/backend/store/v2/wrap"
+	"github.com/sensu/sensu-go/testing/fixture"
+	"github.com/sensu/sensu-go/testing/mockstore"
+)
+
+func TestHandlers_GetV3Resource(t *testing.T) {
+	meta := corev2.NewObjectMeta("default", "bar")
+	barResource := &fixture.V3Resource{Metadata: &meta}
+	wrapper, _ := wrap.Resource(barResource)
+	tests := []struct {
+		name      string
+		urlVars   map[string]string
+		storeFunc func(*mockstore.V2MockStore)
+		want      interface{}
+		wantErr   bool
+	}{
+		{
+			name:    "invalid URL parameter",
+			urlVars: map[string]string{"id": "%"},
+			wantErr: true,
+		},
+		{
+			name:    "store ErrNotFound",
+			urlVars: map[string]string{"id": "foo"},
+			storeFunc: func(s *mockstore.V2MockStore) {
+				s.On("Get", mock.Anything).
+					Return((*wrap.Wrapper)(nil), &store.ErrNotFound{})
+			},
+			wantErr: true,
+		},
+		{
+			name:    "store ErrInternal",
+			urlVars: map[string]string{"id": "foo"},
+			storeFunc: func(s *mockstore.V2MockStore) {
+				s.On("Get", mock.Anything).
+					Return((*wrap.Wrapper)(nil), &store.ErrInternal{})
+			},
+			wantErr: true,
+		},
+		{
+			name:    "successful get",
+			urlVars: map[string]string{"id": "foo"},
+			storeFunc: func(s *mockstore.V2MockStore) {
+				s.On("Get", mock.Anything).
+					Return(wrapper, nil)
+			},
+			want: barResource,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			store := &mockstore.V2MockStore{}
+			if tt.storeFunc != nil {
+				tt.storeFunc(store)
+			}
+
+			h := Handlers{
+				V3Resource: &fixture.V3Resource{},
+				StoreV2:    store,
+			}
+
+			r, _ := http.NewRequest(http.MethodGet, "/", nil)
+			r = mux.SetURLVars(r, tt.urlVars)
+
+			got, err := h.GetV3Resource(r)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Handlers.GetResource() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Handlers.GetResource() = %#v, want %#v", got, tt.want)
+			}
+		})
+	}
+}
