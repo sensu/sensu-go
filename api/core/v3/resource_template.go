9c43613b722c817fe5f50414574524c32bde2d24
Eric Chlebek <eric@sensu.io>
2021-01-26 13:27:31 -0800

    Add template execution
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- api/core/v3/resource_template.go
+++ api/core/v3/resource_template.go
@@ -1,15 +1,53 @@
 package v3
 
-import corev2 "github.com/sensu/sensu-go/api/core/v2"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"html/template"
 
+	corev2 "github.com/sensu/sensu-go/api/core/v2"
+	types "github.com/sensu/sensu-go/types"
+)
+
+// ResourceTemplate is a template for core/v3 resources.
 type ResourceTemplate struct {
 	Metadata   *corev2.ObjectMeta `json:"metadata"`
 	APIVersion string             `json:"api_version"`
 	Type       string             `json:"type"`
 	Template   string             `json:"template"`
-	Path       string             `json:"path"`
 }
 
 func (r *ResourceTemplate) GetMetadata() *corev2.ObjectMeta {
 	return r.Metadata
+}
+
+// Execute executes the Template in the ResourceTemplate. It's given a metadata
+// object to draw variables from. Typically, templating will be done with a
+// variable namespace or name, but could also be done with labels and annotations
+// from the metadata, depending on the nature of the template.
+func (r *ResourceTemplate) Execute(meta *corev2.ObjectMeta) (Resource, error) {
+	tmpl, err := template.New("resource").Parse(r.Template)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing resource template: %s", err)
+	}
+	var buf bytes.Buffer
+	if err := tmpl.Execute(&buf, meta); err != nil {
+		return nil, fmt.Errorf("error executing resource template: %s", err)
+	}
+	t, err := types.ResolveRaw(r.APIVersion, r.Type)
+	if err != nil {
+		return nil, fmt.Errorf("error expanding resource template: unknown type: %s", err)
+	}
+	resource, ok := t.(Resource)
+	if !ok {
+		return nil, fmt.Errorf("error expanding resource template: %T is not a core/v3 resource", t)
+	}
+	if err := json.Unmarshal(buf.Bytes(), &resource); err != nil {
+		return nil, fmt.Errorf("error expanding resource template: invalid json: %s", err)
+	}
+	if err := resource.Validate(); err != nil {
+		return nil, fmt.Errorf("error expanding resource template: resource not valid: %s", err)
+	}
+	return resource, nil
 }
