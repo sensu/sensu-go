baf9b2373c9d8ce2d64acd6c7b0d4c43990b9f56
Eric Chlebek <eric@sensu.io>
2021-02-25 16:02:59 -0800

    Add flag for prometheus metrics
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- cmd/loadit/main.go
+++ cmd/loadit/main.go
@@ -27,6 +27,7 @@ var (
 	flagKeepaliveInterval = flag.Int("keepalive-interval", agent.DefaultKeepaliveInterval, "Keepalive interval")
 	flagKeepaliveTimeout  = flag.Int("keepalive-timeout", types.DefaultKeepaliveTimeout, "Keepalive timeout")
 	flagProfilingPort     = flag.Int("pprof-port", 6060, "pprof port to bind to")
+	flagPromPort          = flag.String("prom", ":8080", "binding for prometheus server")
 )
 
 func main() {
@@ -74,7 +75,7 @@ func main() {
 		cfg.MockSystemInfo = true
 		cfg.BackendHeartbeatInterval = 30
 		cfg.BackendHeartbeatTimeout = 300
-		cfg.PrometheusPort = ":8080"
+		cfg.PrometheusPort = *flagPromPort
 
 		agent, err := agent.NewAgent(cfg)
 		if err != nil {
