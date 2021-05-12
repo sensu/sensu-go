42018a38003e941bf3283b9f3080f94b40cedbc8
James Phillips <jamesdphillips@gmail.com>
2021-05-11 18:46:00 -0700

    only apply deathsig on linux platform
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

---
+++ command/proc_group_windows.go
@@ -0,0 +1,13 @@
+// +build windows
+
+package command
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+// SetProcessGroup sets the process group of the command process
+func SetProcessGroup(cmd *exec.Cmd) {
+	cmd.SysProcAttr.CreationFlags = syscall.CREATE_NEW_PROCESS_GROUP
+}
