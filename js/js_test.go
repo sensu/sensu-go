6555322be77ff04b2a176a13be694868e408fa09
Jef Spaleta <jef@sensu.io>
2021-06-11 14:20:50 -0800

    extend time helper functions for filters (#4314)
    * extend time helper functions for filters
    
    Signed-off-by: Jef Spaleta <jspaleta@gmail.com>
    
    * add additional tests for the time function comparison expressions
    
    Signed-off-by: Jef Spaleta <jspaleta@gmail.com>

--- js/js_test.go
+++ js/js_test.go
@@ -5,6 +5,7 @@ import (
 
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
 	"github.com/sensu/sensu-go/types/dynamic"
+	"github.com/stretchr/testify/assert"
 )
 
 // This is a unit test to cover the race condition found in
@@ -20,4 +21,88 @@ func TestEvaluateRaceCondition(t *testing.T) {
 	go func() {
 		_, _ = Evaluate("true", params, nil)
 	}()
+}
+
+func TestTimeFunctions(t *testing.T) {
+	timestamp := int64(7323)
+	t.Run("failing hour() function", func(t *testing.T) {
+		entity := corev2.FixtureEvent("foo", "bar")
+		entity.Timestamp = timestamp
+		synth := dynamic.Synthesize(entity)
+		params := map[string]interface{}{"event": synth}
+
+		result, err := Evaluate("hour(event.timestamp) != 2", params, nil)
+		assert.NoError(t, err)
+		assert.Equal(t, false, result)
+	})
+	t.Run("failing minute() function", func(t *testing.T) {
+		entity := corev2.FixtureEvent("foo", "bar")
+		entity.Timestamp = timestamp
+		synth := dynamic.Synthesize(entity)
+		params := map[string]interface{}{"event": synth}
+
+		result, err := Evaluate("minute(event.timestamp) != 2", params, nil)
+		assert.NoError(t, err)
+		assert.Equal(t, false, result)
+	})
+	t.Run("failing second() function", func(t *testing.T) {
+		entity := corev2.FixtureEvent("foo", "bar")
+		entity.Timestamp = timestamp
+		synth := dynamic.Synthesize(entity)
+		params := map[string]interface{}{"event": synth}
+
+		result, err := Evaluate("second(event.timestamp) != 3", params, nil)
+		assert.NoError(t, err)
+		assert.Equal(t, false, result)
+	})
+	t.Run("failing seconds_since() function", func(t *testing.T) {
+		entity := corev2.FixtureEvent("foo", "bar")
+		entity.Timestamp = timestamp
+		synth := dynamic.Synthesize(entity)
+		params := map[string]interface{}{"event": synth}
+
+		result, err := Evaluate("seconds_since(event.timestamp) <= 10000", params, nil)
+		assert.NoError(t, err)
+		assert.Equal(t, false, result)
+	})
+	t.Run("passing hour() function", func(t *testing.T) {
+		entity := corev2.FixtureEvent("foo", "bar")
+		entity.Timestamp = timestamp
+		synth := dynamic.Synthesize(entity)
+		params := map[string]interface{}{"event": synth}
+
+		result, err := Evaluate("hour(event.timestamp) == 2", params, nil)
+		assert.NoError(t, err)
+		assert.Equal(t, true, result)
+	})
+	t.Run("passing minute() function", func(t *testing.T) {
+		entity := corev2.FixtureEvent("foo", "bar")
+		entity.Timestamp = timestamp
+		synth := dynamic.Synthesize(entity)
+		params := map[string]interface{}{"event": synth}
+
+		result, err := Evaluate("minute(event.timestamp) == 2", params, nil)
+		assert.NoError(t, err)
+		assert.Equal(t, true, result)
+	})
+	t.Run("passing second() function", func(t *testing.T) {
+		entity := corev2.FixtureEvent("foo", "bar")
+		entity.Timestamp = timestamp
+		synth := dynamic.Synthesize(entity)
+		params := map[string]interface{}{"event": synth}
+
+		result, err := Evaluate("second(event.timestamp) == 3", params, nil)
+		assert.NoError(t, err)
+		assert.Equal(t, true, result)
+	})
+	t.Run("passing seconds_since() function", func(t *testing.T) {
+		entity := corev2.FixtureEvent("foo", "bar")
+		entity.Timestamp = timestamp
+		synth := dynamic.Synthesize(entity)
+		params := map[string]interface{}{"event": synth}
+
+		result, err := Evaluate("seconds_since(event.timestamp) > 10000", params, nil)
+		assert.NoError(t, err)
+		assert.Equal(t, true, result)
+	})
 }
