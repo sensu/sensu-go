8ee5917c8e64f5596106867e23bc0773fa1be234
Justin Kolberg <amd.prophet@gmail.com>
2021-09-09 12:10:38 -0700

    use t.TempDir() for config-dir tests (#4409)
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- cli/client/config/basic/basic_test.go
+++ cli/client/config/basic/basic_test.go
@@ -11,12 +11,12 @@ import (
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestFlags(t *testing.T) {
 	flags := pflag.NewFlagSet("api-url", pflag.ContinueOnError)
 	flags.String("api-url", "foo", "")
+	flags.String("config-dir", t.TempDir(), "")
 	v := viper.New()
 	_ = v.BindPFlags(flags)
 
@@ -29,6 +29,7 @@ func TestFlags(t *testing.T) {
 func TestEnv(t *testing.T) {
 	flags := pflag.NewFlagSet("api-url", pflag.ContinueOnError)
 	_ = os.Setenv("SENSU_API_URL", "foo_env")
+	_ = os.Setenv("SENSU_CONFIG_DIR", t.TempDir())
 	v, _ := helpers.InitViper(flags)
 
 	config := Load(flags, v)
@@ -39,10 +40,7 @@ func TestEnv(t *testing.T) {
 
 func TestLoad(t *testing.T) {
 	// Create a dummy directory for testing
-	dir, _ := ioutil.TempDir("", "sensu")
-	defer func() {
-		require.NoError(t, os.RemoveAll(dir))
-	}()
+	dir := t.TempDir()
 
 	// Set flags
 	flags := pflag.NewFlagSet("config-dir", pflag.ContinueOnError)
@@ -69,9 +67,12 @@ func TestLoad(t *testing.T) {
 }
 
 func TestLoadMissingFiles(t *testing.T) {
+	// Create a dummy directory for testing
+	dir := t.TempDir()
+
 	// Set flags
 	flags := pflag.NewFlagSet("config-dir", pflag.ContinueOnError)
-	flags.String("config-dir", "/tmp/sensu", "")
+	flags.String("config-dir", dir, "")
 	v := viper.New()
 	_ = v.BindPFlags(flags)
 
@@ -81,11 +82,7 @@ func TestLoadMissingFiles(t *testing.T) {
 
 func TestOpen(t *testing.T) {
 	// Create a dummy directory for testing
-	dir, err := ioutil.TempDir("", "sensu")
-	require.NoError(t, err)
-	defer func() {
-		require.NoError(t, os.RemoveAll(dir))
-	}()
+	dir := t.TempDir()
 
 	// Create a dummy cluster file
 	cluster := &Cluster{APIUrl: "localhost"}
@@ -99,8 +96,11 @@ func TestOpen(t *testing.T) {
 }
 
 func TestOpenMissingFile(t *testing.T) {
+	// Create a dummy directory for testing
+	dir := t.TempDir()
+
 	config := &Config{}
 
-	err := config.open("/tmp/sensu/missingfile")
+	err := config.open(filepath.Join(dir, "missingfile"))
 	assert.Error(t, err)
 }
