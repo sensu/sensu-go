c5414e74f63642eb6736abea4c7ee2c9abbb4038
Cyril Cressent <cyril@sensu.io>
2021-09-21 08:42:41 -0700

    Add API_KEY and TIMEOUT to sensuctl env output
    Signed-off-by: Cyril Cressent <cyril@sensu.io>

--- cli/commands/env/env_test.go
+++ cli/commands/env/env_test.go
@@ -2,6 +2,7 @@ package env
 
 import (
 	"testing"
+	"time"
 
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
 	mockclient "github.com/sensu/sensu-go/cli/client/testing"
@@ -17,9 +18,10 @@ func testSetupMocks(t *testing.T, config *mockclient.MockConfig) {
 	config.On("Tokens").Return(
 		corev2.FixtureTokens("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NjIxODkzMTcsImp0aSI6IjAwZDFlYTE2OGU1MTQ1ZGEzN2U2Njg0YmRlOTgwNDM4Iiwic3ViIjoiYWRtaW4iLCJncm91cHMiOlsiY2x1c3Rlci1hZG1pbnMiLCJzeXN0ZW06dXNlcnMiXSwicHJvdmlkZXIiOnsicHJvdmlkZXJfaWQiOiJiYXNpYyIsInVzZXJfaWQiOiJhZG1pbiJ9fQ.ksuMGCJtkN5724CQ7e2W1P7T2ZPpR8IxU3fH9WhBMLk", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0MGVhYTRiMzRkMzU4YTkzNTY5YzIzZWM1YjcxNmZiMiIsInN1YiI6ImFkbWluIiwiZ3JvdXBzIjpudWxsLCJwcm92aWRlciI6eyJwcm92aWRlcl9pZCI6IiIsInVzZXJfaWQiOiIifX0.7t0qoBvKEkHD1DJbhP-VfSj95yhsFyrPoeFhqEbKOn8"),
 	)
-	config.On("APIKey").Return("")
+	config.On("APIKey").Return("some-api-key")
 	config.On("TrustedCAFile").Return("")
 	config.On("InsecureSkipTLSVerify").Return(false)
+	config.On("Timeout").Return(time.Duration(42) * time.Second)
 }
 
 func TestEnvCommandBash(t *testing.T) {
@@ -29,8 +31,10 @@ func TestEnvCommandBash(t *testing.T) {
 	cmd := Command(cli)
 	_ = cmd.Flags().Set("shell", "bash")
 	out, err := test.RunCmd(cmd, nil)
-	assert.Regexp(t, `export SENSU_API_URL="http://127.0.0.1:8080"`, out)
 	assert.NoError(t, err)
+	assert.Regexp(t, `export SENSU_API_URL="http://127.0.0.1:8080"`, out)
+	assert.Regexp(t, `export SENSU_API_KEY="some-api-key"`, out)
+	assert.Regexp(t, `export SENSU_TIMEOUT="42s"`, out)
 }
 
 func TestEnvCommandCmd(t *testing.T) {
@@ -40,8 +44,10 @@ func TestEnvCommandCmd(t *testing.T) {
 	cmd := Command(cli)
 	_ = cmd.Flags().Set("shell", "cmd")
 	out, err := test.RunCmd(cmd, nil)
-	assert.Regexp(t, `SET SENSU_API_URL=http://127.0.0.1:8080`, out)
 	assert.NoError(t, err)
+	assert.Regexp(t, `SET SENSU_API_URL=http://127.0.0.1:8080`, out)
+	assert.Regexp(t, `SET SENSU_API_KEY=some-api-key`, out)
+	assert.Regexp(t, `SET SENSU_TIMEOUT=42s`, out)
 }
 
 func TestEnvCommandPowershell(t *testing.T) {
@@ -51,6 +57,8 @@ func TestEnvCommandPowershell(t *testing.T) {
 	cmd := Command(cli)
 	_ = cmd.Flags().Set("shell", "powershell")
 	out, err := test.RunCmd(cmd, nil)
-	assert.Regexp(t, `\$Env:SENSU_API_URL = "http://127.0.0.1:8080"`, out)
 	assert.NoError(t, err)
+	assert.Regexp(t, `\$Env:SENSU_API_URL = "http://127.0.0.1:8080"`, out)
+	assert.Regexp(t, `\$Env:SENSU_API_KEY = "some-api-key"`, out)
+	assert.Regexp(t, `\$Env:SENSU_TIMEOUT = "42s"`, out)
 }
