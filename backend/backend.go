839f564939af4f4b028e3e3c47d2cd5417fa99a9
James Phillips <jamesdphillips@gmail.com>
2021-11-01 15:06:46 -0700

    configure apid
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- backend/backend.go
+++ backend/backend.go
@@ -585,6 +585,7 @@ func Initialize(ctx context.Context, config *Config) (*Backend, error) {
 	b.APIDConfig = apid.Config{
 		ListenAddress:       config.APIListenAddress,
 		RequestLimit:        config.APIRequestLimit,
+		WriteTimeout:        config.APIWriteTimeout,
 		URL:                 config.APIURL,
 		Bus:                 bus,
 		Store:               b.Store,
