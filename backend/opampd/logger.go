0a2a11fdfbd8cb9e90ccb8e1f8e20267dbf90f49
Francis Guimond <fguimond@users.noreply.github.com>
2021-12-07 17:04:28 -0500

    Cleanup
    Signed-off-by: Francis Guimond <fguimond@users.noreply.github.com>

--- backend/opampd/logger.go
+++ backend/opampd/logger.go
@@ -7,18 +7,3 @@ import (
 var logger = logrus.WithFields(logrus.Fields{
 	"component": "opampd",
 })
-
-//type logrusIOWriter struct {
-//	entry *logrus.Entry
-//}
-//
-//// Write satifies the io.Writer interface
-//func (w *logrusIOWriter) Write(b []byte) (int, error) {
-//	n := len(b)
-//
-//	// Remove all leading and trailing white space
-//	b = bytes.TrimSpace(b)
-//
-//	w.entry.Warning(string(b))
-//	return n, nil
-//}
