d071b681817a0bb5d785df99e756b79e7e30dc9b
c-kruse <ctkruse99@gmail.com>
2021-12-23 14:59:17 -0800

    add unit test coverage
    Signed-off-by: c-kruse <ctkruse99@gmail.com>

--- backend/backend_unit_test.go
+++ backend/backend_unit_test.go
@@ -3,17 +3,23 @@ package backend
 import (
 	"errors"
 	"testing"
+
+	"go.uber.org/atomic"
 )
 
+var closedCt atomic.Int64
+
 type mockStopper struct {
-	hang bool
-	err  error
+	hang   bool
+	err    error
+	closed int64
 }
 
-func (s mockStopper) Stop() error {
+func (s *mockStopper) Stop() error {
 	if s.hang {
 		select {}
 	}
+	s.closed = closedCt.Inc()
 	return s.err
 }
 
@@ -23,13 +29,21 @@ func (s mockStopper) Name() string {
 
 func TestStopGroupNormalStop(t *testing.T) {
 	var sg stopGroup
-	sg.Add(mockStopper{})
-	sg.Add(mockStopper{})
-	sg.Add(mockStopper{})
+	stoppers := []*mockStopper{{}, {}, {}, {}}
+	for _, s := range stoppers {
+		sg.Add(s)
+	}
 	if err := sg.Stop(); err != nil {
 		t.Fatal(err)
 	}
-	sg.Add(mockStopper{err: errors.New("err")})
+	// validate stopper close order
+	for i := 1; i < len(stoppers); i++ {
+		if stoppers[i].closed != stoppers[i-1].closed-1 {
+			t.Errorf("expected stopper %d to be closed before %d", i, i-1)
+		}
+	}
+	// validate erorr handling
+	sg.Add(&mockStopper{err: errors.New("err")})
 	if err := sg.Stop(); err == nil {
 		t.Fatal("expected non-nil error")
 	}
