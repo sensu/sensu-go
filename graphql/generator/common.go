2d972640d708339883653e6df0983ea729a38c43
James Phillips <jamesdphillips@gmail.com>
2022-03-04 14:15:41 -0800

    appease staticcheck
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- graphql/generator/common.go
+++ graphql/generator/common.go
@@ -111,14 +111,6 @@ func genRegisterFn(node ast.Node, resolverImpl jen.Code) jen.Code {
 	return code
 }
 
-func isNonNullableEnum(tt ast.Type, i info) bool {
-	t, ok := tt.(*ast.NonNull)
-	if !ok {
-		return false
-	}
-	return isEnum(t.Type, i)
-}
-
 func isEnum(tt ast.Type, i info) bool {
 	n, ok := tt.(*ast.NonNull)
 	if ok {
