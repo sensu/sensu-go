f51d5a9bd9f727a67354ef17fa763c4b86bfad40
Eric Chlebek <eric@sensu.io>
2022-04-24 01:54:28 -0700

    Simplify build.sh and build.ps1
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- build.ps1
+++ build.ps1
@@ -164,7 +164,7 @@ function unit_test_commands
 {
     echo "Running unit tests..."
 
-    go test -v $RACE ./agent/... ./asset/... ./bonsai/... ./cli/... ./command/...
+	go test $RACE ./...
     If ($LASTEXITCODE -ne 0) {
         echo "Unit testing failed..."
         exit 1
