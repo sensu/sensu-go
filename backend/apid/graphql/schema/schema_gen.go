fbc3b4df7391129ee9547537810b3192e933609f
Melissa Page <73hl10n@gmail.com>
2022-07-06 11:20:52 -0700

    [GraphQL] Add Builds to Asset (#4790)
    * Add builds to asset
    Signed-off-by: Melissa Page <73hl10n@gmail.com>
    Co-authored-by: James Phillips <jamesdphillips@gmail.com>

--- backend/apid/graphql/schema/schema_gen.go
+++ backend/apid/graphql/schema/schema_gen.go
@@ -1,4 +1,4 @@
 package schema
 
-//go:generate go run github.com/sensu/sensu-go/scripts/gen_gqltype -types Secret,Pipeline,PipelineWorkflow,ResourceReference -pkg-path ../../../../api/core/v2 -o ./corev2.gen.graphql
+//go:generate go run github.com/sensu/sensu-go/scripts/gen_gqltype -types Secret,Pipeline,PipelineWorkflow,ResourceReference,AssetBuild -pkg-path ../../../../api/core/v2 -o ./corev2.gen.graphql
 //go:generate go run github.com/sensu/sensu-go/scripts/gengraphql .
