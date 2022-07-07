4d8876c1cf7a6b8ad037f5601ccacdd713e16b17
James Phillips <jamesdphillips@gmail.com>
2022-07-07 15:28:16 -0700

    test
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- backend/apid/graphql/rbac.go
+++ backend/apid/graphql/rbac.go
@@ -67,6 +67,7 @@ func (*roleImpl) ID(p graphql.ResolveParams) (string, error) {
 // IsTypeOf is used to determine if a given value is associated with the type
 func (*roleImpl) IsTypeOf(s interface{}, p graphql.IsTypeOfParams) bool {
 	_, ok := s.(*corev2.Role)
+	logger.Warnf("T: %T, OK: %s", s, ok)
 	return ok
 }
 
