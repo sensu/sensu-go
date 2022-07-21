d5727eff7f9bbfe2a8489172f2059bd27129e470
James Phillips <jamesdphillips@gmail.com>
2022-07-20 22:51:41 -0700

    fields()
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- api/core/v3/entity_state.go
+++ api/core/v3/entity_state.go
@@ -9,3 +9,11 @@ var entityStateRBACName = (&corev2.Entity{}).RBACName()
 func (*EntityState) rbacName() string {
 	return entityStateRBACName
 }
+
+func (e *EntityState) Fields() map[string]string {
+	fields := map[string]string{
+		"entity_state.name":      e.Metadata.Name,
+		"entity_state.namespace": e.Metadata.Namespace,
+	}
+	return fields
+}
