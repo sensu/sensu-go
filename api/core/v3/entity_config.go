7a13587fdcdaacdb94dbf477c375f20f26624d0f
James Phillips <jamesdphillips@gmail.com>
2022-09-12 13:53:10 -0700

    Redact entity configuration metadata (#4862)
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- api/core/v3/entity_config.go
+++ api/core/v3/entity_config.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
+	stringutil "github.com/sensu/sensu-go/api/core/v3/internal/strings"
 )
 
 var entityConfigRBACName = (&corev2.Entity{}).RBACName()
@@ -30,4 +31,36 @@ func MergeMapWithPrefix(a map[string]string, b map[string]string, prefix string)
 	for k, v := range b {
 		a[prefix+k] = v
 	}
+}
+
+func redactMap(m map[string]string, redact []string) map[string]string {
+	if len(redact) == 0 {
+		redact = corev2.DefaultRedactFields
+	}
+	result := make(map[string]string, len(m))
+	for k, v := range m {
+		if stringutil.FoundInArray(k, redact) {
+			result[k] = corev2.Redacted
+		} else {
+			result[k] = v
+		}
+	}
+	return result
+}
+
+// ProduceRedacted redacts the entity according to the entity's Redact fields.
+// A redacted copy is returned. The copy contains pointers to the original's
+// memory, with different Labels and Annotations.
+func (e *EntityConfig) ProduceRedacted() Resource {
+	if e == nil {
+		return nil
+	}
+	if e.Metadata == nil || (e.Metadata.Labels == nil && e.Metadata.Annotations == nil) {
+		return e
+	}
+	copy := &EntityConfig{}
+	*copy = *e
+	copy.Metadata.Annotations = redactMap(e.Metadata.Annotations, e.Redact)
+	copy.Metadata.Labels = redactMap(e.Metadata.Labels, e.Redact)
+	return copy
 }
