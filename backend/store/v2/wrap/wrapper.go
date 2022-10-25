2f0b753d68b1bf0409d95d2137fc2f93e93a2c63
Christian Kruse <ctkruse99@gmail.com>
2022-10-25 14:32:17 -0700

    replace types.ResolveRaw usage
    Signed-off-by: Christian Kruse <ctkruse99@gmail.com>

--- backend/store/v2/wrap/wrapper.go
+++ backend/store/v2/wrap/wrapper.go
@@ -12,6 +12,7 @@ import (
 	"github.com/golang/snappy"
 	corev2 "github.com/sensu/core/v2"
 	corev3 "github.com/sensu/core/v3"
+	apitools "github.com/sensu/sensu-api-tools"
 	"github.com/sensu/sensu-go/backend/store"
 	"github.com/sensu/sensu-go/types"
 )
@@ -220,7 +221,7 @@ func (w *Wrapper) Unwrap() (corev3.Resource, error) {
 
 // UnwrapRaw is like Unwrap, but returns a raw interface{} value.
 func (w *Wrapper) UnwrapRaw() (interface{}, error) {
-	resource, err := types.ResolveRaw(w.TypeMeta.APIVersion, w.TypeMeta.Type)
+	resource, err := apitools.Resolve(w.TypeMeta.APIVersion, w.TypeMeta.Type)
 	if err != nil {
 		return nil, err
 	}
