f05d1b69a5c715d59a7c4b7b606589b9d57030fc
Gustav Danielsson <gdanielsson@sumologic.com>
2023-02-24 16:24:57 -0700

    make graphql-depth-limit validator parameterized
    Signed-off-by: Gustav Danielsson <gdanielsson@sumologic.com>

--- graphql/validators.go
+++ graphql/validators.go
@@ -12,13 +12,55 @@ const (
 	MaxQueryNodeDepth = 5
 )
 
-func reportError(context *graphql.ValidationContext, message string, nodes []ast.Node) (string, interface{}) {
-	context.ReportError(gqlerrors.NewError(message, nodes, "", nil, []int{}, nil))
-	return visitor.ActionNoChange, nil
+type maxDepthRule struct {
+	context    *graphql.ValidationContext
+	depthLimit int
+}
+
+func UnauthedValidators() []graphql.ValidationRuleFn {
+	rules := []graphql.ValidationRuleFn{MaxDepthRule(MaxQueryNodeDepth)}
+	return rules
+}
+
+func MaxDepthRule(depthLimit int) graphql.ValidationRuleFn {
+	rule := &maxDepthRule{
+		depthLimit: depthLimit,
+	}
+	return rule.maxDepthRuleWithContext
+}
+
+func (r *maxDepthRule) maxDepthRuleWithContext(context *graphql.ValidationContext) *graphql.ValidationRuleInstance {
+	rule := &maxDepthRule{
+		context:    context,
+		depthLimit: r.depthLimit,
+	}
+	return &graphql.ValidationRuleInstance{VisitorOpts: rule.maxDepthVisitorOptions()}
+}
+
+func (rule *maxDepthRule) maxDepthVisitorOptions() *visitor.VisitorOptions {
+	return &visitor.VisitorOptions{
+		KindFuncMap: map[string]visitor.NamedVisitFuncs{
+			kinds.OperationDefinition: {
+				Kind: func(p visitor.VisitFuncParams) (string, interface{}) {
+					node := p.Node.(ast.Node)
+					if node != nil {
+						validateMaxDepth(rule.context, node, 1, rule.depthLimit, nil)
+					}
+					return visitor.ActionNoChange, nil
+				},
+			},
+		},
+	}
 }
 
 // validate max depth of a GraphQL query with a depthLimit
-func validateMaxDepth(context *graphql.ValidationContext, node ast.Node, currentDepth int, depthLimit int) int {
+func validateMaxDepth(
+	context *graphql.ValidationContext,
+	node ast.Node,
+	currentDepth int,
+	depthLimit int,
+	visited map[*ast.FragmentDefinition]bool,
+) int {
 	// end recursion early if error reported
 	if errors := context.Errors(); len(errors) > 0 {
 		return -1
@@ -27,6 +69,11 @@ func validateMaxDepth(context *graphql.ValidationContext, node ast.Node, current
 	if currentDepth > depthLimit {
 		reportError(context, "Max depth exceeded", []ast.Node{node})
 		return -1
+	}
+
+	// keep map of visited fragment spreads to prevent infinite loop
+	if visited == nil {
+		visited = map[*ast.FragmentDefinition]bool{}
 	}
 
 	var selectionSet *ast.SelectionSet
@@ -38,21 +85,23 @@ func validateMaxDepth(context *graphql.ValidationContext, node ast.Node, current
 			selections := selectionSet.Selections
 			maxDepth := currentDepth
 			for _, selection := range selections {
-				nextDepth := validateMaxDepth(context, selection.(ast.Node), currentDepth+1, depthLimit)
+				nextDepth := validateMaxDepth(context, selection.(ast.Node), currentDepth+1, depthLimit, visited)
 				if nextDepth > maxDepth {
 					maxDepth = nextDepth
 				}
 			}
 			return maxDepth
 		}
-		return 0
+		return currentDepth
 	case kinds.FragmentSpread:
 		fragName := node.(*ast.FragmentSpread).Name.Value
-		spreadFragment := context.Fragment(fragName)
-		if spreadFragment != nil {
-			return validateMaxDepth(context, spreadFragment, currentDepth, depthLimit)
+		fragment := context.Fragment(fragName)
+		if fragment == nil || visited[fragment] {
+			return currentDepth
 		}
-		return 0
+		visited[fragment] = true
+		// fragment spreads don't increase the depth
+		return validateMaxDepth(context, fragment, currentDepth, depthLimit, visited)
 	case kinds.InlineFragment:
 		selectionSet = node.(*ast.InlineFragment).GetSelectionSet()
 	case kinds.FragmentDefinition:
@@ -65,61 +114,17 @@ func validateMaxDepth(context *graphql.ValidationContext, node ast.Node, current
 		selections := selectionSet.Selections
 		maxDepth := currentDepth
 		for _, selection := range selections {
-			nextDepth := validateMaxDepth(context, selection.(ast.Node), currentDepth, depthLimit)
+			// inline fragments, fragment definitions and operation definitions don't increase the depth
+			nextDepth := validateMaxDepth(context, selection.(ast.Node), currentDepth, depthLimit, visited)
 			if nextDepth > maxDepth {
 				maxDepth = nextDepth
 			}
 		}
 		return maxDepth
 	}
-	return 0
+	return currentDepth
 }
 
-func (rule *maxDepthRule) maxDepthVisitorOptions() *visitor.VisitorOptions {
-	return &visitor.VisitorOptions{
-		KindFuncMap: map[string]visitor.NamedVisitFuncs{
-			kinds.OperationDefinition: {
-				Kind: func(p visitor.VisitFuncParams) (string, interface{}) {
-					node := p.Node.(ast.Node)
-					if node != nil {
-						validateMaxDepth(rule.context, node, 0, rule.depthLimit)
-					}
-					return visitor.ActionNoChange, nil
-				},
-			},
-		},
-	}
-}
-
-type maxDepthRule struct {
-	context    *graphql.ValidationContext
-	depthLimit int
-}
-
-func newMaxDepthRule(context *graphql.ValidationContext, depthLimit int) *maxDepthRule {
-	return &maxDepthRule{
-		context:    context,
-		depthLimit: depthLimit,
-	}
-}
-
-func MaxDepthRule(depthLimit int) graphql.ValidationRuleFn {
-	rule := &maxDepthRule{
-		depthLimit: depthLimit,
-	}
-	return rule.validateRule
-}
-
-func (r *maxDepthRule) validateRule(context *graphql.ValidationContext) *graphql.ValidationRuleInstance {
-	rule := newMaxDepthRule(context, r.depthLimit)
-	return &graphql.ValidationRuleInstance{VisitorOpts: rule.maxDepthVisitorOptions()}
-}
-
-type UnauthedValidatorOpts struct {
-	DepthLimit int
-}
-
-func ProvideUnauthedValidators(opts UnauthedValidatorOpts) []graphql.ValidationRuleFn {
-	rules := []graphql.ValidationRuleFn{MaxDepthRule(opts.DepthLimit)}
-	return rules
+func reportError(context *graphql.ValidationContext, message string, nodes []ast.Node) {
+	context.ReportError(gqlerrors.NewError(message, nodes, "", nil, []int{}, nil))
 }
