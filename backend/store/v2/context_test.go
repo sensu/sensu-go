b2431a470db7860a7b0823d1ab151c35db51ff78
Eric Chlebek <eric@sensu.io>
2023-04-27 17:01:00 -0700

    Add context helpers for TxInfo and ETag directives
    This commit adds some utility functions for putting ETag directives into
    store requests, and also a way for stores to write back transaction
    metadata with TxInfo.
    
    Signed-off-by: Eric Chlebek <eric@sensu.io>

---
+++ backend/store/v2/context_test.go
@@ -0,0 +1,62 @@
+package v2
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+type contextTest[T any] struct {
+	Value  T
+	Create func(ctx context.Context, val T) context.Context
+	Read   func(ctx context.Context) T
+}
+
+func (c contextTest[T]) Test(t *testing.T) {
+	name := fmt.Sprintf("%T", c.Value)
+	t.Run(name+"_success", func(t *testing.T) {
+		ctx := context.Background()
+		ctx = c.Create(ctx, c.Value)
+		if got, want := c.Read(ctx), c.Value; !cmp.Equal(got, want) {
+			t.Errorf("bad %T: got %v, want %v", got, got, want)
+		}
+	})
+	t.Run(name+"_missing", func(t *testing.T) {
+		value := c.Read(context.Background())
+		if !reflect.ValueOf(value).IsZero() {
+			t.Errorf("missing value is not zero value for %T: %v", value, value)
+		}
+	})
+}
+
+type tester interface {
+	Test(*testing.T)
+}
+
+func TestContext(t *testing.T) {
+	tests := []tester{
+		contextTest[*TxInfo]{
+			Value: &TxInfo{
+				Records: []TxRecordInfo{{}},
+			},
+			Create: ContextWithTxInfo,
+			Read:   TxInfoFromContext,
+		},
+		contextTest[IfMatch]{
+			Value:  IfMatch{"hello", "world"},
+			Create: ContextWithIfMatch,
+			Read:   IfMatchFromContext,
+		},
+		contextTest[IfNoneMatch]{
+			Value:  IfNoneMatch{"hello", "world"},
+			Create: ContextWithIfNoneMatch,
+			Read:   IfNoneMatchFromContext,
+		},
+	}
+	for _, test := range tests {
+		test.Test(t)
+	}
+}
