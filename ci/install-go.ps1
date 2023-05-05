3265684dad5f87666207943b8d6fd8a7fdb7bbd4
Justin Kolberg <amd.prophet@gmail.com>
2023-05-05 15:25:39 -0700

    fix logic in ci/install-go.ps1
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- ci/install-go.ps1
+++ ci/install-go.ps1
@@ -21,28 +21,35 @@ Function Test-CommandExists {
 
 If (Test-CommandExists "go") {
     Write-Output "go found"
+
+    go version | Tee-Object -Variable cmdOutput
+
+    If ($cmdOutput -match 'go version go([\d\.]+) .*/.*') {
+        $detectedVersion=$Matches[1]
+
+        Write-Output "Detected Go version: $detectedVersion"
+
+        If ($detectedVersion -eq $version) {
+            Write-Output "Detected Go version matches requested version"
+            return
+        }
+
+        Write-Output "Go version mismatch"
+    } Else {
+        Write-Output "Could not parse version from `"go version`""
+    }
 } Else {
     Write-Output "go not found"
 }
 
-go version | Tee-Object -Variable cmdOutput
-
-If ($cmdOutput -match 'go version go([\d\.]+) .*/.*') {
-    $detectedVersion=$Matches[1]
-} Else {
-    Write-Output "Could not parse version from `"go version`""
-}
-
-Write-Output "Detected Go version: $detectedVersion"
-
-If ($detectedVersion -eq $version) {
-    Write-Output "Detected Go version matches requested version"
-    return
-}
-
-Write-Output "Go version mismatch, installing Go $version"
+Write-Output "Installing Go $version"
 
 $uri="https://storage.googleapis.com/golang/go$version.windows-amd64.msi"
 
+Write-Output "Downloading $uri"
+
 Invoke-WebRequest -Uri $uri -OutFile "C:\go.msi"
+
+Write-Output "Installing go.msi"
+
 msiexec /i C:\go.msi /q
