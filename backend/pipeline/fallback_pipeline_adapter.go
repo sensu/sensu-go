2714dfb6146aa971886545ab57c1450194813cd7
rakibhossainctr <rakib.hossain.ctr@sumologic.com>
2025-07-17 10:46:25 +0530

    completed mutate, handle and filter method for fallback pipelines
    Signed-off-by: rakibhossainctr <rakib.hossain.ctr@sumologic.com>

--- backend/pipeline/fallback_pipeline_adapter.go
+++ backend/pipeline/fallback_pipeline_adapter.go
@@ -114,19 +114,73 @@ func (f *FallbackPipelinesAdapter) executePipeline(ctx context.Context, pipeline
 	return nil
 }
 
-// TODO - implement this method to process filters
-func (f *FallbackPipelinesAdapter) processFilters(ctx context.Context, filters []*corev2.ResourceReference, event *corev2.Event) (bool, error) {
-	return true, nil
+func (f *FallbackPipelinesAdapter) processFilters(ctx context.Context, refs []*corev2.ResourceReference, event *corev2.Event) (bool, error) {
+	// for each filter reference in the workflow, attempt to find a compatible
+	// filter adapter and use it to filter the event.
+	for _, ref := range refs {
+		filtered, err := f.processFilter(ctx, ref, event)
+		if err != nil {
+			return false, err
+		}
+		if filtered {
+			return true, nil
+		}
+	}
+	return false, nil
 }
 
-// TODO - implement this method to process mutators
-func (f *FallbackPipelinesAdapter) processMutator(ctx context.Context, mutator *corev2.ResourceReference, event *corev2.Event) ([]byte, error) {
-	return []byte{}, nil
+func (f *FallbackPipelinesAdapter) processFilter(ctx context.Context, ref *corev2.ResourceReference, event *corev2.Event) (bool, error) {
+	filter, err := f.getFilterAdapterForResource(ctx, ref)
+	if err != nil {
+		return false, err
+	}
+
+	return filter.Filter(ctx, ref, event)
 }
 
-// TODO - implement this method to process handlers
-func (a *FallbackPipelinesAdapter) processHandler(ctx context.Context, handler *corev2.ResourceReference, event *corev2.Event, mutatedData []byte) error {
-	return nil
+func (f *FallbackPipelinesAdapter) getFilterAdapterForResource(ctx context.Context, ref *corev2.ResourceReference) (FilterAdapter, error) {
+	for _, filterAdapter := range f.FilterAdapters {
+		if filterAdapter.CanFilter(ref) {
+			return filterAdapter, nil
+		}
+	}
+	return nil, fmt.Errorf("no filter adapters were found that can filter the resource: %s.%s = %s", ref.APIVersion, ref.Type, ref.Name)
+}
+
+func (f *FallbackPipelinesAdapter) processMutator(ctx context.Context, ref *corev2.ResourceReference, event *corev2.Event) ([]byte, error) {
+	mutator, err := f.getMutatorAdapterForResource(ctx, ref)
+	if err != nil {
+		return nil, err
+	}
+
+	return mutator.Mutate(ctx, ref, event)
+}
+
+func (f *FallbackPipelinesAdapter) getMutatorAdapterForResource(ctx context.Context, ref *corev2.ResourceReference) (MutatorAdapter, error) {
+	for _, mutatorAdapter := range f.MutatorAdapters {
+		if mutatorAdapter.CanMutate(ref) {
+			return mutatorAdapter, nil
+		}
+	}
+	return nil, fmt.Errorf("no mutator adapters were found that can mutate the resource: %s.%s = %s", ref.APIVersion, ref.Type, ref.Name)
+}
+
+func (f *FallbackPipelinesAdapter) processHandler(ctx context.Context, ref *corev2.ResourceReference, event *corev2.Event, mutatedData []byte) error {
+	handler, err := f.getHandlerAdapterForResource(ctx, ref)
+	if err != nil {
+		return err
+	}
+
+	return handler.Handle(ctx, ref, event, mutatedData)
+}
+
+func (f *FallbackPipelinesAdapter) getHandlerAdapterForResource(ctx context.Context, ref *corev2.ResourceReference) (HandlerAdapter, error) {
+	for _, handlerAdapter := range f.HandlerAdapters {
+		if handlerAdapter.CanHandle(ref) {
+			return handlerAdapter, nil
+		}
+	}
+	return nil, fmt.Errorf("no handler adapters were found that can handle the resource: %s.%s = %s", ref.APIVersion, ref.Type, ref.Name)
 }
 
 // TODO - implement this method to retrieve fallback pipelines from the store
