bb97a275011236e1b37f214b3cc7977f4261df98
rakibhossainctr <rakib.hossain.ctr@sumologic.com>
2025-07-21 11:08:33 +0530

    added fallback pipelines fetching code from etcd
    Signed-off-by: rakibhossainctr <rakib.hossain.ctr@sumologic.com>

--- backend/store/etcd/fallback_pipelines_store.go
+++ backend/store/etcd/fallback_pipelines_store.go
@@ -8,21 +8,41 @@ import (
 	"github.com/sensu/sensu-go/backend/store"
 )
 
+const fallbackPipelinesPathPrefix = "fallback_pipelines"
+
+var (
+	fallbackPipelinesKeyBuilder = store.NewKeyBuilder(fallbackPipelinesPathPrefix)
+)
+
+// GetFallbackPipelinesPath gets the path of the fallback pipelines store.
+func GetFallbackPipelinesPath(ctx context.Context, name string) string {
+	return fallbackPipelinesKeyBuilder.WithContext(ctx).Build(name)
+}
+
 // GetFallbackPipelines retrieves fallback pipelines by name.
 func (s *Store) GetFallbackPipelines(ctx context.Context, name string) ([]*corev2.Pipeline, error) {
 	if name == "" {
 		return nil, &store.ErrNotValid{Err: errors.New("must specify name of fallback pipelines")}
 	}
 
-	// TODO - implement the logic to retrieve all the pipeline from store and return the slice of pipelines
-
-	var f corev2.Pipeline
-	if err := Get(ctx, s.client, GetPipelinesPath(ctx, name), &f); err != nil {
+	var fp corev2.FallbackPipelines
+	if err := Get(ctx, s.client, GetFallbackPipelinesPath(ctx, name), &fp); err != nil {
 		if _, ok := err.(*store.ErrNotFound); ok {
 			err = nil
 		}
 		return nil, err
 	}
+	if len(fp.Pipelines) == 0 {
+		return []*corev2.Pipeline{}, nil
+	}
 
-	return []*corev2.Pipeline{&f}, nil
+	var pipelines []*corev2.Pipeline
+	var p corev2.Pipeline
+	for _, pipeline := range fp.Pipelines {
+		if err := Get(ctx, s.client, GetPipelinesPath(ctx, pipeline.Name), &p); err != nil {
+			continue
+		}
+		pipelines = append(pipelines, &p)
+	}
+	return pipelines, nil
 }
