c9e3f4e7625856a1ab60b9888674f0ed06d73164
Sarvodaya Kumar <sarvodaya.kumar.ctr@sumologic.com>
2026-06-02 15:22:23 +0530

    Fixing the TEST
    Signed-off-by: Sarvodaya Kumar <sarvodaya.kumar.ctr@sumologic.com>

--- backend/schedulerd/check_timer_test.go
+++ backend/schedulerd/check_timer_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestNextCronTime(t *testing.T) {
-	now := mockTime.Now()
+	now := mockTime.Now().UTC()
 
 	// Valid cron string will return a time in the future, on an even minute
 	nextCron, err := NextCronTime(now, "* * * * *")
